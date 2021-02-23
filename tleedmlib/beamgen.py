4d901a06b878178c916536cfed530b5d86a2b3c1
Florian Kraushofer <66359721+fkraushofer@users.noreply.github.com>
2021-02-23 16:02:54 +0100

    code cleanup in beamgen; added version number

--- tleedmlib/beamgen.py
+++ tleedmlib/beamgen.py
@@ -13,16 +13,18 @@ import fortranformat as ff
 
 logger = logging.getLogger("tleedm.beamgen")
 
-def runBeamGen(sl, rp, beamgensource = os.path.join('source','beamgen3.out'),
-               domains = False):
-    """Writes necessary input for the beamgen3 code, the runs it. The relevant 
+
+def runBeamGen(sl, rp, beamgensource=os.path.join('fortran_source',
+                                                  'beamgen3.out'),
+               domains=False):
+    """Writes necessary input for the beamgen3 code, the runs it. The relevant
     output file will be renamed to _BEAMLIST."""
     beamgensource = os.path.join(rp.workdir, beamgensource)
     output = ''
     f74x2 = ff.FortranRecordWriter('2F7.4')
     if sl.bulkslab is None:
         sl.bulkslab = sl.makeBulkSlab(rp)
-    ucbulk = np.transpose(sl.bulkslab.ucell[:2,:2])
+    ucbulk = np.transpose(sl.bulkslab.ucell[:2, :2])
     ol = f74x2.write(ucbulk[0])
     ol = ol.ljust(36)
     output += ol + 'ARA1\n'
@@ -36,8 +38,8 @@ def runBeamGen(sl, rp, beamgensource = os.path.join('source','beamgen3.out'),
     ol = i3x2.write([int(round(f)) for f in rp.SUPERLATTICE[1]])
     ol = ol.ljust(36)
     output += ol + 'LATMAT -  matrix\n'
-    output +=('  1                                 SSYM - symmetry code - cf. '
-              'van Hove / Tong 1979, always 1\n')
+    output += ('  1                                 SSYM - symmetry code - cf.'
+               ' van Hove / Tong 1979, always 1\n')
     if not domains:
         dmin = sl.getMinLayerSpacing() * 0.7
     else:
@@ -47,47 +49,47 @@ def runBeamGen(sl, rp, beamgensource = os.path.join('source','beamgen3.out'),
     ol = f71.write([rp.THEO_ENERGIES[1]]) + f41.write([dmin])
     ol = ol.ljust(36)
     output += (ol + 'EMAX,DMIN - max. energy, min. interlayer distance for '
-              'layer doubling\n')
+               'layer doubling\n')
     output += ('   {:.4f}                           TST - convergence '
-              'criterion for fd. reference calculation\n'
-              .format(rp.ATTENUATION_EPS))
+               'criterion for fd. reference calculation\n'
+               .format(rp.ATTENUATION_EPS))
     output += ('9999                                KNBMAX - max. number of '
-              'beams to be written (may be a format problem!)')
+               'beams to be written (may be a format problem!)')
     try:
         with open('DATA', 'w') as wf:
             wf.write(output)
-    except:
+    except Exception:
         logger.error("Failed to write DATA for _BEAMLIST generation.")
         raise
     if os.name == 'nt':
-        logger.error("Beamlist generation is currently not "
-                         "supported on Windows. Use a linux shell to run "
-                         "beamlist generation script.")
+        logger.error("Beamlist generation is currently not supported on "
+                     "Windows. Use a linux shell to run beamlist generation "
+                     "script.")
         raise EnvironmentError("Beamlist generation is currently not "
                                "supported on Windows.")
     else:
         try:
             subprocess.call(beamgensource)
-        except:
+        except Exception:
             logger.error("Failed to execute beamgen script.")
             raise
     # clean up folder, rename files
     try:
         os.remove('BELIST')
         os.remove('PROT')
-    except:
+    except Exception:
         logger.warning("_BEAMLIST generation: Failed to remove BELIST or "
-                        "PROT file")
+                       "PROT file")
     try:
-        os.rename('DATA','beamgen3-input')
-    except:
+        os.rename('DATA', 'beamgen3-input')
+    except Exception:
         logger.warning("Failed to rename beamlist generation input file "
-                        "DATA to beamgen3-input")
+                       "DATA to beamgen3-input")
     try:
-        os.rename('NBLIST','_BEAMLIST')
-    except:
+        os.rename('NBLIST', '_BEAMLIST')
+    except Exception:
         logger.error("Failed to rename beamlist generation output file "
-                      "NBLIST to _BEAMLIST")
+                     "NBLIST to _BEAMLIST")
         raise
     logger.debug("Wrote to _BEAMLIST successfully.")
     return
