fix: Fixes incorrect symmetry linking through rotation+mirror
Found a bug in displacement assigment: The symrefm (i.e. the matrix that determines how a displacement of one atom in the linked group translates to another) was sometimes being assigned incorrectly. This happened when the atoms that were identified as equivalent through the mirror plane had different rotations with respect to their rotation-only symmetry group.

Found through test case: radial displacements on Te/Pt111(3x3), p3m1 symmetry.

tleedmlib/classes/slab.py
@@ -663,7 +663,8 @@ class Slab:
 
     def mirror(self, symplane, glide=False):
         """Translates the atoms in the slab to have the symplane in the
-        origin, applies a mirror or glide matrix, then translates back"""
+        origin, applies a mirror or glide matrix, then translates back.
+        Very inefficient implementation!"""
         ang = angle(np.array([1, 0]), symplane.dir)
         rotm = np.array([[np.cos(ang), np.sin(ang)],
                          [-np.sin(ang), np.cos(ang)]])
@@ -673,13 +674,13 @@ class Slab:
         if glide:
             abt = self.ucell[:2, :2].T
             glidevec = (symplane.par[0]*abt[0]+symplane.par[1]*abt[1])/2
+        else:
+            glidevec = np.zeros(2)
         for at in self.atlist:
             at.cartpos[:2] -= symplane.pos     # translate to plane
             at.cartpos[:2] = np.dot(rotmirm, at.cartpos[:2])    # apply mirror
             at.cartpos[:2] += symplane.pos     # translate back
-            if glide:
-                # if we're testing for glide plane, add the appropriate vector
-                at.cartpos[0:2] += glidevec
+            at.cartpos[:2] += glidevec   # 0 if not glides
 
     def getLowOccLayer(self):
         """Finds and returns the lowest occupancy sublayer"""

tleedmlib/symmetry.py
@@ -1002,19 +1002,21 @@ def enforceSymmetry(sl, rp, planegroup="fromslab",
                 for (ati, at1) in enumerate(sl1.atlist):
                     for (atj, at2) in enumerate(tmpslab.sublayers[sli]
                                                 .atlist):
-                        if not sl1.atlist[atj] in at1.linklist:
+                        if sl1.atlist[atj] in at1.linklist:
                             # don't check atoms that are already linked
-                            if at1.isSameXY(at2.cartpos[:2], eps):
-                                # combine the two linklists
-                                at1.linklist.extend(sl1.atlist[atj]
-                                                    .linklist)
-                                for at3 in sl1.atlist[atj].linklist:
-                                    at3.symrefm = np.dot(m, np.dot(
-                                        at1.symrefm, at3.symrefm))
-                                    if not at3 == sl1.atlist[atj]:
-                                        at3.linklist = at1.linklist
-                                sl1.atlist[atj].linklist = at1.linklist
-                                break
+                            continue
+                        if not at1.isSameXY(at2.cartpos[:2], eps):
+                            continue
+                        # combine the two linklists
+                        at1.linklist.extend(sl1.atlist[atj].linklist)
+                        for at3 in sl1.atlist[atj].linklist:
+                            at3.symrefm = np.linalg.multi_dot([
+                               np.linalg.inv(at2.symrefm),
+                               at3.symrefm, m, at1.symrefm])
+                            if not at3 == sl1.atlist[atj]:
+                                at3.linklist = at1.linklist
+                        sl1.atlist[atj].linklist = at1.linklist
+                        break
         # TEST MIRROR AND GLIDE PLANES
         if planegroup not in ["p2", "p3", "p4", "p6"]:
             if planegroup in ["pm", "pg", "cm", "pmg", "rcm"]:
@@ -1050,17 +1052,20 @@ def enforceSymmetry(sl, rp, planegroup="fromslab",
                     for (atj, at2) in enumerate(tmpslab.sublayers[sli]
                                                 .atlist):
                         # don't check atoms that are already linked
-                        if not sl1.atlist[atj] in at1.linklist:
-                            if at1.isSameXY(at2.cartpos[:2], eps):
-                                # combine the two linklists
-                                at1.linklist.extend(sl1.atlist[atj].linklist)
-                                for at3 in sl1.atlist[atj].linklist:
-                                    at3.symrefm = np.dot(m, np.dot(
-                                        at1.symrefm, at3.symrefm))
-                                    if not at3 == sl1.atlist[atj]:
-                                        at3.linklist = at1.linklist
-                                sl1.atlist[atj].linklist = at1.linklist
-                                break
+                        if sl1.atlist[atj] in at1.linklist:
+                            continue
+                        if not at1.isSameXY(at2.cartpos[:2], eps):
+                            continue
+                        # combine the two linklists
+                        at1.linklist.extend(sl1.atlist[atj].linklist)
+                        for at3 in sl1.atlist[atj].linklist:
+                            at3.symrefm = np.linalg.multi_dot([
+                                np.linalg.inv(at2.symrefm),
+                                at3.symrefm, m, at1.symrefm])
+                            if not at3 == sl1.atlist[atj]:
+                                at3.linklist = at1.linklist
+                        sl1.atlist[atj].linklist = at1.linklist
+                        break
     sl.linklists = []     # re-create linklists
     for at in sl.atlist:
         if len(at.linklist) > 1 and at.linklist not in sl.linklists: