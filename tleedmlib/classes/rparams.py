Fixes several issues with the error calculation
Fixes some bugs and problems left in the error calculation. This mostly concerns cases where atoms were present that had to be included with a constant parameter value (because of an offset), but that were not being varied themselves.

tleedmlib/classes/rparams.py
@@ -43,19 +43,19 @@ class SearchPar:
         self.deltaname = deltaname
         self.steps = -1     # not used for interpretation, info only
         self.edges = (None, None)  # the first and last value in the range
-        self.center = 1     # the index closest to 0, i.e. "no change"
+        self.center = 1  # the index closest to "no change"
+        self.non_zero = False   # whether the center is truly "unchanged"
         self.restrictTo = None  # None, Index, or other search par
         self.linkedTo = None    # other search par linked via 'atom number'
         self.parabolaFit = {"min": None,
                             "err_co": np.nan, "err_unco": np.nan}
         d = {}
         if mode == "occ":
-            self.steps = len(next(iter(atom.disp_occ.values())))
-            if el in atom.site.occ and el in atom.disp_occ:
-                n = [abs(v - atom.site.occ[el]) for v in atom.disp_occ[el]]
-                self.center = n.index(min(n))
-            else:  # should not happen
-                self.center = int(round(self.steps / 2) + 1)
+            el = next(iter(atom.disp_occ.keys()))  # look at any element
+            self.steps = len(atom.disp_occ[el])
+            self.center = atom.disp_center_index[mode][el] + 1
+            self.non_zero = (abs(atom.disp_occ[el][self.center-1]
+                                 - atom.site.occ[el]) >= 1e-4)
         else:
             if mode == "geo":
                 d = atom.disp_geo
@@ -68,8 +68,11 @@ class SearchPar:
                 k = "all"
             self.steps = len(d[k])
             self.edges = (d[k][0], d[k][-1])
-            n = [np.linalg.norm(v) for v in d[k]]
-            self.center = n.index(min(n)) + 1
+            if k not in atom.disp_center_index[mode]:
+                self.center = atom.disp_center_index[mode]["all"] + 1
+            else:
+                self.center = atom.disp_center_index[mode][k] + 1
+            self.non_zero = (np.linalg.norm(d[k][self.center-1]) >= 1e-4)
 
 
 class DomainParameters:

tleedmlib/files/iosuperpos.py
@@ -17,10 +17,40 @@ logger = logging.getLogger("tleedm.files.iosuperpos")
 
 def writeSuperposInput(sl, rp, config, param_name="PARAM",
                        contrin_name="superpos-CONTRIN",
-                       for_error=False):
-    """Writes a PARAM and a CONTRIN file as input for the Superpos
-    calculation. Returns the contents of CONTRIN, as this will also be needed
-    as an input stream for the superpos calculation."""
+                       for_error=False, only_vary=None):
+    """
+    Writes a PARAM and a CONTRIN file as input for the Superpos calculation.
+    Returns the contents of CONTRIN, as this will also be needed as an input
+    stream for the superpos calculation.
+
+    Parameters
+    ----------
+    sl : Slab
+        The Slab object to be modified.
+    rp : Rparams
+        The run parameters.
+    config : list of int
+        Parameter indices to be applied.
+    param_name : str, optional
+        Alternative name for the PARAM file.
+    contrin_name : str, optional
+        Alternative name for the superpos-CONTRIN file.
+    for_error : bool, optional
+        If True, the 'config' parameter will be ignored, and superpos will
+        instead be calculated for all variations.
+    only_vary : list of SearchPar, optional
+        If running for error, pass a list of search parameters that should be
+        varied. All other search parameters will be kept at their "centered"
+        position.
+
+    Returns
+    -------
+    str
+        Contents of CONTRIN.
+
+    """
+    if only_vary is None:
+        only_vary = rp.searchpars
     mnfiles = 0
     occupations = []    # occupation per delta file
     deltanames = []     # names of delta files
@@ -30,18 +60,18 @@ def writeSuperposInput(sl, rp, config, param_name="PARAM",
     n_conc = 1
     if for_error:
         n_var = max([sp.steps for sp in rp.searchpars
-                     if sp.mode in ("geo", "vib")
-                     and sp.atom in rp.search_atlist])
-        n_conc = max([sp.steps for sp in rp.searchpars if sp.mode == "occ"
-                      and sp.atom in rp.search_atlist])
+                     if sp.mode in ("geo", "vib") and sp in only_vary])
+        n_conc = max([sp.steps for sp in rp.searchpars
+                      if sp.mode == "occ" and sp in only_vary])
     # determine which atoms are "at the surface"
     surfats = sl.getSurfaceAtoms(rp)
     # make lists to print
-    for at in rp.search_atlist:
+    which_atoms = [at for at in rp.search_atlist
+                   if [sp for sp in rp.searchpars
+                       if sp.atom == at and (sp.non_zero or sp in only_vary)]]
+    for at in which_atoms:
         sps = [sp for sp in rp.searchpars if sp.atom == at]
-        if not for_error:
-            occpar = [sp for sp in sps if sp.mode == "occ"][0]  # exactly one
-            occind = rp.searchpars.index(occpar)
+        occpar = [sp for sp in sps if sp.mode == "occ"][0]  # exactly one
         totalocc = np.array([0.] * n_conc)
         for el in at.disp_occ:
             if at in surfats:
@@ -49,7 +79,10 @@ def writeSuperposInput(sl, rp, config, param_name="PARAM",
             else:
                 surfaceChecks.append(0)
             if not for_error:
+                occind = rp.searchpars.index(occpar)
                 o = [at.disp_occ[el][config[occind]-1]]
+            elif occpar not in only_vary:
+                o = [at.disp_occ[el][occpar.center-1]] * n_conc
             else:
                 try:
                     o = [at.disp_occ[el][j] for j in range(n_conc)]
@@ -70,7 +103,7 @@ def writeSuperposInput(sl, rp, config, param_name="PARAM",
                 return ""
             deltanames.append(pl[0].deltaname)
             mnfiles += 1
-            if not for_error:
+            if not for_error or any(sp not in only_vary for sp in pl):
                 if el in at.disp_geo:
                     k = el
                 else:
@@ -78,26 +111,31 @@ def writeSuperposInput(sl, rp, config, param_name="PARAM",
                 ngeo = len(at.disp_geo[k])
                 vibpar = [sp for sp in pl if sp.mode == "vib"]
                 if len(vibpar) > 0:
-                    vibind = config[rp.searchpars.index(vibpar[0])] - 1
+                    if not for_error:
+                        vibind = config[rp.searchpars.index(vibpar[0])] - 1
+                    else:
+                        vibind = vibpar[0].center - 1
                 else:
                     vibind = 0
                 geopar = [sp for sp in pl if sp.mode == "geo"]
                 if len(geopar) > 0:
-                    geoind = config[rp.searchpars.index(geopar[0])] - 1
+                    if not for_error:
+                        geoind = config[rp.searchpars.index(geopar[0])] - 1
+                    else:
+                        geoind = geopar[0].center - 1
                 else:
                     geoind = 0
                 # cast vib and geo indices from 2D to 1D:
-                indices.append([(ngeo * vibind) + geoind + 1])
+                indices.append([(ngeo * vibind) + geoind + 1] * n_var)
             else:
-                if pl[0].steps == 1:
-                    indices.append([1] * n_var)
-                elif pl[0].steps == n_var:
+                whichpar = max(pl, key=lambda x: x.steps)
+                if whichpar.steps == n_var:
                     indices.append(list(range(1, n_var + 1)))
                 else:
                     logger.error("Superpos for error: Inconsistent number of "
                                  "variation steps. Found maximum {} steps,"
                                  " but only {} for {}."
-                                 .format(n_var, pl[0].steps, at))
+                                 .format(n_var, whichpar.steps, at))
                     rp.setHaltingLevel(2)
                     return ""
         vp = [sp for sp in sps if sp.el == "vac"]