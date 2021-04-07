58def362f6de00b916b4e734fbaf66a63f07c634
michele-riva <riva@iap.tuwien.ac.at>
2021-04-07 18:26:13 +0200

    Update guilib.leedsim.__init__
    Reorder some imports

--- guilib/leedsim/__init__.py
+++ guilib/leedsim/__init__.py
@@ -1,4 +1,5 @@
-"""
+"""Module guilib of viperleed.
+
 ======================================
   ViPErLEED Graphical User Interface
 ======================================
@@ -9,23 +10,22 @@ properly indexing an experimental LEED pattern
 """
 
 from viperleed.vprglobals import GLOBALS
-# from vprglobals import GLOBALS
 
-__all__ = ['LEEDSymmetryDomains', 'LEEDsubpattern', 'Woods', 'LEEDPattern',
+__all__ = ['LEEDSymmetryDomains',
+           # 'LEEDsubpattern',  # OLD
+           'Woods', 'LEEDPattern',
            'RealSpace', 'export_pattern_csv', 'LEEDParameters',
            'LEEDParametersList', 'LEEDEquivalentBeams', 'LEEDStructuralDomains']
 
 from viperleed.guilib.leedsim.classes import (LEEDEquivalentBeams,    # not sure I need this to be exposed
                                               LEEDSymmetryDomains,    # not sure I need this to be exposed
-                                              LEEDsubpattern, Woods,  # not sure I need these to be exposed, there were also mpl_colors and degrees
-                                              LEEDPattern, RealSpace,
-                                              LEEDStructuralDomains)  # not sure I need this to be exposed
+                                              LEEDStructuralDomains,  # not sure I need this to be exposed
+                                              # LEEDsubpattern,         # OLD
+                                              Woods,                  # not sure I need these to be exposed
+                                              LEEDPattern, RealSpace)
 from viperleed.guilib.leedsim.exportcsv import export_pattern_csv
 from viperleed.guilib.leedsim.leedparameters import (LEEDParameters,
                                                      LEEDParametersList)
-# from .classes import *
-# from .exportcsv import export_pattern_csv
-# from .leedparameters import *
 if GLOBALS['USE_GUI']:
     from viperleed.guilib.leedsim.widgets import (DomsBlock, EnergyBlock,
                                                   RotationBlock, ToggleButton,
@@ -35,10 +35,6 @@ if GLOBALS['USE_GUI']:
     from viperleed.guilib.leedsim.mainwindow import LEED_GUI
     from viperleed.guilib.leedsim.newfiledialog import NewFileDialog
     from viperleed.guilib.leedsim.exportcsvdialog import ExportCSVDialog
-    # from .widgets import *
-    # from .mainwindow import LEED_GUI
-    # from .NewFileDialog import NewFileDialog
-    # from .ExportCSVDialog import ExportCSVDialog
     __all__.extend(['DomsBlock', 'EnergyBlock', 'RotationBlock', 'ToggleButton',
                     'LEEDCanvas', 'RealCanvas', 'HoverAnnot', 'MatricesPopup',
                     'TEST', 'LEED_GUI', 'NewFileDialog', 'ExportCSVDialog'])
