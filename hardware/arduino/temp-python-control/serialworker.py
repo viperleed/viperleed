05474be8bc459d514ef6eda34f681473343931e0
michele-riva <riva@iap.tuwien.ac.at>
2021-07-01 09:13:42 +0200

    hardware: BaseSerialWorker no QThread parent
    Make BaseSerialWorker just an object subclass rather than a QThred subclass, as this seems to be discouraged according to the Qt documentation (https://doc.qt.io/qt-5/qthread.html), especially since we are clearly defining new slots here.
    
    Other minor edits: some docstring & TODO comments

--- hardware/arduino/temp-python-control/serialworker.py
+++ hardware/arduino/temp-python-control/serialworker.py
@@ -56,7 +56,14 @@ SERIAL_ERROR_MESSAGES = {
 }
 
 
-class ExtraSerialErrors(enum.IntEnum):
+# TODO: perhaps nicer to have this just an Enum subclass and have tuples
+#       where the first element is the code and the second one the message.
+#       This way we can have consistent descriptions all in one place.
+#       Perhaps we should also explicitly have (tuple, Enum) inheritance.
+#       Actually even better, have a base ViPErLEEDErrorEnum class with
+#       no members that inherits tuple, and that can be used as a parent
+#       class for all ViPErLEED errors!
+class ExtraSerialErrors(enum.Enum):
     """Data class for basic serial errors not available in QSerialPort."""
     NO_MESSAGE_ERROR = 50
     NO_START_MARKER_ERROR = 51
@@ -64,19 +71,14 @@ class ExtraSerialErrors(enum.IntEnum):
     UNSUPPORTED_COMMAND_ERROR = 53
 
 
-class BaseSerialWorker(qtc.QThread):
-    """Base calss for serial communication for a ViPErLEED controller.
-
-    Serial communcation happens in its own thread. Use .run() to
-    start the thread, before calling any of its methods.                TODO: check this is true
-    """
+class BaseSerialWorker:
+    """Base class for serial communication for a ViPErLEED controller."""
 
     error_occurred = qtc.pyqtSignal(int, str)
-    data_received = qtc.pyqtSignal(tuple)
+    data_received = qtc.pyqtSignal(object)
 
-    def __init__(self, parent=None, port_name='', settings=None):
+    def __init__(self, port_name='', settings=None):
         """Initialize window."""
-        super().__init__(parent)
         self.__port = qts.QSerialPort(port_name)
 
         if settings is None:
@@ -106,9 +108,11 @@ class BaseSerialWorker(qtc.QThread):
 
     @port_name.setter
     def port_name(self, new_port_name):
-        """Set a nw port name.
+        """Set a new port name.
 
-        This will disconnect already an open port.
+        This will disconnect an already-open port, but will not
+        connect to the port with the newly set name. Explicitly
+        use .connect() after changing .port_settings, if needed.
 
         Parameters
         ----------
@@ -120,9 +124,23 @@ class BaseSerialWorker(qtc.QThread):
         Returns
         -------
         None.
+
+        Raises
+        ------
+        TypeError
+            If new_port_name is neither 'str' nor 'QSerialPortInfo'
         """
         self.__port.close()
-        self.__port = qts.QSerialPort(new_port_name)
+        if isinstance(new_port_name, str):
+            self.__port.setPortName(new_port_name)
+        elif isinstance(new_port_name, qts.QSerialPortInfo):
+            self.__port.setPort(new_port_name)
+        else:
+            raise TypeError(
+                f"{self.__class__.__name__}: Invalid port "
+                f"name type {type(new_port_name).__name__!r}. "
+                "Should be 'str' or 'QSerialPortInfo'"
+                )
 
     @property
     def port_settings(self):
@@ -135,32 +153,75 @@ class BaseSerialWorker(qtc.QThread):
 
         Parameters
         ----------
-        new_settings : ConfigParser
+        new_settings : dict or ConfigParser
             Configuration of port. It must have a 'serial_port_settings'
-            section. The section must contain also BYTE_ORDER and
-            MSG_END options.
+            section.
+            The following fields in new_settings['serial_port_settings']
+            will be used (no quotes if using a ConfigParser!):
+            BYTE_ORDER : str, {'big', 'little'}
+                Order in which bytes are transmitted. 'big' means
+                most-significant-byte first.
+            MSG_END : bytes
+                Byte to be used as a marker for the end of a message
+            MSG_START : bytes, optional
+                Byte to be used as a marker for the beginning of a
+                message. Default is no start marker.
+            'baud_rate': number
+                Baud rate for the transmission. Will be interpreted
+                as an integer.
+            'break_enabled' : bool, optional
+                Whether the serial device makes use of the break
+                condition for communication. Default is False.
+            'data_bits' : int, optional
+                Number of data bits per data packet. Default is 8.
+            'data_terminal_ready' : bool, optional
+                Whether the Data Terminal Ready (DTR) line should
+                be held high or not for the device to communicate.
+                Default is False.
+            'flow_control' : {'NoFlowControl', 'HardwareControl',
+                              'SoftwareControl'}, optional
+                The type of flow control used by the device. Default
+                is NoFlowControl.
+            'parity' : {'NoParity', 'EvenParity', 'OddParity',
+                        'SpaceParity', 'MarkParity'}, optional
+                Which parity bits are used for each data packet.
+                Default is NoParity.
+            'request_to_send' : bool, optional
+                Whether the Request To Send (RTS) line should be held
+                high or not for communication. This option has no effect
+                if 'flow_control' is set to 'HardwareControl'. Default
+                is False.
+            'stop_bits' : int, optional
+                Number of bits used as stop bits for each data packet.
+                Default is 1.
 
         Raises
         ------
         TypeError
-            If new_settings is not a ConfigParser
+            If new_settings is neither a dict nor a ConfigParser
         ValueError
             If new_settings does not contain a 'serial_port_settings'
             section, of if the section does not contain the mandatory
             options MSG_END and BYTE_ORDER.
         """
-        if not isinstance(new_settings, ConfigParser):
-            raise TypeError(f"{self.__class__.__name__}: settings "
-                            "should be a ConfigParser, not "
-                            f"{type(new_settings).__name__}")
+        if not isinstance(new_settings, (dict, ConfigParser)):
+            raise TypeError(
+                f"{self.__class__.__name__}: invalid type "
+                f"{type(new_settings).__name__} for settings. "
+                "Should be 'dict' or 'ConfigParser'."
+                )
         if 'serial_port_settings' not in new_settings:
-            raise ValueError(f"{self.__class__.__name__}: "
-                             "settings should must contain a "
-                             "'serial_port_settings' section")
+            raise ValueError(
+                f"{self.__class__.__name__}: settings must "
+                "contain a 'serial_port_settings' section"
+                )
         if any(key not in new_settings['serial_port_settings']
                for key in ('MSG_END', 'BYTE_ORDER')):
-            raise ValueError(f"{self.__class__.__name__}: settings should "
-                             "contain both 'MSG_END' and 'BYTE_ORDER' options")
+            raise ValueError(
+                f"{self.__class__.__name__}: section 'serial_port_settings' "
+                "of the settings should contain both 'MSG_END' and "
+                "'BYTE_ORDER' fields."
+                )
 
         self.__serial_settings = new_settings
         self.__byte_order = new_settings.get('serial_port_settings',
@@ -180,8 +241,8 @@ class BaseSerialWorker(qtc.QThread):
     def clear_errors(self):
         """Clear all errors.
 
-        This method must be called in the reimplementation of identify_error
-        after the error has been correctly identified.
+        This method must be called in the reimplementation of
+        identify_error after the error has been correctly identified.
         """
         self.__port.clearErrors()
         self._timeout.stop()
@@ -212,18 +273,25 @@ class BaseSerialWorker(qtc.QThread):
         subclasses should reimplement this function and emit a signal
         self.error_occurred(code, text) where code is the error code
         and text is an informative message that will be displayed as
-        a message box in the GUI.
+        a message box in the GUI. It is advisable to define new error
+        codes in a specific enum.Enum subclass.
 
         Error identification means picking the correct message(s)
         that contain information about the error. This information
-        max be contained in the error message itself, or may come
-        as data messages following the error message.
+        may be contained in the error message itself, or may come
+        as data messages following the error message. All messages
+        that came after an error message are discarded after
+        identify_error() successfully identifies error information.
+        New messages can be processed only if identify_error() is
+        successfully completed, i.e., is calls clear_errors().
 
         The function should be a no-op if identification of the error
         should fail because the data did not arrive yet.
 
-        After an error has been correctly identified, call the
-        clear_errors() method.
+        The reimplementation must call the clear_errors() method to
+        be considered successfully completed. It makes sense to call
+        clear_errors() only after an error has been successfully
+        identified (or if no error information is expected).
 
         Parameters
         ----------
@@ -232,6 +300,8 @@ class BaseSerialWorker(qtc.QThread):
             message was received, including the error message itself.
             messages_since_error[0] is the error message. Each message
             is a bytes object, containing the already decoded message.
+            All messages_since_error will be discarded after the error
+            has been identified.
 
         Returns
         -------
@@ -244,7 +314,7 @@ class BaseSerialWorker(qtc.QThread):
 
         Emits
         -----
-        error_occurred
+        error_occurred(error_code, descriptive_text)
             After the error has been identified
         """
         raise NotImplementedError(
@@ -275,26 +345,26 @@ class BaseSerialWorker(qtc.QThread):
 
     def is_message_supported(self, message):
         """Check whether message is a supported command.
-        
+
         This method is guaranteed to be called exactly once
         during send_message() right before actually sending
         the message, and before encoding(). The message will
         be encoded and sent only if this method returns True.
-        
+
         This method can be reimplemented to (i) keep track of
-        the last´'request' command sent, right before sending
+        the last 'request' command sent, right before sending
         it, and (ii) check whether the command requested is one
         of those supported by the controller, and/or (iii) the
         syntax of the command is acceptable.
-        
+
         The base implementation always returns True.
-        
+
         Parameters
         ----------
         message : object
             The same message given to self.send_message(), right
             before encoding and sending to the device
-        
+
         Returns
         -------
         message_acceptable : bool
@@ -340,15 +410,15 @@ class BaseSerialWorker(qtc.QThread):
             a bytes/bytearray, a number, or a string.
         timeout : int, optional
             Maximum amount of time in milliseconds that the worker
-            will wait before emitting  timeout error. If negative,
+            will wait before emitting a timeout error. If negative,
             it will never time out. A timeout of zero will cause
             an immediate time out. Default is -1.
 
         Returns
         -------
-        None
+        None.
 
-        Emits
+        Emits       # TODO: probably nothing actually!
         -----
         error_occurred
             If message could not be sent
@@ -361,7 +431,7 @@ class BaseSerialWorker(qtc.QThread):
             return
         self.__port.write(self.encode(message))
         # TODO: the error that we're handling below should already
-        # be tken care of by __on_serial_error, but needs checking!
+        # be taken care of by __on_serial_error, but needs checking!
         # if self.__port.write(self.encode(message)) < 0:
             # self.error_occurred.emit(
                 # "Error sending message. Check communication and port settings?"
@@ -395,19 +465,25 @@ class BaseSerialWorker(qtc.QThread):
         message : bytes or bytearray
             The message to be checked. Integrity checks only involve
             making sure that the message is not empty before and after
-            removal of a start marker if used.
+            removal of a start marker if used. If a start marker is
+            used (i.e., a MSG_START is present in self.port_settings),
+            the method considers the message valid only if the first
+            character is a MSG_START.
 
         Returns
         -------
         processed_message : bytes or bytearray or None
-            Message stripped of the start_marker, if used, otherwise
+            Message stripped of the MSG_START marker, if used, otherwise
             the message is returned unchanged. Returns None if message
             is invalid.
 
         Emits
         -----
-        error_occurred
-            If message is invalid
+        error_occurred(ExtraSerialErrors.NO_MESSAGE_ERROR, msg)
+            If message is empty (before or after removal of MSG_START
+        error_occurred(ExtraSerialErrors.NO_START_MARKER_ERROR)
+            If the first byte of message is not MSG_START, if there
+            is a MSG_START in self.port_settings.
         """
         if not message:
             self.error_occurred.emit(
@@ -468,14 +544,35 @@ class BaseSerialWorker(qtc.QThread):
         self.process_messages()
 
     def __on_serial_error(self, error_code):
-        """React to a serial-port error."""
+        """React to a serial-port error.
+
+        Parameters
+        ----------
+        error_code : one of QSerialPort.SerialPortError
+            The error code of the serial port
+
+        Emits
+        -----
+        error_occurred(error_code, msg)
+            Always
+        """
         error_msg = SERIAL_ERROR_MESSAGES[error_code].format(self.port_name)
         self.error_occurred.emit(error_code, error_msg)
 
         print(error_msg)
 
     def __on_serial_timeout(self):
-        """React on a serial timeout."""
+        """React on a serial timeout.
+
+        Returns
+        -------
+        None.
+
+        Emits
+        -----
+        error_occurred(ExtraSerialErrors.TIMEOUT_ERROR, msg)
+            Always
+        """
         self.error_occurred.emit(
             ExtraSerialErrors.TIMEOUT_ERROR,
             "Serial Timeout: No message received in the last"
@@ -521,11 +618,12 @@ class BaseSerialWorker(qtc.QThread):
             )
         self.__port.setParity(parity)
 
-        self.__port.setRequestToSend(
-            settings.getbool('serial_port_settings',
-                             'request_to_send',
-                             self.__port.isRequestToSend())
-            )
+        if flow_control is not self.__port.HardwareControl:
+            self.__port.setRequestToSend(
+                settings.getbool('serial_port_settings',
+                                 'request_to_send',
+                                 self.__port.isRequestToSend())
+                )
         self.__port.setStopBits(
             settings.getint('serial_port_settings',
                             'stop_bits',
