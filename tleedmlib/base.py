fix: sign error in symmetry linking, refactor rotation matrices
There was an inconsistency in the definitions of rotation matrices, causing incorrect rotation of the displacement vectors for some systems. Fixed, and hopefully improved overall robustness by refactoring the code such that rotation matrices are never defined in place, but access the same definition in base.py every time.

Found through test case: radial displacements on Te/Ir100(2x2), p4m symmetry. Should still be tested more thoroughly on three-fold symmetric systems.

tleedmlib/base.py
@@ -102,29 +102,22 @@ class BackwardsReader:
 #                FUNCTIONS                    #
 ###############################################
 
-def rotMatrix(order):
+def rotation_matrix(angle, dim=2):
     """Returns a (2x2) matrix for in-plane rotation of the given rotation
-    order."""
-    # these explicit definitions are likely useless, but sqrts might be
-    #  marginally more accurate than sin/cos
-    if order == 2:
-        return np.array([[-1, 0], [0, -1]])
-    elif order == -3:
-        return np.array([[-0.5, -np.sqrt(3)/2], [np.sqrt(3)/2, -0.5]])
-    elif order == 3:
-        return np.array([[-0.5, np.sqrt(3)/2], [-np.sqrt(3)/2, -0.5]])
-    elif order == -4:
-        return np.array([[0, 1], [-1, 0]])
-    elif order == 4:
-        return np.array([[0, -1], [1, 0]])
-    elif order == -6:
-        return np.array([[0.5, np.sqrt(3)/2], [-np.sqrt(3)/2, 0.5]])
-    elif order == 6:
-        return np.array([[0.5, -np.sqrt(3)/2], [np.sqrt(3)/2, 0.5]])
-    else:
-        angle = 2*np.pi/order
-        return np.array([[np.cos(angle), np.sin(angle)],
-                         [-np.sin(angle), np.cos(angle)]])
+    angle. Set dim=3 to get a 3x3 matrix with rotation in [:2, :2]."""
+    if dim < 2:
+        raise ValueError("Dimension matrix needs at least dimension 2")
+    m = np.eye(dim, dtype=float)
+    m[:2, :2] = np.array([[np.cos(angle), -np.sin(angle)],
+                          [np.sin(angle), np.cos(angle)]])
+    return m
+
+
+def rotation_matrix_order(order, dim=2):
+    """Returns a (2x2) matrix for in-plane rotation of the given rotation
+    order. Set dim=3 to get a 3x3 matrix with rotation in [:2, :2]."""
+    angle = 2*np.pi/order
+    return rotation_matrix(angle, dim=dim)
 
 
 def fortranContLine(s):

tleedmlib/classes/slab.py
@@ -21,7 +21,8 @@ try:
 except Exception:
     has_ase = False
 
-from viperleed.tleedmlib.base import (angle, rotMatrix, dist_from_line)
+from viperleed.tleedmlib.base import (angle, rotation_matrix_order,
+                                      rotation_matrix, dist_from_line)
 from viperleed.tleedmlib.classes.atom import Atom
 import viperleed.tleedmlib as tl
 # from tleedmlib import DEFAULT
@@ -681,7 +682,7 @@ class Slab:
         applies an order-fold rotation matrix to the atom positions, then
         translates back"""
         self.getCartesianCoordinates()
-        m = rotMatrix(order)
+        m = rotation_matrix_order(order)
         for at in self.atlist:
             # translate origin to candidate point, rotate, translate back
             at.cartpos[0:2] = np.dot(m, at.cartpos[0:2] - axis) + axis
@@ -692,7 +693,7 @@ class Slab:
         the same. Note that this rotates in the opposite direction as
         rotateAtoms."""
         self.getCartesianCoordinates()
-        m = rotMatrix(order)
+        m = rotation_matrix_order(order)
         m3 = np.identity(3, dtype=float)
         m3[:2, :2] = m
         self.ucell = np.dot(m3, self.ucell)
@@ -704,9 +705,8 @@ class Slab:
         """Translates the atoms in the slab to have the symplane in the
         origin, applies a mirror or glide matrix, then translates back.
         Very inefficient implementation!"""
-        ang = angle(np.array([1, 0]), symplane.dir)
-        rotm = np.array([[np.cos(ang), np.sin(ang)],
-                         [-np.sin(ang), np.cos(ang)]])
+        ang = angle(symplane.dir, np.array([1, 0]))
+        rotm = rotation_matrix(ang)
         rotmirm = np.dot(np.linalg.inv(rotm),
                          np.dot(np.array([[1, 0], [0, -1]]), rotm))
         # rotates to have plane in x direction, mirrors on x
@@ -734,7 +734,7 @@ class Slab:
     def isRotationSymmetric(self, axis, order, eps):
         """Evaluates whether the slab is equivalent to itself when rotated
         around the axis with the given rotational order"""
-        m = rotMatrix(order)
+        m = rotation_matrix_order(order)
         ab = self.ucell[:2, :2]
         abt = ab.T
         releps = [eps / np.linalg.norm(abt[j]) for j in range(0, 2)]
@@ -934,17 +934,15 @@ class Slab:
     def isBulkScrewSymmetric(self, order, sldisp, eps):
         """Evaluates whether the slab has a screw axis of the given order when
         translated by the given number of sublayers."""
-        m = np.identity(3, dtype=float)
-        m[:2, :2] = rotMatrix(order)
+        m = rotation_matrix_order(order, dim=3)
         return self.isBulkTransformSymmetric(m, sldisp, eps)
 
     def isBulkGlideSymmetric(self, symplane, sldisp, eps):
         """Evaluates whether the bulk has a glide plane along a given
         direction, i.e. mirror at this direction, then some translation."""
         m = np.identity(3, dtype=float)
-        ang = angle(np.array([1, 0]), symplane.dir)
-        rotm = np.array([[np.cos(ang), np.sin(ang)],
-                         [-np.sin(ang), np.cos(ang)]])
+        ang = angle(symplane.dir, np.array([1, 0]))
+        rotm = rotation_matrix(ang)
         m[:2, :2] = np.dot(np.linalg.inv(rotm),
                            np.dot(np.array([[1, 0], [0, -1]]), rotm))
         return self.isBulkTransformSymmetric(m, sldisp, eps)
@@ -952,9 +950,8 @@ class Slab:
     def isMirrorSymmetric(self, symplane, eps, glide=False):
         """Evaluates whether the slab is equivalent to itself when applying a
         mirror or glide operation at a given plane"""
-        ang = angle(np.array([1, 0]), symplane.dir)
-        rotm = np.array([[np.cos(ang), np.sin(ang)],
-                         [-np.sin(ang), np.cos(ang)]])
+        ang = angle(symplane.dir, np.array([1, 0]))
+        rotm = rotation_matrix(ang)
         rotmirm = np.dot(np.linalg.inv(rotm),
                          np.dot(np.array([[1, 0], [0, -1]]), rotm))
         # rotates to have plane in x direction, mirrors on x

tleedmlib/sections/initialization.py
@@ -15,7 +15,7 @@ import copy
 import numpy as np
 
 import viperleed.tleedmlib as tl
-from viperleed.tleedmlib.base import angle
+from viperleed.tleedmlib.base import angle, rotation_matrix
 from viperleed.tleedmlib.beamgen import runBeamGen
 from viperleed.tleedmlib.psgen import runPhaseshiftGen
 from viperleed.tleedmlib.files.poscar import readPOSCAR, writeCONTCAR
@@ -488,12 +488,8 @@ def init_domains(rp):
             logger.info("Bulk unit cells of domain {0} and domain {1} are "
                         "mismatched, but can be matched by rotating domain "
                         "{1}.".format(rp.domainParams[0].name, dp.name))
-            ang = angle(bulkuc[0], bulkuc0[0])
-            rotm = np.array([[np.cos(ang), np.sin(ang)],
-                             [-np.sin(ang), np.cos(ang)]])
-            rotm = np.identity(3)
-            rotm[:2, :2] = np.array([[np.cos(ang), np.sin(ang)],
-                                    [-np.sin(ang), np.cos(ang)]])
+            ang = angle(bulkuc0[0], bulkuc[0])
+            rotm = rotation_matrix(ang, dim=3)
             rotm_t = np.transpose(rotm)
             # dp.sl.ucell = np.transpose(np.dot(np.transpose(dp.sl.ucell),
             #                                   rotuc))

tleedmlib/symmetry.py
@@ -15,7 +15,8 @@ import scipy.spatial as sps
 import itertools
 
 import viperleed.tleedmlib as tl
-from viperleed.tleedmlib.base import angle
+from viperleed.tleedmlib.base import (angle,
+                                      rotation_matrix_order, rotation_matrix)
 from viperleed.tleedmlib.classes.slab import SymPlane
 from viperleed.tleedmlib.files.parameters import modifyPARAMETERS
 
@@ -987,9 +988,7 @@ def enforceSymmetry(sl, rp, planegroup="fromslab",
             tmpslab = copy.deepcopy(sl)
             tmpslab.rotateAtoms(np.array([0, 0]), toprotsym)
             tmpslab.collapseCartesianCoordinates()
-            ang = 2*np.pi/toprotsym
-            m = np.array([[np.cos(ang), -np.sin(ang)],
-                          [np.sin(ang), np.cos(ang)]])
+            m = np.linalg.inv(rotation_matrix_order(toprotsym))
             for (sli, sl1) in enumerate(sl.sublayers):
                 for (ati, at1) in enumerate(sl1.atlist):
                     for (atj, at2) in enumerate(tmpslab.sublayers[sli]
@@ -1035,10 +1034,9 @@ def enforceSymmetry(sl, rp, planegroup="fromslab",
             tmpslab.mirror(testplane, glide=g)
             tmpslab.collapseCartesianCoordinates()
             ang = angle(np.array([1, 0]), testplane.dir)
-            rotm = np.array([[np.cos(ang), np.sin(ang)],
-                             [-np.sin(ang), np.cos(ang)]])
-            m = np.dot(np.linalg.inv(rotm), np.dot(np.array([[1, 0], [0, -1]]),
-                                                   rotm))
+            rotm = rotation_matrix(ang)
+            m = np.dot(rotm, np.dot(np.array([[1, 0], [0, -1]]),
+                                                   np.linalg.inv(rotm)))
             for (sli, sl1) in enumerate(sl.sublayers):
                 for (ati, at1) in enumerate(sl1.atlist):
                     for (atj, at2) in enumerate(tmpslab.sublayers[sli]
@@ -1283,9 +1281,7 @@ def enforceSymmetry(sl, rp, planegroup="fromslab",
                     found = True
         if not found:
             ang = angle(np.array([1, 0]), mirrordirs[0])
-            rotm = np.array([[np.cos(ang), np.sin(ang), 0],
-                             [-np.sin(ang), np.cos(ang), 0],
-                             [0, 0, 1]])
+            rotm = rotation_matrix(ang, dim=3)
             sl.ucell = np.dot(rotm, sl.ucell)
             for i in range(0, 3):
                 for j in range(0, 3):