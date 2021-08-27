9eee496199fc4c35697763bc545b404880989653
Florian Kraushofer <66359721+fkraushofer@users.noreply.github.com>
2021-08-27 17:43:07 +0200

    feat: Compatibility for TensErLEED v1.7 (1)
    Implement new formatting for beamgen.v1.7

--- tleedmlib/beamgen.py
+++ tleedmlib/beamgen.py
@@ -14,47 +14,53 @@ from viperleed import fortranformat as ff
 logger = logging.getLogger("tleedm.beamgen")
 
 
-def runBeamGen(sl, rp, beamgensource=os.path.join('tensorleed',
-                                                  'beamgen3.out'),
-               domains=False):
+def runBeamGen(sl, rp, beamgensource='', domains=False):
     """Writes necessary input for the beamgen3 code, the runs it. The relevant
     output file will be renamed to BEAMLIST."""
+    if rp.TL_VERSION < 1.7:
+        beamgensource = os.path.join('tensorleed', 'beamgen3.out')
+    else:
+        beamgensource = os.path.join('tensorleed', 'beamgen.v1.7')
     beamgensource = os.path.join(rp.sourcedir, beamgensource)
     output = ''
-    f74x2 = ff.FortranRecordWriter('2F7.4')
+    if rp.TL_VERSION < 1.7:
+        formatter = {'uc': ff.FortranRecordWriter('2F7.4'),
+                     'latmat': ff.FortranRecordWriter('2I3'),
+                     'emax': ff.FortranRecordWriter('F7.1'),
+                     'dmin': ff.FortranRecordWriter('F4.1'),
+                     'tst': ff.FortranRecordWriter('F11.6'),
+                     }
+    else:
+        formatter = {'uc': ff.FortranRecordWriter('2F9.4'),
+                     'latmat': ff.FortranRecordWriter('2I4'),
+                     'emax': ff.FortranRecordWriter('F6.1'),
+                     'dmin': ff.FortranRecordWriter('F7.4'),
+                     'tst': ff.FortranRecordWriter('F8.6'),
+                     }
     if sl.bulkslab is None:
         sl.bulkslab = sl.makeBulkSlab(rp)
     ucbulk = np.transpose(sl.bulkslab.ucell[:2, :2])
-    ol = f74x2.write(ucbulk[0])
-    ol = ol.ljust(36)
-    output += ol + 'ARA1\n'
-    ol = f74x2.write(ucbulk[1])
-    ol = ol.ljust(36)
-    output += ol + 'ARA2\n'
-    i3x2 = ff.FortranRecordWriter('2I3')
-    ol = i3x2.write([int(round(f)) for f in rp.SUPERLATTICE[0]])
-    ol = ol.ljust(36)
-    output += ol + 'LATMAT - overlayer\n'
-    ol = i3x2.write([int(round(f)) for f in rp.SUPERLATTICE[1]])
-    ol = ol.ljust(36)
-    output += ol + 'LATMAT -  matrix\n'
-    output += ('  1                                 SSYM - symmetry code - cf.'
-               ' van Hove / Tong 1979, always 1\n')
+    output += formatter['uc'].write(ucbulk[0]).ljust(36) + 'ARA1\n'
+    output += formatter['uc'].write(ucbulk[1]).ljust(36) + 'ARA2\n'
+    ol = formatter['latmat'].write([int(round(f)) for f in rp.SUPERLATTICE[0]])
+    output += ol.ljust(36) + 'LATMAT - overlayer\n'
+    ol = formatter['latmat'].write([int(round(f)) for f in rp.SUPERLATTICE[1]])
+    output += ol.ljust(36) + 'LATMAT -  matrix\n'
+    if rp.TL_VERSION < 1.7:
+        output += ('  1                                 SSYM - symmetry code'
+                   ' - cf. van Hove / Tong 1979, always 1\n')
     if not domains:
         dmin = sl.getMinLayerSpacing() * 0.7
     else:
         dmin = min([dp.sl.getMinLayerSpacing() for dp in rp.domainParams])*0.7
-    f71 = ff.FortranRecordWriter('F7.1')
-    f41 = ff.FortranRecordWriter('F4.1')
-    ol = f71.write([rp.THEO_ENERGIES[1]]) + f41.write([dmin])
-    ol = ol.ljust(36)
+    ol = (formatter['emax'].write([rp.THEO_ENERGIES[1]])
+          + formatter['dmin'].write([dmin])).ljust(36)
     output += (ol + 'EMAX,DMIN - max. energy, min. interlayer distance for '
                'layer doubling\n')
-    output += ('   {:.4f}                           TST - convergence '
-               'criterion for fd. reference calculation\n'
-               .format(rp.ATTENUATION_EPS))
-    output += ('9999                                KNBMAX - max. number of '
-               'beams to be written (may be a format problem!)')
+    output += (formatter['tst'].write([rp.ATTENUATION_EPS]).ljust(36)
+               + 'TST - convergence criterion for fd. reference calculation\n')
+    output += ('99999                               KNBMAX - max. number of '
+               'beams to be written')
     try:
         with open('DATA', 'w') as wf:
             wf.write(output)
