253e986084d9a09d047eb0099ae5cb37fd7bb102
FlorianDoerr <79530483+FlorianDoerr@users.noreply.github.com>
2021-09-13 13:51:19 +0200

    hardware: mandatory settings for controller reworked
    primary controller will now know that it needs additional mandatory settings
    settings are not optional during instantiation anymore

--- guilib/measure/controller/controllerabc.py
+++ guilib/measure/controller/controllerabc.py
@@ -47,19 +47,53 @@ class ControllerABC(qtc.QObject, metaclass=QMetaABC):
 
     _mandatory_settings = [
         ('controller', 'serial_port_class'),
+        ('available_commands')
         ]
     controller_busy = qtc.pyqtSignal(bool)
 
-    def __init__(self, settings=None, port_name='', sets_energy=False):
-        """Initialize the controller instance."""
+    # def __init__(self, settings=None, port_name='', sets_energy=False):
+    def __init__(self, settings, port_name='', sets_energy=False):                # NOTE: we will need a settings file that remember which devices were connected to which port
+        """Initialize the controller instance.
+        
+        Parameters
+        ----------
+        settings : ConfigParser
+            The controller settings
+        port_name : str, optional
+            Name of the serial port to be used to communicate with
+            the controller. This parameter is optional only in case
+            settings contains a 'controller'/'port_name' option. If
+            this is given, it will also be stored in the settings
+            file, overriding the value that may be there. Default is
+            an empty string.
+        sets_energy : bool, optional
+            Used to determine whether this controller is responsible
+            for setting the electron energy by communicating with the
+            LEED optics. Only one controller may be setting the energy.
+            Default is False.
+        
+        Raises
+        ------
+        TypeError
+            If no port_name is given, and none was present in the
+            settings file.
+        """
 
-        # Switches the controller from set_energy() to measure_now().
         self.__sets_energy = sets_energy
-        self.__port_name = port_name
         self.__settings = None
         self.__serial = None
 
-        self.settings = settings
+        if not port_name:
+            if not settings.has_option('controller', 'port_name'):
+                raise TypeError("No port name given, and none found in the "
+                                "configuration file. Cannot instantiate "
+                                "a controller without a valid port.")
+            port_name = settings.get('controller', 'port_name')
+        else:
+            settings.set('controller', 'port_name', port_name)
+        self.__port_name = port_name
+
+        self.set_settings(settings)
 
         # Is used to determine if the next step
         # in the measurement cycle can be done.
@@ -90,11 +124,13 @@ class ControllerABC(qtc.QObject, metaclass=QMetaABC):
         """Return the serial port instance used."""
         return self.__serial
 
-    def __get_sets_energy(self):
+    @property
+    def sets_energy(self):
         """Return whether the controller sets the energy."""
         return self.__sets_energy
 
-    def set_sets_energy(self, energy_setter):
+    @set_energy.setter
+    def sets_energy(self, energy_setter):
         """Set the serial to controls energy True/False.
 
         Parameters
@@ -103,8 +139,6 @@ class ControllerABC(qtc.QObject, metaclass=QMetaABC):
             True if the controller sets the energy.
         """
         self.__sets_energy = bool(energy_setter)
-
-    sets_energy = property(__get_sets_energy, set_sets_energy)
 
     def __get_settings(self):
         """Return the current settings used as a ConfigParser."""
@@ -126,16 +160,25 @@ class ControllerABC(qtc.QObject, metaclass=QMetaABC):
             emit_error(self, ControllerErrors.MISSING_SETTINGS)
             return
 
+        # The next extra setting is mandatory only for a controller
+        # that sets the LEED energy on the optics
+        extra_mandatory = ('measurement_settings', 'settle_time')
+        if (self.sets_energy
+                and extra_mandatory not in self._mandatory_settings):
+            self._mandatory_settings.append(extra_mandatory)
+
         new_settings, invalid = config_has_sections_and_options(
             self, new_settings,
             self._mandatory_settings
             )
-        for setting in invalid:
-            emit_error(self, ControllerErrors.INVALID_CONTROLLER_SETTINGS,
-                       setting)
 
         if invalid:
+            error_msg = ', '.join(invalid)
+            emit_error(self, ControllerErrors.INVALID_CONTROLLER_SETTINGS,
+                       error_msg)
             return
+
+
 
         serial_cls_name = new_settings.get('controller', 'serial_port_class')
         if self.serial.__class__.__name__ != serial_cls_name:
@@ -143,14 +186,19 @@ class ControllerABC(qtc.QObject, metaclass=QMetaABC):
                 serial_class = class_from_name('serial', serial_cls_name)
             except ValueError:
                 emit_error(self, ControllerErrors.INVALID_CONTROLLER_SETTINGS,
-                           setting)
+                           'controller/serial_port_class')
                 return
-            self.__serial = serial_class(settings=new_settings,
+            self.__serial = serial_class(new_settings,
                                          port_name=self.__port_name)
+        else:
+            # The next line will also check that new_settings contains
+            # appropriate settings for the serial port class used.
+            self.serial.port_settings = new_settings
+            self.serial.port_name = self.__port_name
 
-        # The next line will also check that new_settings contains
-        # appropriate settings for the serial port class used
-        self.serial.port_settings = new_settings
+        # Notice that the .connect() will run anyway, even if the
+        # settings are invalid (i.e., missing mandatory fields)!
+        self.serial.connect()
         self.__settings = self.serial.port_settings
 
     settings = property(__get_settings, set_settings)
