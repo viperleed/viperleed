d090854588f352ee4b87ee61f9ee299389a44460
FlorianDoerr <79530483+FlorianDoerr@users.noreply.github.com>
2021-09-17 11:14:06 +0200

    hardware: init bugfixes to make modules accessible

--- guilib/measure/controller/__init__.py
+++ guilib/measure/controller/__init__.py
@@ -1,5 +1,17 @@
-"""
-TLEEDMAP GUI - measure module
-"""
+"""Package controller of viperleed.?????.
 
-# print('You have imported', __name__)
+========================================
+   ViPErLEED Graphical User Interface
+========================================
+
+Created: 2021-07-08
+Author: Michele Riva
+Author: Florian Doerr
+
+This package contains controller classes.
+"""
+import sys
+
+# Here import all the concrete reimplementations of controllerabc.ControllerABC
+# and measurecontrollerabc.MeasureController so that they are available to class_from_name()
+from viperleed.guilib.measure.controller.viperinocontroller import ViPErinoController
