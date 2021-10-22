59c8887ca98b3aa98c79b1498ddde6a9e9a257eb
michele-riva <riva@iap.tuwien.ac.at>
2021-10-22 08:49:05 +0200

    imgsource: import correct driver for platform
    For now only Windows is supported.

--- guilib/measure/camera/drivers/imagingsource/__init__.py
+++ guilib/measure/camera/drivers/imagingsource/__init__.py
@@ -10,3 +10,17 @@ Author: Florian Doerr
 
 This module contains drivers for cameras produced by The Imaging Source.
 """
+
+import os
+from viperleed.guilib.measure.camera.drivers.imagingsource.tisgrabber import (
+    WindowsCamera, FrameReadyCallbackType, SinkFormat
+    )
+from viperleed.guilib.measure.camera.drivers.imagingsource.winerrors import (
+    ImagingSourceError
+    )
+
+if 'nt' in os.name:
+    ISCamera = WindowsCamera
+else:
+    raise EnvironmentError("Imaging Source cameras are unsupported on "
+                           "non-Windows platforms.")
