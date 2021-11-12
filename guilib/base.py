d24b6b857331dbf292ad936da7e1cf6643712fb0
Florian Kraushofer <66359721+fkraushofer@users.noreply.github.com>
2021-11-12 10:39:04 +0100

    fix: Tightens restraints for equality in unit cell type detection
    The current epsilon for unit cell side lengths / angles was too lenient, would detect e.g. 110° as hexagonal.
    
    Epsilon was last modified in commit 4612398ea710e744aa9868782ca5ce7fbaf61ebb - note that it has to be consistent between tleedmlib and guilib.

--- guilib/base.py
+++ guilib/base.py
@@ -1804,7 +1804,7 @@ class Lattice():
         basis = self._basis
 
         # Relative tolerance factor within which things are assumed to be equal
-        eps = 1e-3
+        eps = 1e-4
 
         # cosine of angle between vectors
         cosine = np.dot(basis[0], basis[1])/(np.linalg.norm(basis[0])
