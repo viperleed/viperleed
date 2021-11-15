c947d49fa96080a02fb1781213146e3905a9d9da
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2021-11-15 18:33:57 +0100

    updated syntax

--- tensorleed/new_r-factor/rfactor_example.py
+++ tensorleed/new_r-factor/rfactor_example.py
@@ -1,5 +1,5 @@
 import numpy as np
-import rfactor
+from rfactor import r_factor_new as rf
 import time
 import timeit # dependency for testing! Don't use in Viperleed build
 
@@ -28,7 +28,9 @@ denominator = None
 N_overlapping_points = None
 
 # Test calls
-rfactor.r_factor_beam(y1, size_y1, y2, size_y2, E_start1, E_start2, E_step, V0r_shift,
+print(rf.test_exec(1.0))
+
+rf.r_factor_beam(y1, size_y1, y2, size_y2, E_start1, E_start2, E_step, V0r_shift,
                       R_pendry, numerator, denominator, N_overlapping_points)
 
 print(R_pendry)
