248943eef65396e4199d46fe903982429c0e9f65
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2021-11-16 10:57:27 +0100

    implement simple test cases

--- tensorleed/new_r-factor/rfactor_example.py
+++ tensorleed/new_r-factor/rfactor_example.py
@@ -11,13 +11,12 @@ f2py -c rfactor.pyf rfactor.f90
 
 # Prepare some example arrays with curve data
 
-y1 = []
-size_y1 = 10
-y2 = []
-size_y2 = 10
+y1 = np.array([-1, -0.5 ,0, 0.5, 1, 1.5, 2.5, 1.9,1,0])
+y2_match = np.array([-1, -0.5 ,0, 0.5, 1, 1.5, 2.5, 1.9,1,0])
+y2_bad_match = np.array([-1, -1.5 ,-1, -0.5, 0.5, 1.5, 3.5, 1.9,-2,0])
 
 E_start1 = 20.0
-E_start2 = 20.0
+E_start2 = 22.0
 E_step = 1.0
 
 V0r_shift = 0
@@ -28,10 +27,23 @@ denominator = None
 N_overlapping_points = None
 
 # Test calls
+print("Test exec:")
 print(rf.test_exec(1.0))
+print("\n")
 
-rf.r_factor_beam(y1, size_y1, y2, size_y2, E_start1, E_start2, E_step, V0r_shift,
-                      R_pendry, numerator, denominator, N_overlapping_points)
+R_pendry, numerator, denominator, N_overlapping_points = rf.r_factor_beam(y1, y2_match, E_start1, E_start2, E_step, V0r_shift)
 
+print("Matching: R, num, dem, N_ovl")
 print(R_pendry)
+print(numerator)
+print(denominator)
+print(N_overlapping_points)
+print("\n")
+
+R_pendry, numerator, denominator, N_overlapping_points = rf.r_factor_beam(y1, y2_bad_match, E_start1, E_start2, E_step, V0r_shift)
+
+print("Non-Matching: R, num, dem, N_ovl")
+print(R_pendry)
+print(numerator)
+print(denominator)
 print(N_overlapping_points)
