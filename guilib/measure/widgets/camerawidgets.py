viperleed/viperleed

commit 102b082f1f9e229dd6842875f82e6aebbc0ca531

feat: CameraViewer singleton
Make only one CameraViewer object for each camera.

This is done by keeping a cache of {camera: viewer} (with weakrefs to prevent cyclic references) to decide whether a viewer exists already for a certain camera.

guilib/measure/widgets/camerawidgets.py
@@ -11,6 +11,8 @@ Created: 2021-10-29
 Author: Michele Riva
 """
 
+import weakref
+
 import numpy as np
 
 from PyQt5 import (QtCore as qtc,
@@ -82,9 +84,49 @@ class CameraViewer(qtw.QScrollArea):
         camera frames changes. Carries the new zoom factor.
     """
 
+    # _viewers is a dict of {camera: viewer} used to prevent
+    # creating multiple viewers for the same camera. It holds
+    # weak references to the viewer objects, so that we don't
+    # risk creating cyclic references.
+    _viewers = {}
+
     shown = qtc.pyqtSignal()
     image_size_changed = qtc.pyqtSignal()
     zoom_changed = qtc.pyqtSignal(float)  # new zoom factor
+
+    def __new__(cls, camera, *args, **kwargs):
+        """Return a CameraViewer instance.
+
+        This method is reimplemented such that only one viewer
+        can exists for a given camera.
+
+        Parameters
+        ----------
+        camera : CameraABC
+            The camera object whose frames are displayed.
+        *args : object
+            Other positional arguments not used within__new__.
+        **kwargs : object
+            Optional arguments not used within __new__.
+
+        Returns
+        -------
+        viewer : CameraViewer
+            The viewer object.
+        """
+        if camera in cls._viewers:
+            return cls._viewers[camera]
+        new_viewer = super().__new__(cls, camera, *args, **kwargs)
+        cls._viewers[camera] = weakref.ref(new_viewer)
+        return new_viewer
+
+    def __del__(self):
+        """Remove self from the dictionary of viewers."""
+        self._viewers.pop(self.__camera)
+        try:
+            super().__del__()
+        except AttributeError:
+            pass
 
     def __init__(self, camera, *args, parent=None, stop_on_close=True,
                  **kwargs):

commit 8067eede4353f598e45a4f44ef58a244993cf788

fix: bug with singleton CameraViewer
Bug was introduced in commit 102b082f1f9e229dd6842875f82e6aebbc0ca531.

Did not read properly the documentation of weakref. The weakref.ref returns a weak reference object, which has to be called to retrieve the actual object it references. Returns None if the object was garbage-collected.

guilib/measure/widgets/camerawidgets.py
@@ -115,7 +115,12 @@ class CameraViewer(qtw.QScrollArea):
             The viewer object.
         """
         if camera in cls._viewers:
-            return cls._viewers[camera]
+            viewer = cls._viewers[camera]()
+            if viewer is not None:
+                # Viewer is alive
+                return viewer
+            # Viewer has been garbage-collected. Remove it from cache.
+            cls._viewers.pop(camera)
         new_viewer = super().__new__(cls, camera, *args, **kwargs)
         cls._viewers[camera] = weakref.ref(new_viewer)
         return new_viewer
@@ -129,7 +134,7 @@ class CameraViewer(qtw.QScrollArea):
             pass
 
     def __init__(self, camera, *args, parent=None, stop_on_close=True,
-                 **kwargs):
+                 visible=True, **kwargs):
         """Initialize widget.
 
         Parameters
@@ -157,6 +162,7 @@ class CameraViewer(qtw.QScrollArea):
         self.__img_size = qtc.QSize()
         self.__camera = camera
         self.__stop_on_close = bool(stop_on_close)
+        self.visible = bool(visible)
         self.__roi = RegionOfInterest(parent=self.__img_view)
 
         try:
@@ -477,7 +483,7 @@ class CameraViewer(qtw.QScrollArea):
 
         self.__img_view.set_image(image)
         self.image_size = image.size()
-        if not self.isVisible():
+        if self.visible and not self.isVisible():
             self.show()
 
     def __zoom(self, direction='in'):