f34d8d66accb30783edebf670fbae6ff8e0d0f38
michele-riva <riva@iap.tuwien.ac.at>
2021-12-15 10:11:16 +0100

    codestyle: pylint

--- guilib/measure/widgets/measurement_plot.py
+++ guilib/measure/widgets/measurement_plot.py
@@ -1,4 +1,4 @@
-"""Module uimeasurement of viperleed.guilib.measure.widgets
+"""Module measurement_plot of viperleed.guilib.measure.widgets.
 
 ===============================================
       ViPErLEED Graphical User Interface
@@ -11,12 +11,16 @@ Author: Florian Doerr
 Defines the MeasurementPlot class.
 """
 
-import PyQt5.QtCore as qtc
-import PyQt5.QtWidgets as qtw
-import PyQt5.QtGui as qtg
+from PyQt5 import QtWidgets as qtw
 
-from viperleed.guilib.basewidgets import MeasurementFigureCanvas as Figure
+from viperleed.guilib.basewidgets import MeasurementFigureCanvas as Canvas
 from viperleed.guilib.measure.datapoints import DataPoints
+
+
+# TODO: temporarily one can adjust here the structure in which a
+# measurement will be plotted (flat or step-wise). Will then become
+# a combo box.
+SEPARATE_STEPS = False
 
 
 class MeasurementPlot(qtw.QWidget):
@@ -29,16 +33,9 @@ class MeasurementPlot(qtw.QWidget):
         self._glob = {'plot_lines': []}
         self.__markers = ('.', 'o', 'x', '+', '*')
         self.__data_points = None
-        self.__canvas = Figure()
+        self.__canvas = Canvas()
 
         self.__compose()
-
-    def __compose(self):
-        """Prepare widget."""
-        layout = qtw.QVBoxLayout()
-        layout.addWidget(self.__canvas)
-
-        self.setLayout(layout)
 
     @property
     def data_points(self):
@@ -53,8 +50,13 @@ class MeasurementPlot(qtw.QWidget):
 
         Parameters
         ----------
-        data_points : DataPoints object
-            Contains all measured data.
+        data_points : DataPoints
+            The data points containing measurement data to be plotted.
+
+        Raises
+        ------
+        TypeError
+            if data_points is not a DataPoints instance.
         """
         if not isinstance(data_points, DataPoints):
             raise TypeError(
@@ -65,7 +67,28 @@ class MeasurementPlot(qtw.QWidget):
         self.__data_points = data_points
         self.__canvas.ax.clear()
 
-    # TODO: temporarily we will still use the measured_quantity variable for plotting with the new class.
+    def plot_all_data(self, measured_quantity):
+        """Plot data on the canvas for the first time.
+
+        This function is called if there is no plotted
+        data yet or if the quantity that has to be plotted
+        is changed. This method is much slower than calling
+        plot_new_data()
+        """
+        if not self.data_points or not self.data_points.has_data():
+            return
+        self.__canvas.ax.clear()
+        if not SEPARATE_STEPS:
+            self._glob['plot_lines'] = []
+
+        if self.data_points.is_time_resolved():
+            self.__plot_all_time_resolved_data(measured_quantity)
+        else:
+            self.__plot_all_energy_resolved_data(measured_quantity)
+        self.__canvas.draw_idle()
+
+    # TODO: temporarily we will still use the measured_quantity
+    # variable for plotting with the new class.
     # it is passed on from the Measure class
     def plot_new_data(self, measured_quantity):
         """Plot new data to already-plotted data.
@@ -83,6 +106,42 @@ class MeasurementPlot(qtw.QWidget):
             self.__plot_new_energy_resolved_data(measured_quantity)
         self.__canvas.draw_idle()
 
+    def __compose(self):
+        """Prepare widget."""
+        layout = qtw.QVBoxLayout()
+        layout.addWidget(self.__canvas)
+
+        self.setLayout(layout)
+
+    def __plot_all_energy_resolved_data(self, measured_quantity):
+        marker = self.__markers[0]
+        data, nominal_energies = (
+            self.data_points.get_energy_resolved_data(
+                measured_quantity, include_energies=True
+                )
+            )
+        for ctrl_data in data:
+            self.__canvas.ax.plot(nominal_energies, ctrl_data, marker)
+
+    def __plot_all_time_resolved_data(self, measured_quantity):
+        fig = self.__canvas
+        marker = self.__markers[0]
+        data, times = (
+            self.data_points.get_time_resolved_data(
+                measured_quantity, separate_steps=SEPARATE_STEPS,
+                absolute_times=not SEPARATE_STEPS, include_energies=False
+                )
+            )
+
+        for ctrl_times, ctrl_data in zip(times, data):
+            if SEPARATE_STEPS:
+                for step_time, step_data in zip(ctrl_times, ctrl_data):
+                    fig.ax.plot(step_time, step_data, marker)
+            else:
+                self._glob['plot_lines'].extend(
+                    fig.ax.plot(ctrl_times, ctrl_data, marker)
+                    )
+
     def __plot_new_energy_resolved_data(self, measured_quantity):
         marker = self.__markers[0]  # TODO: will have to loop through different quantities
         data, nominal_energies = (
@@ -96,18 +155,16 @@ class MeasurementPlot(qtw.QWidget):
     def __plot_new_time_resolved_data(self, measured_quantity):
         fig = self.__canvas
         marker = self.__markers[0]  # TODO: will have to loop through different quantities
-        separate_steps = False
-        # TODO: temporarily one can adjust here the structure in which a measurement will be plotted (flat or step wise)
         data, times = (
             self.data_points.get_time_resolved_data(
-                measured_quantity, separate_steps=separate_steps,
-                absolute_times=not separate_steps, include_energies=False
+                measured_quantity, separate_steps=SEPARATE_STEPS,
+                absolute_times=not SEPARATE_STEPS, include_energies=False
                 )
             )
         n_controllers = len(data)
         # Loop over controllers
         for i, (ctrl_times, ctrl_data) in enumerate(zip(times, data)):
-            if separate_steps:
+            if SEPARATE_STEPS:
                 # Enough to plot the last step for each
                 fig.ax.plot(ctrl_times[-1], ctrl_data[-1], marker)
             else:
@@ -118,56 +175,6 @@ class MeasurementPlot(qtw.QWidget):
                     continue
                 line = self._glob['plot_lines'][i]
                 line.set_data(ctrl_times, ctrl_data)
-        if not separate_steps:
+        if not SEPARATE_STEPS:
             fig.ax.relim()
             fig.ax.autoscale_view()
-
-    def plot_all_data(self, measured_quantity):
-        """Plot data on the canvas for the first time.
-
-        This function is called if there is no plotted
-        data yet or if the quantity that has to be plotted
-        is changed. This method is much slower than calling
-        plot_new_data()
-        """
-        if not self.data_points or not self.data_points.has_data():
-            return
-        self.__canvas.ax.clear()
-        if not separate_steps:
-            self._glob['plot_lines'] = []
-
-        if self.data_points.is_time_resolved():
-            self.__plot_all_time_resolved_data(measured_quantity)
-        else:
-            self.__plot_all_energy_resolved_data(measured_quantity)
-        self.__canvas.draw_idle()
-
-    def __plot_all_energy_resolved_data(self, measured_quantity):
-        data, nominal_energies = (
-            self.data_points.get_energy_resolved_data(
-                measured_quantity, include_energies=True
-                )
-            )
-        for ctrl_data in data:
-            self.__canvas.ax.plot(nominal_energies, ctrl_data, marker)
-
-    def __plot_all_time_resolved_data(self, measured_quantity):
-        fig = self.__canvas
-        marker = self.__markers[0]
-        separate_steps = False
-        # TODO: temporarily one can adjust here the structure in which a measurement will be plotted (flat or step wise)
-        data, times = (
-            self.data_points.get_time_resolved_data(
-                measured_quantity, separate_steps=separate_steps,
-                absolute_times=not separate_steps, include_energies=False
-                )
-            )
-
-        for i, (ctrl_times, ctrl_data) in enumerate(zip(times, data)):
-            if separate_steps:
-                for step_time, step_data in zip(ctrl_times, ctrl_data):
-                    fig.ax.plot(step_time, step_data, marker)
-            else:
-                self._glob['plot_lines'].extend(
-                    fig.ax.plot(ctrl_times, ctrl_data, marker)
-                    )
