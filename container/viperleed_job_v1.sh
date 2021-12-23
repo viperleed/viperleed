065e343754cc8be25da57f90bb1ad7d600117627
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2021-12-23 15:34:09 +0100

    Singularity definition file and sample SLURM job script
    Definition file for a singularity container containing all dependencies for ViPErLEED and a sample SLURM job script for running from source on clusters like the VSC.

---
+++ container/viperleed_job_v1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH -J viperleed_testv1
+# use 5 cores
+#SBATCH -n 5
+#SBATCH --mem=8G
+
+#SBATCH --qos=devel_0096
+#SBATCH --partition=mem_0096
+#SBATCH --account=p71704
+
+#SBATCH --ntasks-per-core=2
+
+
+# load singularity
+module purge
+spack load -r /fp2564h
+echo "Singularity loaded"
+
+image_path=$HOME/singularity_images/viperleed_testv1.sif
+vpr_path=$HOME/source/
+work_path=$DATA/Tests/
+echo "Using image:			$image_path"
+echo "ViPErLEED Source:		$vpr_path"
+echo "Working directory:	$work_path"
+echo
+
+# run ViPErLEED in Singularity image – /gpfs needs to be bound to /gpfs for access to $DATA
+singularity exec --bind /gpfs:/gpfs $image_path python3 job.py $vpr_path $work_path
+
+echo "ViPErLEED finished"
