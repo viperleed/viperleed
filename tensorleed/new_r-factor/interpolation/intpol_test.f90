f9f5de13b32e846d3b5c2c6a368cb50cb13f4e3a
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2021-12-30 14:29:46 +0100

    Update intpol_test.f90

--- tensorleed/new_r-factor/interpolation/intpol_test.f90
+++ tensorleed/new_r-factor/interpolation/intpol_test.f90
@@ -1,12 +1,13 @@
 program intpol_test
     
     use, intrinsic :: iso_fortran_env
+
+    use interpolation
+    implicit none
+    
     integer, parameter :: sp = REAL32
     integer, parameter :: dp = REAL64
     integer, parameter :: qp = REAL128
-    use interpolation
-
-    implicit none
 
     real(dp) :: data(11), data_out(30)
     data = (0,1,2,3,-3,-1,0,1,-2,3,0)
