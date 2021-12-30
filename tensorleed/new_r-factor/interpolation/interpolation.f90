bcd2e8b3e90db9c7c15d339bde46a8aeaebb838c
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2021-12-30 14:27:22 +0100

    update dp and use

--- tensorleed/new_r-factor/interpolation/interpolation.f90
+++ tensorleed/new_r-factor/interpolation/interpolation.f90
@@ -1,11 +1,16 @@
 ! Created by Alexander M. Imre on 04.12.21.
 
 module interpolation
+    use, intrinsic :: iso_fortran_env
+    integer, parameter :: sp = REAL32
+    integer, parameter :: dp = REAL64
+    integer, parameter :: qp = REAL128
+    
     implicit none
     contains
 
     subroutine equidist_quint_spline_coeffs(in_data, n_in, coeffs)
-        integer, (intent in) :: n_in
+        integer,  (intent in) :: n_in
         real(dp), (intent in) :: in_data(n_in)
 
         real(dp), (intent out) :: coeffs(5*n_in)
--- tensorleed/new_r-factor/interpolation/intpol_test.f90
+++ tensorleed/new_r-factor/interpolation/intpol_test.f90
@@ -1,10 +1,16 @@
 program intpol_test
-    implicit none
+    
+    use, intrinsic :: iso_fortran_env
+    integer, parameter :: sp = REAL32
+    integer, parameter :: dp = REAL64
+    integer, parameter :: qp = REAL128
     use interpolation
+
+    implicit none
 
     real(dp) :: data(11), data_out(30)
     data = (0,1,2,3,-3,-1,0,1,-2,3,0)
 
-    
+
 end program intpol_test
 
