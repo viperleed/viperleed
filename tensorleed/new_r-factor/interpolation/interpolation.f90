b9e6768afbe722fb47b26ead6f9664534fa63d31
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2022-01-03 09:53:15 +0100

    fix: order of statements

--- tensorleed/new_r-factor/interpolation/interpolation.f90
+++ tensorleed/new_r-factor/interpolation/interpolation.f90
@@ -2,23 +2,25 @@
 
 module interpolation
     use, intrinsic :: iso_fortran_env
+    implicit none
+    
     integer, parameter :: sp = REAL32
     integer, parameter :: dp = REAL64
     integer, parameter :: qp = REAL128
     
-    implicit none
+    
     contains
 
     subroutine equidist_quint_spline_coeffs(in_data, n_in, coeffs)
-        integer,  (intent in) :: n_in
-        real(dp), (intent in) :: in_data(n_in)
+        integer, intent (in) :: n_in
+        real(dp), intent (in) :: in_data(n_in)
 
-        real(dp), (intent out) :: coeffs(5*n_in)
-
-
+        real(dp), intent (out) :: coeffs(5*n_in)
 
 
-    end subroutine equidist_spline_intpol_coeffs
+
+
+    end subroutine equidist_quint_spline_coeffs
 
 
 end module interpolation
--- tensorleed/new_r-factor/interpolation/intpol_test.f90
+++ tensorleed/new_r-factor/interpolation/intpol_test.f90
@@ -2,7 +2,7 @@ program intpol_test
     
     use, intrinsic :: iso_fortran_env
 
-    use interpolation
+    use interpolation, only : equidist_quint_spline_coeffs
     implicit none
     
     integer, parameter :: sp = REAL32
@@ -10,8 +10,9 @@ program intpol_test
     integer, parameter :: qp = REAL128
 
     real(dp) :: data(11), data_out(30)
-    data = (0,1,2,3,-3,-1,0,1,-2,3,0)
+    data = (/0,1,2,3,-3,-1,0,1,-2,3,0/)
 
+    print*, data
 
 end program intpol_test
 
