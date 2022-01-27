e0201b3efd4b63feabe050f1b0df2bede7765fcf
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2022-01-27 17:14:22 +0100

    feat: also f2py wrap the interpolation module

--- tensorleed/new_r-factor/wrapf2py/test.sh
+++ tensorleed/new_r-factor/wrapf2py/test.sh
@@ -3,6 +3,9 @@
 gfortran -c -g -Og -llapack ../interpolation/interpolation.f90
 cp ../interpolation/interpolation.mod ./interpolation.mod
 cp ../interpolation/interpolation.o ./interpolation.o
+f2py -m interpolation ../interpolation/interpolation.f90 -h interpolation.pyf --overwrite-signature --debug-capi only: get_array_sizes de_Boor_size single_calc_spline pre_eval_input_grid calc_spline_with_pre_eval single_interpolate_coeffs_to_grid get_intervals calc_deBoor eval_bspline_fast
+f2py -c --f90flags="-g -Og -llapack" interpolation.pyf ../interpolation/interpolation.f90
+
 cp ../rfactor.f90 ./rfactor.f90
 gfortran -c -g -Og rfactor.f90
 
