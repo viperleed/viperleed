6ab2ff058036c2634581fedca8fa7bc7bfc9df48
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2022-04-26 18:43:54 +0200

    fix: various fixes in ASE API
    ) renamed file and functions
    ) implements notes left by MR in last commit
    ) checked direction of rotation
    ) checked effect on chirality
    ) implemented error message via warnings module
    
    Co-Authored-By: Michele Riva <riva@iap.tuwien.ac.at>

--- viperleed_from_ase.py
+++ viperleed_from_ase.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """A collection of functions that run ViPErLEED from a provided ASE object.
 
-@Author: Alexander M. Imre
+@Author: Alexander M. Imre, Michele Riva
 based on tleedm_from_ase.py by Florian Kraushofer
 
 Requires viperleed to be in the system path (or on PYTHONPATH).
 """
 
 import os
+import warnings
 from pathlib import Path
 import numpy as np
 import shutil
@@ -15,28 +16,27 @@ from io import StringIO
 
 import viperleed
 
-# for calculate_from_ase_structure
+# for run_from_ase
 from viperleed.tleedm import run_tleedm
 from viperleed.tleedmlib import Slab, Rparams
 from viperleed.tleedmlib.files.poscar import writePOSCAR
-from viperleed.tleedmlib.files.parameters import (readPARAMETERS,
-                                                  interpretPARAMETERS)
+from viperleed.tleedmlib.files.parameters import readPARAMETERS, interpretPARAMETERS
 
-# for run_rfactor_from_csv
+# for rfactor_from_csv
 from viperleed.tleedmlib.files.beams import readOUTBEAMS
 from viperleed.tleedmlib.files.iorfactor import beamlist_to_array
 from viperleed.tleedmlib.wrapped.rfactor import r_factor_new as rf
 from viperleed.tleedmlib.wrapped.error_codes import error_codes
 
 
-def calculate_from_ase_structure(
+def run_from_ase(
     exec_path,
     ase_object,
     inputs_path=None,
     cut_cell_c_fraction=0.4,
     uc_transformation_matrix=None,
     uc_scaling=None,
-    ):
+):
     """Perform a ViPErLEED calulation starting from an ase.Atoms object.
     
     This function is a core part of the API for ViPErLEED. It allows to run
@@ -70,7 +70,7 @@ def calculate_from_ase_structure(
         the surface at the "top" -- i.e., at high z coordinates, and bulk-like
         layers at the bottom). Cutting will be performed along the vector c
         AFTER all transformations (see uc_transformation_matrix) are applied.
-        Only the part of the cell strictly above (i.e., >) this value will be
+        Only the part of the cell above (i.e., >=) this value will be
         kept and everything else will be discarded.
     uc_transformation_matrix : np.ndarray, optional
         3x3 array, can contain an arbitray orthogonal transforamtion (i.e.,
@@ -142,6 +142,9 @@ def calculate_from_ase_structure(
         flip_c_mat : Flips (i.e., mirrors) cell along c
         rot_mat_c(theta) : Returns a rotation matrix around c (by theta rad.)
         rot_mat_a(theta) : Returns a rotation matrix around a (by theta rad.)
+    For applying multiple operations, the order does matter. You can combine
+    operations via matrix multiplication (@ symbol or np.dot); the left-most
+    matrix is applied last.
     """
 
     exec_path = Path(exec_path)
@@ -157,20 +160,20 @@ def calculate_from_ase_structure(
         "DISPLACEMENTS",
         "EXPBEAMS",
         "EXPBEAMS.csv",
-        ]
+    ]
 
     # Copy all files in the input Path
     if inputs_path is not None:
         inputs_path = Path(inputs_path)
         for file in input_files:
             try:
-                shutil.copy2(inputs_path/file, exec_path/file)
+                shutil.copy2(inputs_path / file, exec_path / file)
             except FileNotFoundError:
                 pass
 
     # check for PARAMETERS file - without that we can't proceede
     parameters_name = "PARAMETERS"
-    if not (exec_path/parameters_name).exists():
+    if not (exec_path / parameters_name).exists():
         # No PARAMETERS file – Error
         raise RuntimeError("No PARAMETERS file found – this is required")
     # If present, we are good to go.
@@ -195,7 +198,7 @@ def calculate_from_ase_structure(
     # check if the now transformed slab has any z components in vectors a & b
     # raise an error because this would mess up parts of the TensErleed
     # calculations (as we pass on only the first two components).
-    
+
     a, b, _ = slab.ucell.T
     if abs(a[2]) > 1e-5 or abs(b[2]) > 1e-5:
         raise RuntimeError(
@@ -206,9 +209,7 @@ def calculate_from_ase_structure(
         )
 
     # Cut the slab as given in cut_cell_c_fraction - very important
-    slab.atlist = [
-        at for at in slab.atlist if at.pos[2] > cut_cell_c_fraction
-        ]
+    slab.atlist = [at for at in slab.atlist if at.pos[2] >= cut_cell_c_fraction]
     # Update Slab
     slab.updateAtomNumbers()
     slab.updateElementCount()
@@ -224,12 +225,13 @@ def calculate_from_ase_structure(
 
     poscar_name = "POSCAR"
     if (exec_path / poscar_name).exists():
-        raise RuntimeError(                                                        # perhaps make it a warning from the warnings stlib?
+        warnings.warn(
             "A 'POSCAR' file is already present in exec_path - calculation "
             "aborted. Check if a ViPErLEED calculation was already run in "
-            "this directory."
-            )
-    # If no POSCAR - good :)
+            "this directory.",
+            category=RuntimeWarning,
+        )
+
     writePOSCAR(slab, poscar_name)
 
     # Take care of input files and work directory
@@ -269,11 +271,11 @@ def calculate_from_ase_structure(
     amp_real_name = "Complex_amplitudes_real.csv"
     amp_imag_name = "Complex_amplitudes_imag.csv"
 
-    with open(theobeams_name, 'r', encoding='utf-8') as fproxy:
+    with open(theobeams_name, "r", encoding="utf-8") as fproxy:
         theobeams_file_str = fproxy.read()
-    with open(amp_real_name, 'r', encoding='utf-8') as fproxy:
+    with open(amp_real_name, "r", encoding="utf-8") as fproxy:
         amp_real_file_str = fproxy.read()
-    with open(amp_imag_name, 'r', encoding='utf-8') as fproxy:
+    with open(amp_imag_name, "r", encoding="utf-8") as fproxy:
         amp_imag_file_str = fproxy.read()
 
     # Move back home
@@ -282,14 +284,14 @@ def calculate_from_ase_structure(
     return theobeams_file_str, amp_real_file_str, amp_imag_file_str, v0i
 
 
-def run_rfactor_from_csv(
+def rfactor_from_csv(
     beams_files,
     v0i,
     beams_file_is_content=(False, False),
     v0r_shift_range=(-3, 3),
     intpol_deg=5,
     intpol_step=0.5,
-    ):
+): ## TODO: add kwarg for mapping for averaging
     """Compute the Pendry R-factor between two CSV files (in ViPErLEED format).
     
     Read in two CSV files containing LEED-I(V) spectra and compute the mutual
@@ -339,8 +341,7 @@ def run_rfactor_from_csv(
     # Check if file is supposed to be read from disk or from memory and pass
     # it to readOUTBEMS accordingly.
     beams_tmp = [None, None]
-    for i, (is_content, file) in enumerate(zip(beams_file_is_content,
-                                               beams_files)):
+    for i, (is_content, file) in enumerate(zip(beams_file_is_content, beams_files)):
         if is_content:
             file = StringIO(file)
         beams_tmp[i] = readOUTBEAMS(filename=file, sep=",")
@@ -354,52 +355,58 @@ def run_rfactor_from_csv(
     # exactly the same order. Not sure the order is consistent between
     # consecutive runs, though (it will be if the order of the output is
     # the same as in IVBEAMS).
+    # AMI: Could do, but compared to the refcalc required for the
+    # theoretical specetra, it's still very fast.
+
+    # Check if multiple beams have the same index. This should never be
+    # the case and would throw off the calculation.
+
+    for i, beams in enumerate((beams1, beams2)):
+        unique_lables = set()
+        for beam in beams:
+            unique_lables.add(beam.hkfrac)
+        if len(unique_lables) != len(beams):
+            raise RuntimeError(
+                f"Multiple beams sharing the same index encountered"
+                f" in input beamset {i}!"
+            )
     
-    # Check for common beams                                                   # could be easier to handle with a dict
-    beam1_has_correspondece = [False] * len(beams1)
-    beam2_correspondence = [None] * len(beams2)
+    # Check for common beams
+    beam_corr_1_2 = {}
 
     for i, beam1 in enumerate(beams1):
         for j, beam2 in enumerate(beams2):
-            # if _comp_beam_index(beam1.hkfrac, beam2.hkfrac):
             if beam1.hkfrac == beam2.hkfrac:
-                beam1_has_correspondece[i] = True
-                beam2_correspondence[j] = i
+                beam_corr_1_2[beam1] = beam2
 
-    for beam_id in beam2_correspondence:
-        if (beam_id is not None
-            and beam2_correspondence.count(beam_id) != 1):
-            # print(beams1[beam_id].hkfrac, beams2[beam_id].hkfrac)
-            raise RuntimeError(
-                "Multiple beams sharing the same index encountered!"
-                )
+    for beam in beams1:
+        if beam not in list(beam_corr_1_2.keys()):
+            err_msg += f"Beam {beam.label} from file 1 has no correspondence.\n"
+    for beam in beams2:
+        if beam not in list(beam_corr_1_2.values()):
+            err_msg += f"Beam {beam.label} from file 2 has no correspondence.\n"
 
     corr_beams1 = []
     corr_beams2 = []
-    # any beams without correspondence?
-    for i, beam1 in enumerate(beams1):
-        if beam1_has_correspondece[i]:
-            corr_beams1.append(beam1)
-            corr_beams2.append(beams2[beam2_correspondence.index(i)])
-        else:
-            err_msg += f"Beam {beam1.label} from file 1 has no correspondence\n"
-    for j, beam2 in enumerate(beams2):
-        if beam2_correspondence[j] is None:
-            err_msg += f"Beam {beam2.label} from file 2 has no correspondence\n"
 
+    # Warn but don't raise
     if err_msg:
-        print(err_msg)                                                                  # use warning?
+        warnings.warn(err_msg, category=RuntimeWarning)
 
     # Done with checking beam indices
-    n_beams = len(corr_beams1)
+    n_beams = len(beam_corr_1_2)
+
+    for key, value in beam_corr_1_2.items():
+        corr_beams1.append(key)
+        corr_beams2.append(value)
 
     # Now get beams into arrays
-    beams1_en, beam1_id_start, beam1_n_E_beams, beam1_arr = beamlist_to_array(
+    beams1_en, beams1_id_start, beams1_n_E_beams, beams1_arr = beamlist_to_array(
         corr_beams1
-        )
-    beams2_en, beam2_id_start, beam2_n_E_beams, beam2_arr = beamlist_to_array(
+    )
+    beams2_en, beams2_id_start, beams2_n_E_beams, beams2_arr = beamlist_to_array(
         corr_beams2
-        )
+    )
 
     minen1, minen2 = beams1_en.min(), beams2_en.min()
     minen = max(minen1, minen2)
@@ -412,52 +419,57 @@ def run_rfactor_from_csv(
         maxen += v0r_shift_range[1]
 
     grid = np.arange(minen, maxen + intpol_step, intpol_step)
-
+    
     skip_stages = np.int32([0, 0, 0, 0, 0])  # usused - we don't skip anything
     averaging_scheme = np.int32(np.arange(n_beams) + 1)  # don't average
 
     # interpolate and prepare beam arrays
-    (beams1_e_start_beams_out,
-     beams1_n_e_beams_out,
-     beams1_intpol_intensity,        # may be needed for R2
-     beams1_yfunc,
-     ierr) = rf.prepare_beams(beams1_en,
-                              beam1_arr,
-                              beam1_id_start + 1,
-                              beam1_n_E_beams,
-                              skip_stages,
-                              n_beams,
-                              averaging_scheme,
-                              which_r,
-                              intpol_deg,
-                              grid,
-                              v0i)
+    (
+        beams1_e_start_beams_out,
+        beams1_n_e_beams_out,
+        beams1_intpol_intensity,  # may be needed for R2
+        beams1_yfunc,
+        ierr,
+    ) = rf.prepare_beams(
+        beams1_en,
+        beams1_arr,
+        beams1_id_start + 1,
+        beams1_n_E_beams,
+        skip_stages,
+        n_beams,
+        averaging_scheme,
+        which_r,
+        intpol_deg,
+        grid,
+        v0i,
+    )
     _check_ierr(ierr)
 
-    (beams2_e_start_beams_out,
-     beams2_n_e_beams_out,
-     beams2_intpol_intensity,        # may be needed for R2
-     beams2_yfunc,
-     ierr) = rf.prepare_beams(beams2_en,
-                              beam2_arr,
-                              beam2_id_start + 1,
-                              beam2_n_E_beams,
-                              skip_stages,
-                              n_beams,
-                              averaging_scheme,
-                              which_r,
-                              intpol_deg,
-                              grid,
-                              v0i)
+    (
+        beams2_e_start_beams_out,
+        beams2_n_e_beams_out,
+        beams2_intpol_intensity,  # may be needed for R2
+        beams2_yfunc,
+        ierr,
+    ) = rf.prepare_beams(
+        beams2_en,
+        beams2_arr,
+        beams2_id_start + 1,
+        beams2_n_E_beams,
+        skip_stages,
+        n_beams,
+        averaging_scheme,
+        which_r,
+        intpol_deg,
+        grid,
+        v0i,
+    )
     _check_ierr(ierr)
 
     # Ready to calculate R-factor
-    v0r_shift_range_int = np.int32(
-        [round(v / intpol_step) for v in v0r_shift_range]
-        )
+    v0r_shift_range_int = np.int32([round(v / intpol_step) for v in v0r_shift_range])
     v0r_center = sum(v0r_shift_range_int) // 2
-    start_guess = np.int32([v0r_shift_range_int[0], v0r_center,
-                            v0r_shift_range_int[1]])
+    start_guess = np.int32([v0r_shift_range_int[0], v0r_center, v0r_shift_range_int[1]])
 
     # Same settings as used in R-factor for ViPErLEED
     fast_search = False  # TODO change to True as default if we are sure it is better
@@ -465,11 +477,7 @@ def run_rfactor_from_csv(
     tol_r_2 = (1 - 5e-2,)
     max_fit_range = 6
 
-    (_,
-     best_v0r,
-     best_R,
-     *_,
-     ierr) = rf.r_beamset_v0r_opt_on_grid(
+    (_, best_v0r, best_r, *_, ierr) = rf.r_beamset_v0r_opt_on_grid(
         which_r,
         v0r_shift_range_int,
         start_guess,
@@ -484,27 +492,11 @@ def run_rfactor_from_csv(
         tol_r=tol_r,
         tol_r_2=tol_r_2,
         max_fit_range=max_fit_range,
-        )
+    )
     # If encountered an error
     _check_ierr(ierr)
 
-    return best_R, best_v0r
-
-
-def _comp_beam_index(id1, id2, eps=1e-5):                                               # NEEDED? probably using id1 == id2 is enough
-    # Compares diffraction beam indices and checks if they refer to the same beam
-    # for beam1 (a|b) and beam2 (c|d) check:
-    denom_magnitude_equal = abs(float(abs(id1[1]) - abs(id2[1]))) < eps  # |a|=|c|      # These are not the denominators, this is "h" as a Fraction
-    num_magnitude_equal = abs(float(abs(id1[0]) - abs(id2[0]))) < eps  # |b|=|d|        # Same here, this is "k" as a Fraction
-    fraction_equivalent = (
-        abs(float(id1[0]) * float(id2[1])) - abs(float(id1[1]) * float(id2[0])) < eps
-    )  # |a*d| = |c*b| (same as a/b= +/- c/d)
-    direction_equal = (
-        abs(float(id1[0]) - float(id2[0])) + abs(float(id1[1]) - float(id2[1])) < eps
-    )
-
-    check = denom_magnitude_equal and num_magnitude_equal and direction_equal
-    return check
+    return best_r, best_v0r
 
 
 def _check_ierr(ierr):
@@ -515,42 +507,46 @@ def _check_ierr(ierr):
 def rot_mat_a(theta):
     """Generates a rotation matrix around the a axis.
     
-    TODO: Say which way it rotates (when used above)
+    The rotation is positive, i.e. clockwise when looking along a.
     
     Parameters
     ----------
     theta : float
-        Angle of rotation in radians. Use e.g. np.radians(deg) to
-        convert from degrees.
+        Angle of rotation in degrees. (Use np.degrees to convert)
     """
+    theta = np.radians(theta)
     rot_mat = [
         [1, 0, 0],
         [0, np.cos(theta), -np.sin(theta)],
         [0, np.sin(theta), np.cos(theta)],
-        ]
+    ]
     return np.array(rot_mat)
 
 
 def rot_mat_c(theta):
     """Generates a rotation matrix around the c axis.
     
-    TODO: Say which way it rotates (when used above)
+    The rotation is positive, i.e. clockwise when looking along c.
     
     Parameters
     ----------
     theta : float
-        Angle of rotation in radians Use e.g. np.radians(deg) to
-        convert from degrees.
+        Angle of rotation in degrees. (Use np.degrees to convert)
     """
+    theta = np.radians(theta)
     rot_mat = [
         [np.cos(theta), -np.sin(theta), 0],
         [np.sin(theta), np.cos(theta), 0],
         [0, 0, 1],
-        ]
+    ]
     return np.array(rot_mat)
 
 
-# some other useful matrices                                                     # These are a bit funky, as they change the handedness of the cell.
+# some other useful matrices
+
+# Be careful: These WILL change chirality (handedness) of the cell -
+# use rotation around a or c if you want to avoid this. (Chirality
+# changes can not be expressed via rotations.)
 switch_b_c_mat = np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]])  # swap b and c
 switch_a_b_mat = np.array([[0, 1, 0], [1, 0, 0], [0, 0, 1]])  # swap a and b
 
