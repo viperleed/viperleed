9c716d3c0a4d6c5c134cc66cd80175f1582849cf
Michele Riva <riva@iap.tuwien.ac.at>
2022-07-12 20:06:14 +0200

    fix: fix: CameraViewer popup after stopped 2
    Version no.2 of another commit (0fae0f314facf95dd08f48e1aaa05828bd0fb090) that did not entirely solved the issue.
    
    Now a viewer should not pop up any longer after it is closed because of arrivals of new frames while the camera is being stopped.

--- guilib/measure/widgets/camerawidgets.py
+++ guilib/measure/widgets/camerawidgets.py
@@ -323,13 +323,12 @@ class CameraViewer(qtw.QScrollArea):
         """Extend to stop camera when window is closed by the user."""
         camera = self.camera
         if (event.spontaneous() and camera.is_running and self.stop_on_close):
-            camera.stop()
-
             # Disconnect signals that may pop up the window
             # again should a frame arrive in the meantime
             if self.show_auto:
                 base.safe_disconnect(camera.image_processed, self.__show_image)
                 base.safe_disconnect(camera.frame_ready, self.__show_image)
+            camera.stop()
         super().closeEvent(event)
 
     def keyPressEvent(self, event):  # pylint: disable=invalid-name
