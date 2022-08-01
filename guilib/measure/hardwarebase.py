88af9e6c278e707d72e75c2a03c462e75f4e0d2a
Michele Riva <riva@iap.tuwien.ac.at>
2022-08-01 11:23:27 +0200

    feat: new class Version
    Simple class that handles version comparisons. Accepts (major, minor, patch) with minor and patch optional, and strings of the form "<major>[<minor>[.<patch>]]" with "[]" optional. Supports equality and ordering comparisons (>, <, >=, <=, ==, !=), also with respect to a string of the same form as accepted at instantiation.
    
    Attributes: .major, .minor, .patch. The former is always present. Querying the others may raise RuntimeError if not originally known.

--- guilib/measure/hardwarebase.py
+++ guilib/measure/hardwarebase.py
@@ -336,3 +336,128 @@ class ViPErLEEDErrorEnum(tuple, enum.Enum):
     def code(self):
         """Return the code of this error."""
         return self.value[0]
+
+
+class Version:
+    """Simple class to easily handle versions."""
+
+    def __init__(self, major, minor=-1, patch=-1):
+        """Initialize instance.
+
+        Parameters
+        ----------
+        major : int or str
+            When an integer, interpret as the "major" part of
+            version; when a string, it should be of the form
+            "<major>.<minor>.<patch>", with minor and patch
+            optional, and each field be int-able.
+        minor : int, optional
+            Minor portion of the version. If major is given as
+            a string, this is discarded. If negative or not given,
+            it is interpreted as "zero".
+        minor : int, optional
+            Patch portion of the version. If major is given as
+            a string, this is discarded.  If negative or not given,
+            it is interpreted as "zero".
+
+        Raises
+        ------
+        TypeError
+            If any of the inputs is not an integer. If major is given
+            as a string, the exception is raised if one of the parts
+            is not int-able.
+        """
+        # pylint: disable=redefined-variable
+        # Disabled because it makes it easier
+        if isinstance(major, str):
+            major, *rest = (int(v) for v in major.split('.'))
+            try:
+                minor = rest.pop(0)
+            except IndexError:
+                minor = -1
+            try:
+                patch == rest.pop(0)
+            except IndexError:
+                patch = -1
+        if not all(isinstance(p, int) for p in (major, minor, patch)):
+            raise TypeError(
+                f"{self.__class__.__name__}: Invalid type for one of the parts"
+                )
+        self.__has_parts = (True, minor > 0, patch > 0)
+        self._parts = (major, max(minor, 0), max(patch, 0))
+
+    @property
+    def major(self):
+        """Return the major part of self."""
+        return self._parts[0]
+
+    @property
+    def minor(self):
+        """Return the minor part of self (if present) or RuntimeError."""
+        if not self.__has_parts[1]:
+            raise RuntimeError(f"{self.__class__.__name__} has no minor.")
+        return self._parts[1]
+
+    @property
+    def patch(self):
+        """Return the minor part of self (if present) or RuntimeError."""
+        if not self.__has_parts[2]:
+            raise RuntimeError(f"{self.__class__.__name__} has no patch.")
+        return self._parts[2]
+
+    def __eq__(self, other):
+        """Return True if self == other, NotImplemented otherwise."""
+        if not isinstance(other, self.__class__):
+            try:
+                other = self.__class__(other)
+            except TypeError:
+                return NotImplemented
+        if self._parts == other._parts:
+            return True
+        return NotImplemented
+
+    def __lt__(self, other):
+        """Return whether self < other."""
+        if not isinstance(other, self.__class__):
+            try:
+                other = self.__class__(other)
+            except TypeError:
+                return NotImplemented
+        return self._parts < other._parts
+
+    def __le__(self, other):
+        """Return whether self <= other."""
+        if not isinstance(other, self.__class__):
+            try:
+                other = self.__class__(other)
+            except TypeError:
+                return NotImplemented
+        return self._parts <= other._parts
+
+    def __gt__(self, other):
+        """Return whether self > other."""
+        if not isinstance(other, self.__class__):
+            try:
+                other = self.__class__(other)
+            except TypeError:
+                return NotImplemented
+        return self._parts > other._parts
+
+    def __ge__(self, other):
+        """Return whether self > other."""
+        if not isinstance(other, self.__class__):
+            try:
+                other = self.__class__(other)
+            except TypeError:
+                return NotImplemented
+        return self._parts >= other._parts
+
+    def __str__(self):
+        """Return a string representation of self."""
+        return ".".join(str(p)
+                        for p, had_part in zip(self._parts, self.__has_parts)
+                        if had_part)
+
+    def __repr__(self):
+        """Return a representation of self."""
+        return f"{self.__class__.__name__}({self})"
