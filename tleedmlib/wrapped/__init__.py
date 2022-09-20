69bd70021a461072a66c6300bae9df76fa35e947
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2022-09-20 15:38:04 +0200

    feat: warn if f2py compiled modules incompatible

--- tleedmlib/wrapped/__init__.py
+++ tleedmlib/wrapped/__init__.py
@@ -1,2 +1,9 @@
-from .rfactor import r_factor_new as rf
-from .interpolation import interpolation as intpol
+import warnings
+
+try:
+    from .rfactor import r_factor_new as rf
+    from .interpolation import interpolation as intpol
+except RuntimeError:
+    warnings.warn("F2PY compiled module rfactor and/or interpolation failed to import.")
+    warnings.warn("Check that these modules were compiled with the correct version of numpy/F2PY.")
+    raise
