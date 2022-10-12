c37c23ef6df80bd8580adb6c58a194292c0d3170
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2022-10-12 16:48:20 +0200

    fix: error in import

--- tleedmlib/sections/rfactor.py
+++ tleedmlib/sections/rfactor.py
@@ -17,7 +17,7 @@ from pathlib import Path
 from viperleed.tleedmlib.files.iorefcalc import readFdOut
 from viperleed.tleedmlib.leedbase import fortran_compile_batch, getTLEEDdir, getTensors
 import viperleed.tleedmlib.files.iorfactor as io
-from viperleed.TL_base import validate_multiple_files
+from viperleed.tleedmlib.TL_base import validate_multiple_files
 
 from viperleed.tleedmlib.wrapped.rfactor import r_factor_new as rf
 from viperleed.tleedmlib.wrapped.error_codes import error_codes, check_ierr
