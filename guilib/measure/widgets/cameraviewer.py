fix: image underflow error
When calculating the saturation mask, computing img_array - _min CAN LEAD TO UNDERFLOW of the  uint16 type.

While this should not happen in principle, we have observed this problem in the 174e camera from Julich. This means that I may be calculating wrongly the minimum intensity returned in camera.intensity_limits. The 174e in Julich can return 0 intensities while being a 12bit camera using the higher 12 bits of 16. This is different from the 265 I have been testing on. This may have been a fix introduced at the firmware level by The Imaging Source recently.

guilib/measure/widgets/cameraviewer.py
@@ -727,7 +727,7 @@ class CameraViewer(qtw.QScrollArea):
         # pixels correction to images before showing.
         # Alternatively: use self.camera.bad_pixels
         # to decide depending on how many there are
-        saturation_arr = (img_array - _min) > .99*(_max - _min)
+        saturation_arr = img_array > _min + .99*(_max - _min)
         if np.sum(saturation_arr) <= 5:
             return None
 