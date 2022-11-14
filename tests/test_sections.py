8873615510663de590660242d7ee9150dbf7c6aa
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2022-11-14 09:24:47 +0100

    doc: doc strings

--- tests/test_sections.py
+++ tests/test_sections.py
@@ -50,16 +50,15 @@ class TestInitializationAg(TesTTleedmFromFiles):
 
 class TestCaseInitializationAgRun(TestInitializationAg):
     def test_work_path_exists(self):
-        # Check that work_path was created properly
+        """Check that work_path was created properly."""
         assert Path(self.work_path).is_dir()
 
     def test_files_copied_correctly(self):
-        # Check if all files were copied correctly
-        
+        """Check if all files were copied correctly."""
         input_files = [file.name for file in self.test_dir.glob('*')]
         source_copied = all(file in input_files for file in self.required_files)
         input_files = [file.name for file in Path(self.work_path).glob('*')]
-        work_copied = all(file in input_files for file in self.required_files)        
+        work_copied = all(file in input_files for file in self.required_files)
         assert source_copied and work_copied
 
 
@@ -74,9 +73,11 @@ class TestCaseInitializationAgFileChecks(TestInitializationAg):
         cls.files_after_run = [file.name for file in Path(cls.work_path).glob('*')]
 
     def test_initialization_runs(self):
+        """Test if initialization gives exit code 0."""
         assert self.exit_code == 0
 
     def test_files_present(self):
+        """Checks if files are present after initialization"""
         files_to_test = ('IVBEAMS', 'BEAMLIST', 'VIBROCC')
         for file in files_to_test:
             with self.subTest(file=file):
