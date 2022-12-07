fix for calculation of R+var(R) in Errors.pdf (#53)
* fix: bug in plotting of R+var(R) in Errors.pdf

Tilman reported an issue in the Errors.pdf plotting: The level of R+var(R) may be different if multiple modes are calculated at the same time vs separately. This happens if the modes have different R_min (e.g. geometric and occ if not ran with best configuration with both).
Change write_errors_pdf() such that it doesn't take var as input, but rather calculates it new for each mode.

* fix: missed leftover logging in previous commit

* feat: Error.pdf plotting y-range

Upper range on R-factor axis >= R_min + 0.05 (may look odd otherwise).

tleedmlib/files/ioerrorcalc.py
@@ -99,7 +99,21 @@ def write_errors_csv(errors, filename="Errors.csv", sep=","):
     return
 
 
-def write_errors_pdf(errors, filename="Errors.pdf", var=None):
+def write_errors_pdf(errors, v0i, energy_range, filename="Errors.pdf"):
+    """Creates and writes Errors.pdf.
+
+    Parameters
+    ----------
+    errors : list of R_Error
+        contains the R-factors to be plotted
+    v0i : float
+        Imaginary part of inner potential. Can be taken from rp.V0_IMAG.
+    energy_range : float
+        Total energy range used in the calculation (in eV). Can be taken
+        from rp.total_energy_range().
+    filename : str, optional
+        Path of file to be written, by default "Errors.pdf"
+    """
     global plotting
     if not plotting:
         logger.debug("Necessary modules for plotting not found. Skipping "
@@ -115,11 +129,16 @@ def write_errors_pdf(errors, filename="Errors.pdf", var=None):
               "vib": "Vibrational amplitudes",
               "occ": "Site occupation"}
 
-    rmin = min(r for err in errors for r in err.rfacs)
     for mode in ("geo", "vib", "occ"):
         mode_errors = [err for err in errors if err.mode == mode]
         if not mode_errors:
             continue
+
+        # minimum R-factor and R-factor variance for that mode
+        rmin = min(r for err in mode_errors for r in err.rfacs)
+        var = np.sqrt(8*np.abs(v0i) / energy_range) * rmin
+        logger.info(f"{titles[mode]} error calculation: Found var(R) = {var:.4f}.")
+
         err_x = {}
         err_y = {}
         err_x_inters = {}  # x-values of intersections with var(R)
@@ -176,7 +195,7 @@ def write_errors_pdf(errors, filename="Errors.pdf", var=None):
             ax.set_xlabel('Site occupation (%)')
         ax.set_ylabel('Pendry R-factor')
         ax.set_title(titles[mode])
-        ylim = (0, rmax + ((rmax-rmin)*0.1))
+        ylim = (0, rmax + max(((rmax-rmin)*0.15), 0.05))
         if var and rmin + var < rmax + (rmax-rmin)*0.1:
             ax.plot(xrange, [rmin + var]*2, color="slategray")
             inters = sorted([x for err in mode_errors

tleedmlib/sections/errorcalc.py
@@ -148,9 +148,6 @@ def errorcalc(sl, rp):
     if len(errors) == 0:
         logger.info("Error calculation: Returning with no output.")
         return
-    minR = min(r for err in errors for r in err.rfacs)
-    varR = np.sqrt(8*np.abs(rp.V0_IMAG) / rp.total_energy_range()) * minR
-    logger.info("Found var(R) = {:.4f}".format(varR))
     tl_io.write_errors_csv(errors)
-    tl_io.write_errors_pdf(errors, var=varR)
+    tl_io.write_errors_pdf(errors, v0i=rp.V0_IMAG, energy_range=rp.total_energy_range())
     return