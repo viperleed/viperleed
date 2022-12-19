8e9038c42b755525d384039bf0d63d4ef5f06054
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2022-12-19 17:13:31 +0100

    fix: suppress warnings for F2PY rfactor
    Co-Authored-By: Michele Riva <riva@iap.tuwien.ac.at>

--- tleedmlib/sections/rfactor.py
+++ tleedmlib/sections/rfactor.py
@@ -24,9 +24,10 @@ from viperleed.tleedmlib.checksums import validate_multiple_files
 try:
     from viperleed.tleedmlib.wrapped.rfactor import r_factor_new as rf
 except ImportError:
-    import warnings
-    warnings.warn("Unable to load compiled rfactor module. "
-                "Only legacy rfactor currently available.")
+    pass
+    # import warnings
+    # warnings.warn("Unable to load compiled rfactor module. "
+    #             "Only legacy rfactor currently available.")
 from viperleed.tleedmlib.wrapped.error_codes import error_codes, check_ierr
 
 
