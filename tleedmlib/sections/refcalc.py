942ca0d2dc974b2a90ba75a0f1f4cfd9dfad9b39
Michele Riva <riva@iap.tuwien.ac.at>
2022-12-20 18:22:43 +0100

    minor: remove non-occurring case
    Now that we pre-set rp.PHASESHIFT_EPS to its DEFAULT, it can never be outside the (0, 1) range.

--- tleedmlib/sections/refcalc.py
+++ tleedmlib/sections/refcalc.py
@@ -376,8 +376,6 @@ def refcalc(sl, rp, subdomain=False, parent_dir=""):
     if single_threaded or rp.LMAX[0] == rp.LMAX[1] or rp.TL_VERSION <= 1.6:
         which_lmax = set([rp.LMAX[1]])
     else:    # find appropriate LMAX per energy
-        if rp.PHASESHIFT_EPS == 0: #TODO: this should never happen
-            rp.PHASESHIFT_EPS = 0.01
         ps_en = [(i, ps[0]*27.211396) for (i, ps) in enumerate(rp.phaseshifts)]
         lmax = {}  # lmax as a function of energy
         warn_small = True
