ebb14180b35ca1487995ed0ddbd51769fd084969
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2022-12-22 11:23:06 +0100

    feat: full new beamgen

--- tleedmlib/beamgen.py
+++ tleedmlib/beamgen.py
@@ -9,6 +9,8 @@ import os
 import logging
 import subprocess
 import numpy as np
+from pathlib import Path
+
 from viperleed import fortranformat as ff
 
 from viperleed.guilib.base import get_equivalent_beams, BeamIndex
@@ -16,7 +18,7 @@ from viperleed.guilib.base import get_equivalent_beams, BeamIndex
 logger = logging.getLogger("tleedm.beamgen")
 
 _HARTREE_TO_EV = 27.211396
-_ANGSTROM_TO_BOHR = 0.529177210903
+_BOHR_TO_ANGSTROM = 0.529177210903
 
 
 def runBeamGen(sl, rp, beamgensource='', domains=False):
@@ -105,57 +107,121 @@ def runBeamGen(sl, rp, beamgensource='', domains=False):
     logger.debug("Wrote to BEAMLIST successfully.")
     return
 
-def beamgen(sl, rp, domain=False):
+def beamgen(sl, rp, domains=False, beamlist_name="BEAMLIST"):
+    """Calculates and writes the contents for the file BEAMLIST.
+
+    BEAMLIST contains a list of all diffraction beams that will be used 
+    for internal calculations (as opposed to IVBEAMS, which contains the
+    beams to be output). The file list the beams with indices as float
+    values and a lower cutoff energy below which the beam is evanescent
+    (i.e. does not leave the surface and has intensity 0). The used 
+    format is defined in make_beamlist_string and dictated by TensErLEED
+    and the legacy beamgen scripts that were used before.
+
+    NB: The energies calculated here are slightly higher than the values
+    from beamgenv3 (and beamgen.v1.7) because we use *more accurate*
+    values for unit conversions.The legacy code used these rounded
+    values:
+    HARTREE_TO_EV = 27.21
+    BOHR_TO_ANGSTROM = 0.529
+    Similarly, the list of included beams may be different for the same 
+    energy range, as the legacy code used rounded values and typecast a
+    cutoff from float to int.
+
+    In any case, this version should give more accurate energy values 
+    and be more generous in how many beams are considered.
+
+    Parameters
+    ----------
+    sl : Slab
+        Slab object.
+    rp : Rparams.
+        Run parameters.
+    domains : bool, optional
+        Flag to indicate if performing a domain calculation,
+        by default False.
+    beamlist_name : str, optional
+        Filename to be written, by default "BEAMLIST".
+    """
     if sl.bulkslab is None:
         sl.bulkslab = sl.makeBulkSlab(rp)
     
     e_max = rp.THEO_ENERGIES[1]
     surf_ucell = sl.surface_vectors
     inv_surf_vectors = sl.reciprocal_vectors
-    # TODO: domains !!
-    # if not domains:
-    #     dmin = sl.getMinLayerSpacing() * 0.7
-    # else:
-    #     dmin = min([dp.sl.getMinLayerSpacing() for dp in rp.domainParams])*0.7
 
-    d_min = sl.getMinLayerSpacing() * 0.7
+    if not domains:
+        d_min = sl.getMinLayerSpacing() * 0.7
+    else:
+        d_min = min([dp.sl.getMinLayerSpacing() for dp in rp.domainParams])*0.7
+
     conv_crit = rp.ATTENUATION_EPS  # convergence criterion for refcalc
-    g_max_squared = 2*e_max + (np.log(conv_crit)/d_min)**2
-    
+    # effective cutoff energy to use (scale to correct units)
+    e_max_eff = (2*e_max +
+                 (np.log(conv_crit)/d_min*_BOHR_TO_ANGSTROM)**2
+                 *_HARTREE_TO_EV)
+
+    # use guilib to generate list of beams
     leedParameters = {
-        'eMax': g_max_squared,
+        'eMax': e_max_eff,
         'surfBasis': surf_ucell,
         'SUPERLATTICE': rp.SUPERLATTICE,
         'surfGroup': sl.foundplanegroup,
         'bulkGroup': sl.bulkslab.foundplanegroup,
     }
-    # use guilib to generate list of beams
     equivalent_beams = get_equivalent_beams(leedParameters)
     # convert to float array
     indices = np.array(list(BeamIndex(beam[0]) for beam in equivalent_beams),
                        dtype="float64")
     # calculate cutoff energy for each beam
     energies = (np.sum(np.dot(indices, inv_surf_vectors)**2, axis=1)
-                /2 *_HARTREE_TO_EV *_ANGSTROM_TO_BOHR**2) # scale to correct units
-    # NB: The energies calculated here are slightly higher than
-    #     the values from beamgenv3 (and beamgen.v1.7) because we 
-    #     use *more accurate* values for unit conversions.
-    #     The legacy code used these rounded values:
-    #       HARTREE_TO_EV=27.21
-    #       ANGSTROM_TO_BOHR = 0.529
+                /2 *_HARTREE_TO_EV *_BOHR_TO_ANGSTROM**2)  # scale to correct units
     logger.debug(f"Highest energy considered in BEAMLIST={np.max(energies)}")
+
+    # write to file
+    beamlist_contents = make_beamlist_string(indices, energies)
+    write_file_path = Path(beamlist_name)
+    try:
+        with open(write_file_path, 'w') as file:
+            file.write(beamlist_contents)
+    except Exception:
+        logger.error(f"Unable to write file {beamlist_name}")
+        raise
+
+    logger.debug("Wrote to BEAMLIST successfully.")
+    return
 
 
 def make_beamlist_string(indices, energies):
+    """Creates contents for file BEAMLIST in the format used be the 
+    legacy beamgen scripts by U. Loeffler and R. Doell.
+
+    Parameters
+    ----------
+    indices : np.ndarray, shape=(n_beams, 2)
+        Indices (diffraction orders) of the beams.
+    energies : np.ndarray, shape=(n_beams,)
+        Lower cutoff energies for the beams.
+
+    Returns
+    -------
+    str
+        String representation of the contents of the BEAMLIST file.
+
+    Raises
+    ------
+    ValueError
+        If indices and energies have incompatible shapes.
+    """
     n_beams = indices.shape[0]
-    if not energies.shape[0] == n_beams:
+    if not energies.shape == (n_beams,) or not indices.shape == (n_beams, 2):
         raise ValueError(
             f"Incompatible size of indices (shape={indices.shape})"
             f"and energies (shape={energies.shape}).")
 
     # set up Fortran format as was used by beamgen
     beamlist_format = ff.FortranRecordWriter(
-        "2F10.5,2I3,10X,'E =  ',F10.4,' EV',2X,'NR.',I4"
+        "2F10.5,2I3,10X,'E =  ',F10.4, 2X,'NR.',I4"
         )
     # first line contains number of beams
     content = ff.FortranRecordWriter('10I3').write([n_beams]) + '\n'
