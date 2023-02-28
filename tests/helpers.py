759b1638d8207a9304530f496c7c333c2366677e
Michele Riva <riva@iap.tuwien.ac.at>
2023-02-28 20:39:43 +0100

    future: test helpers
    Currently empty, Can be used to collect various utilities used througout tests

---
+++ tests/helpers.py
@@ -0,0 +1,16 @@
+"""Module helpers of viperleed.tests.
+
+Created on 2023-02-28
+
+@author: Michele Riva
+
+Contains some useful general definitions that can be used when creating
+or running tests.
+"""
+
+
+# Think about a decorator for injecting fixtures.
+# Some ideas at
+# https://github.com/pytest-dev/pytest/issues/2424
+# https://github.com/pytest-dev/pytest/issues/6322
+# https://github.com/nteract/testbook/issues/4
