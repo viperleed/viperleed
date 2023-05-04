06e558c27d592ad921c2981af3b1e7882991a5c9
Michele Riva <riva@iap.tuwien.ac.at>
2023-05-04 17:45:57 +0200

    minor: whitespace
    Co-Authored-By: cpfungen <120585460+cpfungen@users.noreply.github.com>

--- hardware/arduino/spi_tle7209/TLE7209.h
+++ hardware/arduino/spi_tle7209/TLE7209.h
@@ -24,18 +24,20 @@ Date: 21.04.2023
 #define TLE7209_SPIMODE SPI_MODE1   // SPI_MODE1 should translate to CPOL = 0, CPHA = 1
 
 
-// TLE7209 SPI instruction-bye encoding
-#define TLE7209_READ_IDENTIFIER 0x00
-#define TLE7209_READ_VERSION 0x03
-#define TLE7209_READ_DIAG_REGISTER 0x09
+// TLE7209 SPI instruction-byte encoding
+#define TLE7209_READ_IDENTIFIER     0x00
+#define TLE7209_READ_VERSION        0x03
+#define TLE7209_READ_DIAG_REGISTER  0x09
+#define TLE7209_SPI_TRANSMISSION_OK 0b00101010
+#define TLE7209_DEFAULT_DEVICE_ID   0b10100010
 
 
 // TLE7209 diagnostic register definitions
-#define TLE7209_EN_DIS   0x80	// EN/DIS = 0 if EN = low or DIS = high 
+#define TLE7209_EN_DIS    0x80	// EN/DIS = 0 if EN = low or DIS = high 
 #define TLE7209_OVER_TEMP 0x40	// OT = 0 in case of over-temperature
-#define TLE7209_CURR_RED 0x20	// CurrRed = 0 in case of temperature-dependent current limitation
-#define TLE7209_CURR_LIM 0x10	// CurrLim = 0 in case of current limitation
 #define TLE7209_DIA21 0x08	// Diagnostic bit 2 of output OUT2
+#define TLE7209_CURR_RED  0x20	// CurrRed = 0 in case of temperature-dependent current limitation
+#define TLE7209_CURR_LIM  0x10	// CurrLim = 0 in case of current limitation
 #define TLE7209_DIA_20 0x04	// Diagnostic bit 1 of output OUT2
 #define TLE7209_DIA_11 0x02	// Diagnostic bit 2 of output OUT1
 #define TLE7209_DIA_10 0x01	// Diagnostic bit 1 of output OUT1
