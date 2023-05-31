9a5a6083530cad91254fe1476e4725ecb6456a81
Christoph Pfungen <e11812884@student.tuwien.ac.at>
2023-05-31 16:44:18 +0200

    add: gather all coil-relevant defines here

--- hardware/arduino/b_field_comp/b_field_comp.h
+++ hardware/arduino/b_field_comp/b_field_comp.h
@@ -21,4 +21,16 @@ Date: 16.05.2023
 extern byte set_coil_current(double, uint8_t);
 
 
+// The next two pins SHOULD POSSIBLY NOT BE CHANGED. Changing these
+// requires picking a different timer/counter module (currently TC4
+// TC1 or TC3)
+#define COIL_1  6   // PWM output, i.e., voltage value; Also A7;  PD7 on Atmega32U4
+#define COIL_2 10   // PWM output, i.e., voltage value; Also A10; PB6 on Atmega32U4
+
+// Current direction: positive or negative?
+#define COIL_1_SIGN 18  // Used for INA on shunt; PF7 on ATmega32U4 
+#define COIL_2_SIGN 19  // Used for INA on shunt; PF6 on ATmega32U4
+
+#define TLE_CHIPSELECT_1 11     // For SPI communication; PB7 on ATmega32U4 
+#define TLE_CHIPSELECT_2 12     // For SPI communication; PD6 on ATmega32U4 
 #endif
