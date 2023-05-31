27706675755ffeaf138bd50f2f45877b778e4791
Christoph Pfungen <e11812884@student.tuwien.ac.at>
2023-05-31 16:40:47 +0200

    removed comment, moved chipselect define to header

--- hardware/arduino/b_field_comp/b_field_comp.h
+++ hardware/arduino/b_field_comp/b_field_comp.h
@@ -15,9 +15,6 @@ Date: 16.05.2023
 #include "pwm.h"           // for set_pwm_frequency, set_coil_current
 #include "TLE7209.h"
 
-// TODO: move here all the coil-related defines, and make
-// them into a class that also contains relevant methods
-#define TLE_CHIPSELECT 11     // For SPI communication; Also PB7                // TODO: need two of these; both inside the coil class; name suggestion: driver_spi or something along these lines?
 
 
 
