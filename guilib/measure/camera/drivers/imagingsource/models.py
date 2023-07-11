3fe1aba64321c0d1a963ca6cf84d70245453482f
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2023-07-11 19:00:45 +0200

    new: IMX546 sensor & camera
    Added info of new camera sensor

--- guilib/measure/camera/drivers/imagingsource/models.py
+++ guilib/measure/camera/drivers/imagingsource/models.py
@@ -1,4 +1,4 @@
-"""Module ismodels of camera.drivers.imagingsource.
+"""Module models of camera.drivers.imagingsource.
 
 Contains useful data relative to specific camera models
 currently fabricated by The Imaging Source.
@@ -16,6 +16,7 @@ from enum import Enum
 class _Sensors(Enum):
     """Camera sensors. For now only dynamic range in bits is stored."""
 
+    IMX546 = (12,)
     IMX267 = (12,)
     IMX304 = (12,)
     ICX618 = (12,)
@@ -61,8 +62,9 @@ class ISModels(Enum):
     # disable the invalid-name in this case than rewriting
     # the attribute getter for the class.
 
+    DMK_33GX546 = _Sensors.IMX546
     DMK_38GX267 = _Sensors.IMX267
-    DMK_38GX304	= _Sensors.IMX304
+    DMK_38GX304 = _Sensors.IMX304
     DMK_33G618 = _Sensors.ICX618
     DMK_33GX287 = _Sensors.IMX287
     DMK_33GV024 = _Sensors.MT9V024
