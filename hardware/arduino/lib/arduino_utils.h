ba2d7be7b7732c9adc308c11b34a24f727111798
cpfungen <120585460+cpfungen@users.noreply.github.com>
2023-07-11 23:25:40 +0200

    feat: added helper functions
    'pin_to_tc4_reg_addr' and 'pin_to_tc4_channel' will convert from a pin number to the corresponding Timer/Counter4 register address or channel number.
    
    'analogReadMedian', 'getmedian16', 'bigger16' and 'biggest16' implement some refined flavour of 'analogRead'.

--- hardware/arduino/lib/arduino_utils.h
+++ hardware/arduino/lib/arduino_utils.h
@@ -25,4 +25,53 @@ void setChipSelectHigh(byte ioPin) {
 }
 
 
+// If pin definitions on the ATmega32U4 should ever change, 
+// the assignments below would have to change accordingly.
+uint8_t pin_to_tc4_reg_addr(uint8_t pwm_pin) {
+    switch(pwm_pin) {
+      case  6: return 0xD2;
+      case 10: return 0xD0;
+      case 13: return 0xCF;
+    }
+}
+
+
+// If pin definitions on the ATmega32U4 should ever change, 
+// the assignments below would have to change accordingly.
+uint8_t pin_to_tc4_channel(uint8_t pwm_pin) {
+    switch(pwm_pin) {
+      case  6: return TC4_PWM_CH_D;
+      case 10: return TC4_PWM_CH_B;
+      case 13: return TC4_PWM_CH_A;
+    }
+}
+
+
+uint16_t bigger16(uint16_t a, uint16_t b) {
+  return (a > b) ? a : b;
+}
+
+uint16_t biggest16(uint16_t a, uint16_t b, uint16_t c) {
+  return bigger16(a, bigger16(b, c));
+}
+
+/** Gets the median of three numbers */
+uint16_t getMedian16(uint16_t a0, uint16_t a1, uint16_t a2) {
+  uint16_t maximum = biggest16(a0, a1, a2);
+  if (maximum == a0) return bigger16(a1, a2);
+  if (maximum == a1) return bigger16(a0, a2);
+  else return bigger16(a0, a1);
+}
+
+uint16_t analogReadMedian(byte pin) {
+  /**
+  Read the Arduino ADC for a given pin
+  (A0...) three times and return the median
+  */
+  uint16_t a0 = analogRead(pin);
+  uint16_t a1 = analogRead(pin);
+  uint16_t a2 = analogRead(pin);
+  return getMedian16(a0, a1, a2);
+}
+
 #endif
