b5bf3fb6c5f8eb88228bd2bdf835075f087b1fe8
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2023-07-13 15:26:57 +0200

    test: tests for writePOSCAR()

--- tests/test_poscar.py
+++ tests/test_poscar.py
@@ -12,6 +12,8 @@ import os, sys
 from copy import deepcopy
 import numpy as np
 
+from unittest.mock import mock_open, patch
+
 vpr_path = str(Path(__file__).parent.parent.parent)
 if os.path.abspath(vpr_path) not in sys.path:
     sys.path.append(os.path.abspath(vpr_path))
@@ -20,7 +22,7 @@ import viperleed.tleedmlib.files.parameters as parameters
 from viperleed.tleedmlib.files.parameters import (readPARAMETERS,
                                                   ParameterInterpreter,
                                                   Assignment, NumericBounds)
-from viperleed.tleedmlib.files.poscar import readPOSCAR, writePOSCAR
+from viperleed.tleedmlib.files.poscar import readPOSCAR, writePOSCAR, ensure_away_from_c_edges,POSCARReader, POSCARSyntaxError, InvalidUnitCellError
 from viperleed.tleedmlib.classes.rparams import Rparams
 from viperleed.tests.helpers import (ag100_parameters_example,
                                      _FIXTURES_PATH,
@@ -50,3 +52,17 @@ class Test_writePOSCAR:
         slab = example_poscars
         writePOSCAR(slab, tmp_path / 'POSCAR')
         assert (tmp_path / 'POSCAR').exists()
+
+    def test_writePOSCAR_does_not_modify_slab(self, example_poscars, tmp_path):
+        slab = example_poscars
+        n_atoms_original = len(slab.atlist)
+        writePOSCAR(slab, tmp_path / 'POSCAR')
+        assert slab == example_poscars
+        assert len(slab.atlist) == n_atoms_original
+
+    def test_writePOSCAR_writes_correct_number_of_atoms(self, slab_and_expectations, tmp_path):
+        slab, expected_n_atoms, *_ = slab_and_expectations
+        writePOSCAR(slab, tmp_path / 'POSCAR')
+        # read the written POSCAR and check that the number of atoms is correct
+        written_slab = readPOSCAR(tmp_path / 'POSCAR')
+        assert len(written_slab.atlist) == expected_n_atoms
