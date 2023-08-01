e35f40fda7ff43d866553961eb1fa3e85093b8b5
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2023-08-01 10:22:52 +0200

    feat: create __main__.py

---
+++ viperleed/__main__.py
@@ -0,0 +1,13 @@
+"""
+=================
+    ViPErLEED
+=================
+"""
+from viperleed import GLOBALS
+
+def main():
+    print(f"ViPErLEED (Vienna Package for Erlangen LEED) v{GLOBALS['version']}\n"
+          "TODO: add help message here.")
+
+if __name__ == "__main__":
+    main()
