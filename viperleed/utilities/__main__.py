dc192b8263e94b708a546d7b09d8afc4bf4df232
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2023-08-02 15:51:06 +0200

    refactor: organize utilities

---
+++ viperleed/utilities/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""ViPErLEED utilities.
+"""
+
+print("ViPErLEED utilities.")                                                   # TODO: help menu and list of available utilities
