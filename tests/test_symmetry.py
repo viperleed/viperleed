dc502ca637ebd17db85ddebaad72ae1ac3ef8c59
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2023-09-05 15:03:39 +0200

    test,fix: moving fixtures

--- tests/test_symmetry.py
+++ tests/test_symmetry.py
@@ -16,15 +16,8 @@ vpr_path = str(Path(__file__).parent.parent.parent.parent)
 if os.path.abspath(vpr_path) not in sys.path:
     sys.path.append(os.path.abspath(vpr_path))
 
+from viperleed.tleedmlib.symmetry import findSymmetry
 
-from viperleed.tleedmlib.files.displacements import readDISPLACEMENTS, readDISPLACEMENTS_block
-from viperleed.tleedmlib.files.poscar import readPOSCAR
-from viperleed.tleedmlib.files.vibrocc import readVIBROCC
-from viperleed.tleedmlib.symmetry import findSymmetry, enforceSymmetry
-from viperleed.tleedmlib.psgen import runPhaseshiftGen_old
-from viperleed.tleedmlib.classes.atom import Atom
-from viperleed.tleedmlib.classes.rparams import Rparams
-from viperleed.tleedmlib.classes.slab import Slab
 
 class TestSymmetry():
     def test_any_pg_found(self, slab_pg_rp):
