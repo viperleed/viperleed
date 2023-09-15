48915849e96d3792a4d9d7ed170d15db23f235fb
Michele Riva <riva@iap.tuwien.ac.at>
2023-09-15 13:16:10 +0200

    test, refactor: cleanup test_beamgen
    Use all reasonable cases, not only a single one. This gives us the chance to easily add cases that show failures in the future.

--- tests/files/test_beamgen.py
+++ tests/files/test_beamgen.py
@@ -1,52 +1,93 @@
-"""Test module beamgen of viperleed.tests.
+"""Tests for module viperleed.tleedmlib.beamgen.
 
 Created on 2023-06-09
 
-@author: Alexander M. Imre
-
+@author: Alexander M. Imre (@amimre)
+@author: Michele Riva (@michele-riva)
 """
 
-import pytest
 from pathlib import Path
-import os, sys
-from copy import deepcopy
-import numpy as np
+import sys
 
-vpr_path = str(Path(__file__).parent.parent.parent)
-if os.path.abspath(vpr_path) not in sys.path:
-    sys.path.append(os.path.abspath(vpr_path))
+# pylint: disable=unused-import
+# This is a workaround to make sure we do point to the necessary
+# plugin. Will be resolved in installable by adding as a dependency
+import pytest_subtests                                                          # TODO: remove in installable
+# pylint: enable=unused-import
+from pytest_cases import fixture, parametrize_with_cases
 
-from viperleed.tleedmlib.beamgen import get_beam_scattering_subsets
-from viperleed.tleedmlib.beamgen import make_beamlist_string
-from viperleed.tleedmlib.beamgen import calc_and_write_beamlist
+VPR_PATH = str(Path(__file__).resolve().parents[2])
+if VPR_PATH not in sys.path:
+    sys.path.append(VPR_PATH)
 
-_FIXTURES_PATH = Path('tests/fixtures/')
+# pylint: disable=wrong-import-position
+# Will be fixed in installable
+from viperleed.tleedmlib import beamgen, symmetry
+
+from .helpers import CaseTag, exclude_tags
+from .poscar_slabs import CasePOSCARSlabs
+# pylint: enable=wrong-import-position
+
 
 class TestBeamScatteringSubsets:
-    def test_get_beam_scattering_subsets_integer_only(self):
+    """Collection of tests for sorting of beams into subsets."""
+
+    def test_get_beam_scattering_subsets_integer_only(self, subtests):
+        """Test correct identification for integer-order beams."""
         beam_indices_raw = [(0, 0), (1, 0), (2, 1), (1, 5)]
         expected_subset_classes = [(0, 0),]
         expected_reduced_indices = [(0, 0), (0, 0), (0, 0), (0, 0)]
 
-        subset_classes, reduced_indices = get_beam_scattering_subsets(beam_indices_raw)
-        assert subset_classes == expected_subset_classes
-        assert reduced_indices == expected_reduced_indices
+        (classes,
+         indices) = beamgen.get_beam_scattering_subsets(beam_indices_raw)
+        with subtests.test('Integer beams - classes'):
+            assert classes == expected_subset_classes
+        with subtests.test('Integer beams - indices'):
+            assert indices == expected_reduced_indices
 
-
-    def test_get_beam_scattering_subsets_fractional(self):
+    def test_get_beam_scattering_subsets_fractional(self, subtests):
+        """Test correct identification for integer and fractional beams."""
         beam_indices_raw = [(0, 0), (1, 0), (1/2, 1/2), (1, 1/5)]
-        expected_subset_classes = [(0, 0),(0, 1/5), (1/2, 1/2)] # sorted by |k| !
-        expected_reduced_indices = [(0, 0), (0, 0), (1/2, 1/2), (0, 1/5)]
+        expected_classes = [(0, 0), (0, 1/5), (1/2, 1/2)]  # By |k|
+        expected_indices = [(0, 0), (0, 0), (1/2, 1/2), (0, 1/5)]
 
-        subset_classes, reduced_indices = get_beam_scattering_subsets(beam_indices_raw)
-        assert subset_classes == expected_subset_classes
-        assert reduced_indices == expected_reduced_indices
+        (classes,
+         indices) = beamgen.get_beam_scattering_subsets(beam_indices_raw)
+        with subtests.test('Integer and fractional beams - classes'):
+            assert classes == expected_classes
+        with subtests.test('Integer and fractional beams - indices'):
+            assert indices == expected_indices
 
 
+_BEAMGEN_CASES = {'cases': CasePOSCARSlabs,
+                  'filter': exclude_tags(CaseTag.NO_INFO)}                      # TODO: not great to exclude these, but there's a few that really need PARAMETERS
 
-def test_generate_beamlist(tmp_path, ag100_parameters_example):
-    rp, sl = ag100_parameters_example
-    beamlist_path = tmp_path / 'BEAMLIST'
-    sl.createLayers(rp)
-    calc_and_write_beamlist(sl, rp, beamlist_name = beamlist_path)
-    assert beamlist_path.exists()
+
+class TestGenerateBeamlist:
+    """Collection of tests for the generation of beam lists."""
+
+    @fixture(scope='class', name='make_beamlist')
+    @parametrize_with_cases('args', scope='class', **_BEAMGEN_CASES)
+    def fixture_make_beamlist(self, args, tmp_path_factory):
+        """Return slab, parameters, info and the path to a 'BEAMLIST'."""
+        slab, param, info = args
+        slab.createLayers(param)
+        slab.bulkslab = slab.makeBulkSlab(param)
+        symmetry.findSymmetry(slab, param)
+        symmetry.findSymmetry(slab.bulkslab, param, bulk=True)
+        symmetry.findBulkSymmetry(slab.bulkslab, param)
+
+        param.initTheoEnergies()
+
+        tmp_dir = tmp_path_factory.mktemp(
+            basename=f'{info.poscar.name}_beamlist',
+            numbered=True
+            )
+        beamlist = tmp_dir / 'BEAMLIST'
+        beamgen.calc_and_write_beamlist(slab, param, beamlist_name=beamlist)
+        return slab, param, info, beamlist
+
+    def test_generate_beamlist(self, make_beamlist):
+        """Check successful generation of a 'BEAMLIST' file."""
+        *_, beamlist = make_beamlist
+        assert beamlist.exists()                                                # TODO: may want to check it's not empty
