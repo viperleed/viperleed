ac9556623d92c993761ae6c495e3386eae204f3c
Michele Riva <riva@iap.tuwien.ac.at>
2023-09-15 13:04:49 +0200

    test, refactor: cleanup test_r_error
    TODO @amimre: Would be good to include one test for the bug that you recently fixed in viperleed/commit/29adfd79fac3785274bfb2d982398a06142315b3
    
    That corner case was clearly not covered by the tests here.

--- tests/classes/test_r_error.py
+++ tests/classes/test_r_error.py
@@ -1,33 +1,48 @@
-import pytest
-import shutil, tempfile
-import sys
-import os
+"""Tests for viperleed.tleedmlib.classes.RError.
+
+Created on 2023
+
+@author: Alexander M. Imre (@amimre)
+@author: Michele Riva (@michele-riva)
+"""
+
 from pathlib import Path
-from zipfile import ZipFile
-from copy import deepcopy
+import sys
+
 import numpy as np
+import pytest
 
-vpr_path = str(Path(__file__).parent.parent.parent)
-if os.path.abspath(vpr_path) not in sys.path:
-    sys.path.append(os.path.abspath(vpr_path))
+VPR_PATH = str(Path(__file__).resolve().parents[3])
+if VPR_PATH not in sys.path:
+    sys.path.append(VPR_PATH)
+
+# pylint: disable=wrong-import-position
+# Will be fixed in installable version
+from viperleed.tleedmlib.classes import r_error
+# pylint: enable=wrong-import-position
 
 
-from viperleed import tleedmlib
-import viperleed.tleedmlib
+class TestZeroCrossings:
+    """Tests for methods detecting zeros."""
 
+    _x_only = (  # x_values, nr_expected_crossings
+        (np.array([-1, 1, -1]),       2),
+        (np.array([0, 0, 0]),         1),
+        ([-1, 0, -1],                 1),
+        ([-1, 1, 10, 2, -50, 0.1, 0], 3),
+        )
 
-from viperleed.tleedmlib.classes.r_error import get_n_zero_crossings,get_zero_crossing
+    @pytest.mark.parametrize('x_vals,n_crossings', _x_only)
+    def test_nr_zero_crossings(self, x_vals, n_crossings):
+        """Check that the number of detected crossings is correct."""
+        assert r_error.get_n_zero_crossings(x_vals) == n_crossings
 
-@pytest.mark.parametrize('x_arr,n_crossings', ((np.array([-1, 1, -1]),2 ),
-                                               (np.array([0, 0, 0]), 1),
-                                               ([-1, 0, -1], 1),
-                                               ([-1, 1, 10, 2, -50, 0.1, 0], 3))
-)
-def test_get_n_zero_crossings(x_arr, n_crossings):
-    assert get_n_zero_crossings(x_arr) == n_crossings
+    _x_and_y = (  # (x_values, y_values), cross_x_position
+        (([0, 1, 2],       [-1, 0, 1]),         1),
+        (([0, 1, 2, 3, 4], [-1, -3, -2, 2, 5]), 2.5),
+        )
 
-@pytest.mark.parametrize('x_arr,y_arr,crossing_x', (([0, 1, 2], [-1, 0, 1], 1),
-                                                    ([0, 1, 2, 3, 4], [-1, -3, -2, 2, 5], 2.5))
-                        )
-def test_get_zero_crossing(x_arr, y_arr, crossing_x):
-    assert get_zero_crossing(x_arr, y_arr) == crossing_x
+    @pytest.mark.parametrize('xy_vals,crossing_x', _x_and_y)
+    def test_pos_zero_crossing(self, xy_vals, crossing_x):
+        """Check that the x position of the detected crossing is correct."""
+        assert r_error.get_zero_crossing(*xy_vals) == crossing_x
