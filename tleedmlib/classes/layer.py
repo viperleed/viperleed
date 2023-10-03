3d98adb911706f9cf74c6515295672e38a8daa2e
Michele Riva <riva@iap.tuwien.ac.at>
2023-10-03 18:10:29 +0200

    minor, style: docstrings
    I would very much need some help from @fkraushofer to describe clearly the .cartori attribute, as it is a bit confusing for me.

--- tleedmlib/classes/layer.py
+++ tleedmlib/classes/layer.py
@@ -1,21 +1,48 @@
 # -*- coding: utf-8 -*-
-"""
+"""Module layer of viperleed.tleedmlib.classes.
+
 Created on Jun 13 2019
 
-@author: Florian Kraushofer
+@author: Florian Kraushofer (@fkraushofer)
+@author: Michele Riva (@michele-riva)
 
-Class storing position and atom list of a layer
+Classes storing position and atom list of a Layer, and its SubLayer
+subclass. The latter is for atoms of the same chemical species at
+the same z position.
 """
 
 import numpy as np
-# from tleedmlib import DEFAULT
 
 
 class Layer:
-    """To be used with Slab; has origin, atoms (a subset of the ones in slab),
-    and a number. Sublayers also use the "Layer" class."""
+    """A container of atoms residing close to one another along z.
+
+    This is intended to be used with Slab objects. Has origin, atoms
+    (a subset of the ones in slab), and a number.
+
+    Attributes
+    ----------
+    atlist : list of Atom
+        The atoms that belong to this layer.
+    cartori : numpy.ndarray                                                     @fkraushofer: could you please elaborate a bit more on how (x,y) and z are defined for this one? It's not 100% clear to me.
+        The Cartesian origin of this layer. This is derived from
+        the position of its highest atom. A call to update_position()
+        updates this attribute.
+    cartbotz : float
+        Z (i.e., out-of-plane) position of the bottom-most atom in
+        the layer. A call to update_position() updates this attribute.
+    is_bulk : bool
+        Whether this layer has bulk character.
+    num : int
+        A progressive index (zero-based) identifying this layer
+        within its slab. Normally, layer.num == 0 for the layer
+        closest to the solid/vacuum interface.
+    slab : Slab
+        The slab to which this layer belongs.
+    """
 
     def __init__(self, slab, num, isBulk=False, sublayer=False):
+        """Initialize instance."""
         self.slab = slab
         self.num = num      # consecutive layer numbering, 0 being highest
         self.isBulk = isBulk    # defined by BLAY in PARAMETERS file
