60dccdfc8363312232457297abe89fc34cd08bd9
Michele Riva <riva@iap.tuwien.ac.at>
2023-10-04 10:30:06 +0200

    refactor, minor: better __doc__, TODOs
    Especially, clarify the description of cartori, as well as the differences with SubLayer.cartpos

--- tleedmlib/classes/layer.py
+++ tleedmlib/classes/layer.py
@@ -20,7 +20,7 @@ class LayerHasNoAtomsError(LayerError):
     """Operation cannot be performed as the layer is empty."""
 
 
-class Layer:
+class Layer:                                                                    # TODO: modify description of .cartori when flipping .cartpos[2]
     """A container of atoms residing close to one another along z.
 
     This is intended to be used with Slab objects. Has origin, atoms
@@ -30,10 +30,13 @@ class Layer:
     ----------
     atlist : list of Atom
         The atoms that belong to this layer.
-    cartori : numpy.ndarray                                                     @fkraushofer: could you please elaborate a bit more on how (x,y) and z are defined for this one? It's not 100% clear to me.
-        The Cartesian origin of this layer. This is derived from
-        the position of its highest atom. A call to update_position()
-        updates this attribute.
+    cartori : numpy.ndarray
+        The Cartesian origin of this layer. It is the position where a
+        plane passing through the topmost atom in this layer intersects
+        the c vector of the slab's unit cell. Notice that `cartori` is
+        in the same Cartesian frame as the atoms (i.e., z increases
+        going from vacuum into the solid).
+        A call to update_position() updates this attribute.
     cartbotz : float
         Z (i.e., out-of-plane) position of the bottom-most atom in
         the layer. A call to update_position() updates this attribute.
@@ -41,7 +44,7 @@ class Layer:
         Whether this layer has bulk character.
     num : int
         A progressive index (zero-based) identifying this layer
-        within its slab. Normally, layer.num == 0 for the layer
+        within its slab. Normally, `layer.num == 0` for the layer
         closest to the solid/vacuum interface.
     slab : Slab
         The slab to which this layer belongs.
@@ -53,10 +56,14 @@ class Layer:
         self.num = num
         self.is_bulk = is_bulk
         self.atlist = []
-        # cartesian origin: xy from POSCAR with possible displacements,
-        #  z is highest atom
+
+        # Note about the next two: they should be kept in memory,
+        # rather than re-determined on-the-fly, since the position
+        # of the atoms in question may change during a search, but
+        # TensErLEED expects atom positions to be referred to the
+        # layer position as it was used in the reference calculation
         self.cartori = None
-        self.cartbotz = None    # z position of lowest atom
+        self.cartbotz = None
 
     def __iter__(self):
         """Return an iterator of Atoms in this Layer."""
@@ -67,16 +74,9 @@ class Layer:
         """Return the number of atoms in this layer."""
         return len(self.atlist)
 
+
     def update_position(self):
-        """Update the Cartesian position of this layer.
-
-        The z of the highest atom is used. x, y are calculated
-        from the origin of an a, b unit cell at that height.
-
-        Returns
-        -------
-        None.
-        """
+        """Update the Cartesian position of this layer from its atoms."""
         if not self.atlist:
             raise LayerHasNoAtomsError(
                 f'{type(self).__name__} needs atoms to update_position()'
@@ -85,16 +85,17 @@ class Layer:
         topat = sorted_atoms[-1]
         botat = sorted_atoms[0]
 
-        c_vec = self.slab.ucell.T[2]
-        self.cartori = topat.pos[2] * c_vec
-        # this gets x and y correct, but z still in the wrong direction and
-        #  with origin as POSCAR
-        # -> just take the z from the highest atom.
-        self.cartori[2] = topat.cartpos[2]
         self.cartbotz = botat.cartpos[2]
 
+        c_vec = self.slab.ucell.T[2]
+        self.cartori = topat.pos[2] * c_vec
+        # So far x and y are correct, but z is still in the wrong               # TODO: this will not be necessary when we flip .cartpos[2]
+        # direction and with origin as POSCAR. Take the z directly
+        # from the highest atom
+        self.cartori[2] = topat.cartpos[2]
 
-class SubLayer(Layer):
+
+class SubLayer(Layer):                                                          # TODO: modify description of .cartori when flipping .cartpos[2]
     """A Layer with the same chemical element and the same z position.
 
     Attributes
@@ -102,24 +103,33 @@ class SubLayer(Layer):
     atlist : list of Atom
         The atoms that belong to this sublayer.
     cartori : numpy.ndarray
-        The Cartesian origin of this sublayer. A call to
-        update_position() updates this attribute.
+        The Cartesian origin of this layer. It is the position where a
+        plane passing through the topmost atom in this layer intersects
+        the c vector of the slab's unit cell. Notice that `cartori` is
+        in the same Cartesian frame as the atoms (i.e., z increases
+        going from vacuum into the solid).
+        A call to update_position() updates this attribute.
     cartbotz : float
         Z (i.e., out-of-plane) position of the bottom-most atom in
         the layer. A call to update_position() updates this attribute.
     cartpos : numpy.ndarray
-        The Cartesian position of this sublayer. This differs from
-        cartori, as the latter...                                               # TODO: In need a bit better explanation of cartori from @fkraushofer to detail the difference
+        The Cartesian position of this sublayer. Notice that this is
+        not an alias for `cartori`. The (x, y) components of the two
+        are not the same: `cartori[:2]` is a point along the c vector
+        of the unit cell, while `cartpos[:2]` may be a point inside
+        (or outside if atoms are not collapsed) the unit cell.
     element : str
         The chemical element of the atoms of this sublayer.
     is_bulk : bool
         Whether this layer has bulk character.
     num : int
         A progressive index (zero-based) identifying this sublayer
-        within its slab. Normally, sublayer.num == 0 for the layer
-        closest to the solid/vacuum interface.
+        within its slab. Normally, `sublayer.num == 0` for the
+        sublayer closest to the solid/vacuum interface.
     pos : numpy.ndarray
-        The fractional position of this sublayer.
+        The fractional position of this sublayer. This is the
+        fractional position corresponding to `cartpos`, not the
+        one corresponding to `cartori`.
     slab : Slab
         The slab to which this sublayer belongs.
     symposlist : list
