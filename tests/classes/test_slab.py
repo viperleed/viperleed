69c49f4282324dddc2c8fbe7defc8a603176de4f
Michele Riva <riva@iap.tuwien.ac.at>
2023-10-05 12:53:00 +0200

    refactor, tests: reorganize and adapt slab tests
    - Re-sort around tests
    - Adapt tests prepared by @amimre to current renaming of slab methods

--- tests/classes/test_slab.py
+++ tests/classes/test_slab.py
@@ -7,6 +7,7 @@ Created on 2023-07-28
 """
 
 from copy import deepcopy
+import operator
 from pathlib import Path
 import sys
 
@@ -22,6 +23,7 @@ from ..poscar_slabs import POSCARS_WITHOUT_INFO, AG_100
 
 # pylint: disable=wrong-import-position
 # Cannot do anything about it until we make viperleed installable
+from viperleed.tleedmlib.base import pairwise
 from viperleed.tleedmlib.classes.atom import Atom
 from viperleed.tleedmlib.classes.slab import Slab
 from viperleed.tleedmlib.classes.sym_entity import SymPlane
@@ -53,22 +55,62 @@ class TestAtomTransforms:                                                       
             )
 
 
-class TestUnitCellTransforms:
-    """Test simple transformations of the unit cell of a slab."""
+class TestAtomsAndElements:
+    """Collection of tests for atom additions/removals."""
 
-    def test_rotation_on_trigonal_slab(self, manual_slab_1_atom_trigonal):
-        """Test application of a rotation to a trigonal slab."""
-        rot_15 = [[0.96592583, -0.25881905,  0.],
-                  [0.25881905,  0.96592583,  0.],
-                  [0.,  0.,  1.]]
-        expected_cell = [[0.96592583,  0.25881905,  0.],
-                         [-2.99808654, 2.30249368,  0.],
-                         [0.44828774,  2.1906707,   3.]]
-        expected_atom_cartpos = [-1.86064664,  1.88257645]
-        slab = manual_slab_1_atom_trigonal
-        slab.apply_matrix_transformation(rot_15)
-        assert np.allclose(slab.ucell.T, expected_cell)
-        assert np.allclose(slab.atlist[0].cartpos[:2], expected_atom_cartpos)
+    def test_empty_slab(self):
+        """Check that an empty slab has no atoms, layers, etc..."""
+        slab = Slab()
+        assert slab.atlist == []
+        assert slab.elements == ()
+        assert slab.layers == []
+        assert slab.planegroup == 'unknown'
+
+    def test_add_one_atom_n_elements(self):
+        """Check that adding one atom to a slab updates elements correctly."""
+        slab = Slab()
+        new_atom = Atom('C', (0, 0, 0), 1, slab)
+        slab.atlist.append(new_atom)
+        slab.update_element_count()
+        assert new_atom.el in slab.elements
+        assert slab.n_per_elem[new_atom.el] == 1
+
+    def test_remove_one_atom_n_elements(self, make_poscar):
+        """Check that removing one atom updates elements correctly."""
+        slab, *_ = make_poscar(AG_100)
+        n_ag_atoms = slab.n_per_elem['Ag']
+        slab.atlist.pop()
+        slab.update_element_count()
+        assert slab.n_per_elem['Ag'] == n_ag_atoms - 1
+
+
+class TestCoordinates:
+    """Collection of tests for Cartesian/fractional coordinates."""
+
+    def test_cartesian_from_fractional(self, manual_slab_3_atoms):              # TODO: also update_origin
+        """Check correct update of Cartesian atom coordinates."""
+        slab = manual_slab_3_atoms
+        atom = slab.atlist[0]
+        atom.pos = np.array([0.1, 0.2, 0.3])
+        slab.update_cartesian_from_fractional()
+        assert atom.cartpos == pytest.approx([0.3, 0.8, -1.5])
+
+
+    def test_collapse_fractional(self, manual_slab_3_atoms):                    # TODO: use both methods. Find especially cases that are 'problematic' with %1.0: e.g., 1-1e-8, 1-1e-9, 1-1e-15
+        """Check that fractional coordinates are correctly collapsed."""
+        slab = manual_slab_3_atoms
+        atom = slab.atlist[0]
+        atom.pos = np.array([1.1, 2.2, -3.3])
+        slab.collapse_fractional_coordinates()
+        assert atom.pos == pytest.approx([0.1, 0.2, 0.7])
+
+    def test_fractional_from_cartesian(self, manual_slab_3_atoms):
+        """Check correct update of fractional atom coordinates."""
+        slab = manual_slab_3_atoms
+        atom = slab.atlist[0]
+        atom.cartpos = np.array([0.3, 0.8, -1.5])
+        slab.update_fractional_from_cartesian()
+        assert atom.pos == pytest.approx([0.1, 0.2, 0.3])
 
 
 # This class currently contains NO TESTS, as the previous tests were
@@ -109,85 +151,73 @@ class TestRestoreOristate:
                 assert offs_1[element] == pytest.approx(offs_2[element])
 
 
-def test_add_one_atom_n_elements():
-    """Check that adding one atom to a slab updates elements correctly."""
-    slab = Slab()
-    new_atom = Atom('C', (0, 0, 0), 1, slab)
-    slab.atlist.append(new_atom)
-    slab.update_element_count()
-    assert new_atom.el in slab.elements
-    assert slab.n_per_elem[new_atom.el] == 1
 
-class TestSlab:
-    """Test for the Slab class."""
-
-    def test_empty_slab(self):
-        """Check that an empty slab has no atoms."""
-        slab = Slab()
-        assert slab.atlist == []
-        assert slab.elements == ()
-        assert slab.layers == []
-        assert slab.planegroup == 'unknown'
-
-    def test_slab_getCartesianCoordinates(self, manual_slab_3_atoms):
-        slab = manual_slab_3_atoms
-        slab.atlist[0].pos = np.array([0.1, 0.2, 0.3])
-        slab.getCartesianCoordinates()
-        assert slab.atlist[0].cartpos == pytest.approx([0.3, 0.8, -1.5])
-
-    def test_slab_getFractionalCoordinates(self, manual_slab_3_atoms):
-        slab = manual_slab_3_atoms
-        slab.atlist[0].cartpos = np.array([0.3, 0.8, -1.5])
-        slab.getFractionalCoordinates()
-        assert slab.atlist[0].pos == pytest.approx([0.1, 0.2, 0.3])
-
-    def test_slab_collapseFractionalCoordinates(self, manual_slab_3_atoms):
-        slab = manual_slab_3_atoms
-        slab.atlist[0].pos = np.array([1.1, 2.2, -3.3])
-        slab.collapseFractionalCoordinates()
-        assert slab.atlist[0].pos == pytest.approx([0.1, 0.2, 0.7])
+class TestSorting:
+    """Collection of tests for slab sorting."""
 
     @parametrize(info=POSCARS_WITHOUT_INFO)
-    def test_slab_sort_by_z(self, info, make_poscar):
+    @parametrize(bottom_to_top=(True, False))
+    def test_z_sort(self, info, bottom_to_top, make_poscar):
+        """Check successful sorting of atoms by out-of-plane position."""
         slab, *_ = make_poscar(info)
-        slab.sort_by_z()
-        assert all(at1.pos[2] <= at2.pos[2]
-                   for at1, at2 in zip(slab.atlist, slab.atlist[1:]))
+        slab.sort_by_z(bottom_to_top=bottom_to_top)
+        _ordered = operator.ge if bottom_to_top else operator.le                    # TODO: swap when flipping .cartpos
+        assert all(_ordered(at1.cartpos[2], at2.cartpos[2])
+                   for at1, at2 in pairwise(slab))
 
-    def test_updateElementCount(self, make_poscar):
-        slab, *_ = make_poscar(AG_100)
-        n_ag_atoms = slab.n_per_elem['Ag']
-        slab.atlist.pop()
-        slab.updateElementCount()
-        assert slab.n_per_elem['Ag'] == n_ag_atoms - 1
+    @pytest.mark.skip(reason='to be implemented')
+    def test_element_sort(self):
+        """Check correct element-based sorting of a Slab."""
 
-class TestSlabUcell:
-    """Test for the Slab.ucell property."""
-    def test_slab_ucell(self, manual_slab_3_atoms):
-        slab = manual_slab_3_atoms
-        assert slab.ucell.shape == (3, 3)
 
-    def test_apply_scaling_scalar(self, manual_slab_3_atoms):
-        slab = manual_slab_3_atoms
-        slab.apply_scaling(2)
-        assert slab.ucell == pytest.approx(np.array([[6, 0, 0],
-                                                     [0, 8, 0],
-                                                     [0, 0, 10]]))
 
-    def test_apply_scaling_vector(self, manual_slab_3_atoms):
-        slab = manual_slab_3_atoms
-        slab.apply_scaling(1/3, 3.14, 0.1)
-        assert slab.ucell == pytest.approx(np.array([[1, 0, 0],
-                                                     [0, 12.56, 0],
-                                                     [0, 0, 0.5]]))
 
-    def test_angle_between_ucell_and_coord_sys_0(self, manual_slab_3_atoms):
+
+
+
+
+class TestUnitCellTransforms:
+    """Test simple transformations of the unit cell of a slab."""
+
+    def test_ucell_array(self, manual_slab_3_atoms):
+        """Check that the array shape of the unit cell is as expected."""
+        assert manual_slab_3_atoms.ucell.shape == (3, 3)
+
+    def test_angle_between_ucell_and_coord_sys_zero(self, manual_slab_3_atoms):
+        """Check correct identification of the rotation of the a vector."""
         slab = manual_slab_3_atoms
         assert slab.angle_between_ucell_and_coord_sys == pytest.approx(0)
 
-    def test_angle_between_ucell_and_coord_sys_30(self, manual_slab_3_atoms):
+    def test_angle_between_ucell_and_coord_sys_30(self):
+        """Check correct identification of the rotation of the a vector."""
         slab = Slab()
         slab.ucell = np.array([[np.cos(np.pi/6), np.sin(np.pi/6), 0],
                                [np.cos(np.pi/3*4), np.sin(np.pi/3*4), 0],
                                [0, 0, 1]]).T
         assert slab.angle_between_ucell_and_coord_sys == pytest.approx(30)
+
+    _scalings = {  # Applied, expected result
+        'scalar': ((2,),  np.diag((6, 8, 10))),
+        'vector': ((1/3, 3.14, 0.1), np.diag((1, 12.56, 0.5)))
+        }
+
+    @parametrize('scaling,expected', _scalings.values(), ids=_scalings)
+    def test_apply_scaling(self, scaling, expected, manual_slab_3_atoms):
+        slab = manual_slab_3_atoms
+        slab.apply_scaling(*scaling)
+        assert slab.ucell == pytest.approx(expected)
+
+    def test_rotation_on_trigonal_slab(self, manual_slab_1_atom_trigonal):
+        """Test application of a rotation to a trigonal slab."""
+        rot_15 = [[0.96592583, -0.25881905,  0.],
+                  [0.25881905,  0.96592583,  0.],
+                  [0.,  0.,  1.]]
+        expected_cell = [[0.96592583,  0.25881905,  0.],
+                         [-2.99808654, 2.30249368,  0.],
+                         [0.44828774,  2.1906707,   3.]]
+        expected_atom_cartpos = [-1.86064664,  1.88257645]
+        slab = manual_slab_1_atom_trigonal
+        slab.apply_matrix_transformation(rot_15)
+        assert np.allclose(slab.ucell.T, expected_cell)
+        assert np.allclose(slab.atlist[0].cartpos[:2], expected_atom_cartpos)
+
