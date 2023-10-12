928e5540574129dcd24de5e752053f0a85e4bf82
Michele Riva <riva@iap.tuwien.ac.at>
2023-10-12 11:16:35 +0200

    refactor: POSCAR_get_bulk_repeat
    Clean up code, pylint, readability improvements. There should hopefully be no functional changes, as it was primarily about renaming stuff and adding comments.
    
    If @amimre has some tests for this utility in the dedicated branch, it may be useful to run them. I'd happy to fix bugs I may have introduced.

--- POSCAR_get_bulk_repeat.py
+++ POSCAR_get_bulk_repeat.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
+"""Utility POSCAR_get_bulk_repeat.
+
 Created on Wed Dec 16 17:45 2019
 
-@author: Florian Kraushofer
+@author: Florian Kraushofer (@fkraushofer)
+@author: Michele Riva (@michele-riva)
 
-Reads a POSCAR file, asks at what c value the bulk starts, then automatically
-reduces the size of the POSCAR to non-redundant bulk layers only, and outputs
-the appropriate N_BULK_LAYERS and BULK_REPEAT values.
+Reads a POSCAR file, asks at what c value the bulk starts, then
+automatically reduces the size of the POSCAR to non-redundant bulk
+layers only, and outputs the appropriate N_BULK_LAYERS and BULK_REPEAT
+values.
 """
 
 import copy
@@ -20,77 +23,163 @@ from viperleed.tleedmlib.files import poscar
 from viperleed.tleedmlib.files.woods_notation import writeWoodsNotation
 
 
-def main():
-    # print some info
-    print("This utility reads a POSCAR file with arbitrary thickness and "
-          "orientation of the bulk, requiring only an input where the bulk "
-          "starts. The bulk repeat unit is then automatically determined. "
-          "Output is a new POSCAR with a minimum amount of bulk, and values "
-          "for the BULK_REPEAT and N_BULK_LAYERS parameters.\n")
-
-    # read the POSCAR file
-    filename = ""
-    while not filename:
-        filename = input("Enter POSCAR file name (Default: [POSCAR]): ")
-        if not filename:
-            filename = "POSCAR"
-        try:
-            sl = poscar.read(filename=filename)
-        except FileNotFoundError:
-            print(f"File {filename} not found.")
-            filename = ""
-        except Exception:
-            print("Exception while reading POSCAR file")
-            return 1
-
-    # print some info
-    print("Slab POSCAR was read successfully.")
-
-    cut = -1
+def ask_user_bulk_cut():
+    """Return a valid fractional cut position after asking the user."""
+    cut = -1.0
     while cut < 0:
-        cutstr = input("Enter c value at which bulk starts: ")
-        if cutstr != "":
-            try:
-                cut = float(cutstr)
-            except ValueError:
-                print("Failed to convert input to float.")
-                cut = -1
-            else:
-                if not (0 <= cut < 1):
-                    print("Error: c value has to be between 0 and 1.")
-                    cut = -1
+        cutstr = input('Enter c value at which bulk starts: ')
+        if not cutstr:
+            continue
+        try:
+            cut = float(cutstr)
+        except ValueError:
+            print('Failed to convert input to float.')
+            continue
+        if not 0 <= cut < 1:
+            print('Error: c value has to be between 0 and 1.')
+            cut = -1.0
+    return cut
 
-    eps = -1
+
+def ask_user_confirmation_to_cut(bulk_interlayer, bulk_cuts):
+    """Ask confirmation if cutting at a somewhat narrow layer spacing."""
+    if not 0.7 < bulk_interlayer < 1.2:
+        return
+
+    dec = None
+    while dec is None:
+        reply = input('Cutting the bulk into two layers is possible '
+                      f'with a spacing of {round(bulk_interlayer, 2)} A. '
+                      'Proceed? (y/[n]): ')
+        reply = reply.lower()
+        if not reply or reply.startswith('n'):
+            dec = False
+        elif reply.startswith('y'):
+            dec = True
+    if not dec:
+        bulk_cuts.pop()  # Keep only the top one
+
+
+def ask_user_symmetry_eps():
+    """Return tolerance for symmetry search after asking the user."""
+    eps = -1.0
     while eps < 0:
-        s = input("Enter tolerances for symmetry search (Default: [0.1 A]): ")
-        if s == "":
+        reply = input('Enter tolerances for symmetry search '
+                      '(Default: [0.1 A]): ')
+        if not reply:
             eps = 0.1
-        else:
-            try:
-                eps = float(s)
-            except ValueError:
-                print("Could not convert value to float.")
-            else:
-                if eps < 0:
-                    print("Value has to be greater than zero.")
+            break
+        try:
+            eps = float(reply)
+        except ValueError:
+            print('Could not convert value to float.')
+            continue
+        if eps < 0:
+            print('Value has to be greater than zero.')
+    return eps
 
-    rp = Rparams()
-    rp.BULK_LIKE_BELOW = cut
-    rp.SYMMETRY_EPS = rp.SYMMETRY_EPS_Z = eps
+
+def print_utility_description():
+    """Print some introductory information about this utility."""
+    print('This utility reads a POSCAR file with arbitrary thickness and '
+          'orientation of the bulk, requiring only an input where the bulk '
+          'starts. The bulk repeat unit is then automatically determined. '
+          'Output is a new POSCAR with a minimum amount of bulk, and values '
+          'for the BULK_REPEAT and N_BULK_LAYERS parameters.\n')
+
+
+def read_user_poscar():
+    """Return a Slab read from a POSCAR file after asking the user."""
+    # read the POSCAR file
+    filename = ''
+    while not filename:
+        filename = input('Enter POSCAR file name (Default: [POSCAR]): ')
+        if not filename:
+            filename = 'POSCAR'
+        try:
+            slab = poscar.read(filename=filename)
+        except FileNotFoundError:
+            print(f'File {filename} not found.')
+            filename = ''
+    print('Slab POSCAR was read successfully.')
+    return slab
+
+
+def write_poscar_min(slab, rpars, cut, bulk_cuts):
+    """Write to file a POSCAR with one bulk cell only at the bottom."""
+    # Crop off all the atoms at the bottom, leaving only one bulk cell
+    slab = copy.deepcopy(slab)
+    frac_atoms_z = [at.pos[2] for at in slab]
+    frac_topmost_bulk = max(f for f in frac_atoms_z if f <= cut)
+    frac_bottom_slab = min(f for f in frac_atoms_z if f > cut)
+    dist_to_slab = abs(frac_bottom_slab - frac_topmost_bulk)
+    frac_bulk_c = abs(np.linalg.inv(slab.ucell).dot(rpars.BULK_REPEAT)[2])
+
+    new_zero = frac_topmost_bulk + dist_to_slab / 2 - frac_bulk_c
+    slab.atlist = AtomList(at for at in slab if at.pos[2] > new_zero)
+    slab.update_element_count()   # update the number of atoms per element
+
+    # Shift atoms down, then scale the unit-cell height
+    for atom in slab:
+        atom.pos[2] -= new_zero
+    slab.update_cartesian_from_fractional()
+    *_, c_vec = slab.ucell.T
+    c_vec *= 1 - new_zero
+    slab.update_fractional_from_cartesian()
+
+    # Finally, convert the cut positions
+    bulk_cuts = [round(c - new_zero, 3) for c in bulk_cuts]
+
+    # write POSCAR_min
+    slab.sort_original()
+    try:
+        poscar.write(slab, filename='POSCAR_min', comments='none')
+    except OSError:
+        print('Exception occurred while writing POSCAR_min')
+    else:
+        print('Wrote POSCAR_min, to be used with parameters below.')
+
+    # print info
+    print('\nParameters found:')
+    print(f'BULK_REPEAT = xyz{rpars.BULK_REPEAT}')
+    print(f'N_BULK_LAYERS = {len(bulk_cuts)}')
+    print(f'LAYER_CUTS = {" ".join(bulk_cuts)}   ! etc.')
+    input('Program finished, exit with return')
+
+
+def main():
+    """Execute the core functionality of the utility.
+
+    Returns
+    -------
+    success : int
+        0: successful completion, 1: errors
+    """
+    print_utility_description()
+    try:
+        slab = read_user_poscar()
+    except OSError:
+        print('Exception while reading POSCAR file')
+        return 1
+
+    rpars = Rparams()
+    rpars.BULK_LIKE_BELOW = cut = ask_user_bulk_cut()
+    rpars.SYMMETRY_EPS = rpars.SYMMETRY_EPS_Z = ask_user_symmetry_eps()
 
     print('Checking bulk unit cell...')
     try:
-        new_bulk_c, bulk_cuts, bulk_interlayer = sl.detect_bulk(rp, 0.7)
+        _, bulk_cuts, bulk_interlayer = slab.detect_bulk(rpars, 0.7)
     except NoBulkRepeatError:
         _no_repeat = True
     else:
         _no_repeat = False
 
-    ws = writeWoodsNotation(rp.SUPERLATTICE)                                    # TODO: replace writeWoodsNotation with guilib functions
+    superlattice = rpars.SUPERLATTICE
+    ws = writeWoodsNotation(superlattice)                                       # TODO: replace writeWoodsNotation with guilib functions
     if ws:
         info = f'= {ws}'
     else:
-        info = 'M = {} {}, {} {}'.format(*rp.SUPERLATTICE.astype(int).ravel())
+        info = 'M = {} {}, {} {}'.format(*superlattice.astype(int).ravel())
     print(f'Found SUPERLATTICE {info}')
 
     if _no_repeat:
@@ -98,74 +187,27 @@ def main():
               'already be minimal.')
         return 0
 
-    if 0.7 < bulk_interlayer < 1.2:
-        dec = None
-        while dec is None:
-            reply = input('Cutting the bulk into two layers is possible '
-                          f'with a spacing of {round(bulk_interlayer, 2)} A. '
-                          'Proceed? (y/[n]): ')
-            reply = reply.lower()
-            if not reply or reply.startswith('n'):
-                dec = False
-            elif reply.startswith('y'):
-                dec = True
-        if not dec:
-            bulk_cuts.pop()  # Keep only the top one
+    ask_user_confirmation_to_cut(bulk_interlayer, bulk_cuts)
 
     # Update with newly detected info
-    rp.LAYER_CUTS = sl.create_layers(rp, bulk_cuts=bulk_cuts)
-    rp.N_BULK_LAYERS = len(bulk_cuts)
+    rpars.LAYER_CUTS = slab.createLayers(rpars, bulk_cuts=bulk_cuts)
+    rpars.N_BULK_LAYERS = len(bulk_cuts)
 
     # write POSCAR_bulk
-    sl.make_bulk_slab(rp)
+    slab.make_bulk_slab(rpars)
     try:
-        poscar.write(sl.bulkslab, filename='POSCAR_bulk', comments='none')
-    except Exception:
+        poscar.write(slab.bulkslab, filename='POSCAR_bulk', comments='none')
+    except OSError:
         print('Exception occurred while writing POSCAR_bulk')
     else:
         print('Wrote POSCAR_bulk. Check file to see if periodicity is '
               'correct.')
 
-    # Crop off all the atoms at the bottom, leaving only one bulk cell
-    newsl = copy.deepcopy(sl)
-
-    newsl.sort_by_z()
-    topBulkAt = [at for at in newsl if at.pos[2] <= cut][-1]
-    botSlabAt = [at for at in newsl if at.pos[2] > cut][0]
-    fracRepeat = np.dot(np.linalg.inv(newsl.ucell), -new_bulk_c)
-    newZero = (topBulkAt.pos[2]
-               + (abs(botSlabAt.pos[2] - topBulkAt.pos[2]) / 2)
-               - abs(fracRepeat[2]))
-    newsl.atlist = AtomList(at for at in newsl if at.pos[2] > newZero)
-    newsl.update_element_count()   # update the number of atoms per element
-    for at in newsl:
-        at.pos[2] -= newZero
-    newsl.update_cartesian_from_fractional()
-    newsl.ucell[:, 2] = newsl.ucell[:, 2] * (1 - newZero)
-    newsl.update_fractional_from_cartesian()
-
-    # Finally, convert the cut positions
-    bulk_cuts = [round(c - newZero, 3) for c in bulk_cuts]
-
-    # write POSCAR_min
-    newsl.sort_original()
-    try:
-        poscar.write(newsl, filename='POSCAR_min', comments='none')
-    except Exception:
-        print('Exception occurred while writing POSCAR_min')
-    else:
-        print('Wrote POSCAR_min, to be used with parameters below.')
-
-    # print info
-    print('\nParameters found:')
-    print(f'BULK_REPEAT = xyz{rp.BULK_REPEAT}')
-    print(f'N_BULK_LAYERS = {len(bulk_cuts)}')
-    print(f'LAYER_CUTS = {" ".join(bulk_cuts)}   ! etc.')
-    input('Program finished, exit with return')
+    write_poscar_min(slab, rpars, cut, bulk_cuts)
     return 0
 
 
-if __name__ == "__main__":
-    r = main()
-    if r != 0:
-        print("Exit with error")
+if __name__ == '__main__':
+    SUCCESS = main()
+    if not SUCCESS:
+        print('Exit with error')
