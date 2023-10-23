viperleed/viperleed

commit e5ea085a59161d1dcc719797f90c396c44656169

fix: CameraABC tolerance for loaded settings
The previous not-equals check could fail for some floating-point values. Reported by @fkraushofer (gain: 25.799999237060647 != 25.8)

guilib/measure/camera/abc.py
@@ -791,7 +791,7 @@ class CameraABC(qtc.QObject, metaclass=base.QMetaABC):
         error_msg = []
         for setting, (in_config, getter) in to_check.items():
             in_device = getter()
-            if in_config != in_device:
+            if not np.isclose(in_config, in_device):
                 error_msg.append(
                     error_txt.format(setting, in_device, in_config)
                     )

commit 5ad523ef9d666ca795247312b4cb46074832dee7

fix: bug in previous commit
Check 'closeness' of values differently for different data types:
- string need the normal '==',
- numbers use np.isclose
- sequences an array np.allclose

Bug was introduced in e5ea085a59161d1dcc719797f90c396c44656169

guilib/measure/camera/abc.py
@@ -15,7 +15,9 @@ base-class CameraABC. All classes handling cameras should be
 concrete subclasses of CameraABC.
 """
 
+from collections.abc import Sequence
 from abc import abstractmethod
+import operator
 
 import numpy as np
 from PyQt5 import QtCore as qtc
@@ -791,7 +793,13 @@ class CameraABC(qtc.QObject, metaclass=base.QMetaABC):
         error_msg = []
         for setting, (in_config, getter) in to_check.items():
             in_device = getter()
-            if not np.isclose(in_config, in_device):
+            if isinstance(in_config, str):
+                equal = operator.eq
+            elif isinstance(in_config, (Sequence, np.ndarray)):
+                equal = np.allclose
+            else:
+                equal = np.isclose
+            if not equal(in_config, in_device):
                 error_msg.append(
                     error_txt.format(setting, in_device, in_config)
                     )