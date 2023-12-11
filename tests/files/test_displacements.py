9078a2b0b8b920d5ecf15347ef5c5e9234f961cc
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2023-12-11 15:52:52 +0100

    test:read_DISPLACEMENTS with inverted ranges
    Test for bug fixed by @fkraushofer in commit 4cbe029.

--- tests/files/test_displacements.py
+++ tests/files/test_displacements.py
@@ -18,6 +18,14 @@ class TestReadDISPLACEMENTS:
         displ_read = np.array(displaced_atom.disp_geo['all'])
         assert displ_read[:, 2] == approx([0.2, 0.1, 0.0, -0.1, -0.2])
 
+    def test_read_geo_with_inverted_ranges(self, ag100_with_displacements_and_offsets):
+        """Test successful reading of a geometric DISPLACEMENTS block with inverted ranges.
+        Tries reading this displacement: Ag 2 z = 0.2 -0.2 0.1"""
+        slab, *_ = ag100_with_displacements_and_offsets
+        atom = slab.atlist[1]
+        displ_read = np.array(atom.disp_geo['all'])
+        assert displ_read[:, 2] == approx([-0.2, -0.1, 0.0, 0.1, 0.2])
+
     def test_read_vib(self, displaced_atom):
         """Test successful reading of a vibration DISPLACEMENTS block."""
         displ_read = displaced_atom.disp_vib['all']
