bdc35a7ecb6fd19444b941d81a8bb86656306b7a
Michele Riva <riva@iap.tuwien.ac.at>
2023-12-11 21:15:58 +0100

    todo: some notes on dunder methods
    Probably we will need to implement some of those so that they also account for the "z" part if present and they return the right objects (and not a float)

--- tleedmlib/classes/rparams/special/symmetry_eps.py
+++ tleedmlib/classes/rparams/special/symmetry_eps.py
@@ -10,6 +10,15 @@ Defines the class SymmetryEps, which is a float with optional z value.
 from ._base import SpecialParameter
 
 
+# TODO: arithmetic operations return a float. This may be problematic.
+# hash uses the default float implementation. This means that it cannot
+# distinguish SymmetryEps(0.1) from SymmetryEps(0.1, 0.2)
+# Dunder methods that may be needed: '__add__', '__eq__', '__floordiv__',
+# '__ge__', '__gt__', '__hash__', '__le__', '__lt__', '__mod__', '__mul__',
+# '__neg__', '__pos__', '__pow__', '__radd__', '__rdivmod__', '__rfloordiv__',
+# '__rmod__', '__rmul__', '__round__', '__rpow__', '__rsub__', '__rtruediv__',
+# '__sub__', '__truediv__'
+# TODO: forbid negative values!
 
 class SymmetryEps(float, SpecialParameter, param='SYMMETRY_EPS'):
     """SymmetryEps acts like a float but has an optional .z value.
