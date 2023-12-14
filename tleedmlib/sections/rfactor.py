e940a2fbd5c0115583e0c8c699ea6a2a43473fd1
Michele Riva <riva@iap.tuwien.ac.at>
2023-12-14 18:44:47 +0100

    fix: SyntaxError

--- tleedmlib/sections/rfactor.py
+++ tleedmlib/sections/rfactor.py
@@ -135,6 +135,7 @@ def run_new_rfactor(sl, rp, for_error, name, theobeams, expbeams):
      intpol_step) = (
          tl_io.prepare_rfactor_energy_ranges(rp, theobeams, for_error,
                                              n_expand=(rp.INTPOL_DEG-1)/2)
+         )
     out_grid = np.arange(theo_range.min,
                          theo_range.max + 0.1 * intpol_step,
                          intpol_step)
