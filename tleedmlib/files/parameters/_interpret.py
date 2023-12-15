viperleed/viperleed

commit 33cccd07d55dde4f8bd77ad95779597916374fe2

feat,refactor:SymmetryEps special parameter
As discussed with @michele-riva and mentioned in issue #130:
Replace rpars.SYMMETRY_EPS_Z with a special parameter acessed by SYMMETRY_EPS.Z. The special parameter used in the background is derived from float with an optional .Z value.

tleedmlib/files/parameters/_interpret.py
@@ -27,7 +27,8 @@ import numpy as np
 from viperleed.tleedmlib import periodic_table
 from viperleed.tleedmlib.base import readIntRange, readVector
 from viperleed.tleedmlib.base import recombineListElements, splitSublists
-from viperleed.tleedmlib.classes.rparams import LayerCuts, EnergyRange
+from viperleed.tleedmlib.classes.rparams import (LayerCuts, EnergyRange, 
+                                                 SymmetryEps)
 from viperleed.tleedmlib.classes.rparams import TheoEnergies, IVShiftRange
 from viperleed.tleedmlib.files.woods_notation import readWoodsNotation
 from viperleed.tleedmlib.sections._sections import TLEEDMSection as Section
@@ -1517,7 +1518,7 @@ class ParameterInterpreter:
         self._interpret_wood_or_matrix(param, assignment)
 
     def interpret_symmetry_eps(self, assignment):
-        """Assign parameters SYMMETRY_EPS/SYMMETRY_EPS_Z."""
+        """Assign parameters SYMMETRY_EPS and SYMMETRY_EPS.Z."""
         param = 'SYMMETRY_EPS'
         self._ensure_no_flags_assignment(assignment)
         if len(assignment.values) not in (1, 2):
@@ -1526,26 +1527,28 @@ class ParameterInterpreter:
 
         # warning specific to SYMMETRY_EPS
         warning_str = ('PARAMETERS file: SYMMETRY_EPS:\n'
-                       'Given value {}is greater than one Ångström. This is a '
+                       'Given value {} is greater than one Ångström. This is a '
                        'very loose constraint and might lead to incorrect '
                        'symmetry detection. Be sure to check the output.')
 
         # interpret first value as SYMMETRY_EPS
         bounds = NumericBounds(type_=float, range_=(1e-100, None))
-        self.interpret_numerical_parameter(assignment, bounds=bounds)
+        eps_value = (self.interpret_numerical_parameter(
+            assignment, bounds=bounds, return_only=True))
         if self.rpars.SYMMETRY_EPS > 1.0:
             # pylint: disable-next=logging-format-interpolation
             _LOGGER.warning(warning_str.format(''))
-        # interpret possible second value as SYMMETRY_EPS_Z
+        # interpret possible second value as SYMMETRY_EPS.Z
         if not assignment.other_values:
-            self.rpars.SYMMETRY_EPS_Z = self.rpars.SYMMETRY_EPS                 # TODO: not nice, as changes to _EPS that may occur are not reflected on _Z. Could do this with a @property of Rparams or a dedicated class.
+            self.rpars.SYMMETRY_EPS = SymmetryEps(eps_value)
             return
 
         z_assignment = Assignment(assignment.other_values, param)
-        self.interpret_numerical_parameter(z_assignment,
-                                           param='SYMMETRY_EPS_Z',
-                                           bounds=bounds)
-        if self.rpars.SYMMETRY_EPS_Z > 1.0:
+        eps_z_value = self.interpret_numerical_parameter(z_assignment,
+                                           bounds=bounds,
+                                           return_only=True)
+        self.rpars.SYMMETRY_EPS = SymmetryEps(eps_value, eps_z_value)
+        if self.rpars.SYMMETRY_EPS.Z > 1.0:
             # pylint: disable-next=logging-format-interpolation
             _LOGGER.warning(warning_str.format('for z '))
 

POSCAR_get_bulk_repeat.py
@@ -14,6 +14,7 @@ import copy
 import numpy as np
 
 from viperleed.tleedmlib.classes.rparams import Rparams
+from viperleed.tleedmlib.classes.rparams.special.symmetry_eps import SymmetryEps
 from viperleed.tleedmlib.files import poscar
 from viperleed.tleedmlib.files.woods_notation import writeWoodsNotation
 
@@ -75,8 +76,7 @@ def main():
     rp = Rparams()
     rp.LAYER_CUTS.update_from_sequence([cut])
     rp.N_BULK_LAYERS = 1
-    rp.SYMMETRY_EPS = eps
-    rp.SYMMETRY_EPS_Z = eps
+    rp.SYMMETRY_EPS = SymmetryEps(eps)
     sl.fullUpdate(rp)
 
     sl.bulkslab = sl.makeBulkSlab(rp)

tests/files/parameters/test_interpret.py
@@ -1182,7 +1182,7 @@ class TestSymmetryBulk(_TestInterpretBase):
 
 
 class TestSymmetryEps(_TestInterpretBase):
-    """Tests for interpreting SYMMETRY_EPS/SYMMETRY_EPS_Z."""
+    """Tests for interpreting SYMMETRY_EPS/SYMMETRY_EPS.Z."""
 
     param = 'SYMMETRY_EPS'
 
@@ -1190,13 +1190,13 @@ class TestSymmetryEps(_TestInterpretBase):
         """Check correct interpretation of EPS value."""
         self.check_assigned(interpreter, '0.5', 0.5)
         rpars = interpreter.rpars
-        assert rpars.SYMMETRY_EPS == rpars.SYMMETRY_EPS_Z
+        assert rpars.SYMMETRY_EPS == rpars.SYMMETRY_EPS.Z
 
     def test_interpret_multiple_values(self, interpreter):
         """Check correct interpretation of EPS and EPS_Z values."""
         self.interpret(interpreter, '0.1 0.2')
         rpars = interpreter.rpars
-        assert (rpars.SYMMETRY_EPS, rpars.SYMMETRY_EPS_Z) == (0.1, 0.2)
+        assert (rpars.SYMMETRY_EPS, rpars.SYMMETRY_EPS.Z) == (0.1, 0.2)
 
     def test_interpret_invalid_number_of_inputs(self, interpreter):
         """Ensure more than two values raises exceptions."""

tests/poscar_slabs.py
@@ -120,7 +120,6 @@ _PRESETS = {
     'Fe3O4': {'LAYER_CUTS': LayerCuts.from_string('0.1 0.2 <dz(1.0)'),
               'N_BULK_LAYERS': 2,
               'SYMMETRY_EPS': 0.3,
-              'SYMMETRY_EPS_Z': 0.3,
               'BULK_REPEAT': np.array([0.0, -4.19199991, 4.19199991]),
               'SUPERLATTICE': np.array(((1, 1), (1, -1))),
               'superlattice_defined': True},

tleedmlib/classes/rparams/__init__.py
@@ -18,7 +18,8 @@ _limits. Special parameters define their own defaults and limits.
 
 # Important note: import first stuff from .special, as it is used
 # in _rparams and would otherwise lead to cyclic import issues
-from .special.layer_cuts import LayerCuts
 from .special.energy_range import EnergyRange, IVShiftRange, TheoEnergies
+from .special.layer_cuts import LayerCuts
+from .special.symmetry_eps import SymmetryEps
 from ._domain_params import DomainParameters
 from ._rparams import Rparams

tleedmlib/classes/rparams/_rparams.py
@@ -42,6 +42,7 @@ from viperleed.tleedmlib.sections._sections import EXPBEAMS_NAMES
 from ._defaults import NO_VALUE, DEFAULTS
 from ._limits import PARAM_LIMITS
 from .special._base import SpecialParameter, NotASpecialParameterError
+from .special.symmetry_eps import SymmetryEps
 
 _LOGGER = logging.getLogger('tleedm.rparams')
 if _CAN_PLOT:
@@ -120,8 +121,7 @@ class Rparams:
         self.SUPPRESS_EXECUTION = False
         self.SYMMETRIZE_INPUT = True
         self.SYMMETRY_CELL_TRANSFORM = np.identity(2, dtype=float)
-        self.SYMMETRY_EPS = 0.1
-        self.SYMMETRY_EPS_Z = 0.1
+        self.SYMMETRY_EPS = SymmetryEps(0.1)
         self.SYMMETRY_FIND_ORI = True
         self.SYMMETRY_FIX = self.get_default('SYMMETRY_FIX')
         self.SYMMETRY_BULK = {}   # keys: group, rotation, mirror

tleedmlib/classes/rparams/special/symmetry_eps.py
@@ -1,0 +1,38 @@
+"""Module symmetry_eps of viperleed.tleedmlib.classes.rparams.special.
+
+Created on 2023-12-11
+
+@author: Alexander Imre (@amimre)
+
+Defines the class SymmetryEps, which is a float with optional z value.
+"""
+from dataclasses import dataclass
+from typing import Optional
+
+from ._base import SpecialParameter
+
+@dataclass
+class SymmetryEps(float, SpecialParameter):
+    """SymmetryEps acts like a float but has an optional .Z value.
+
+    Used for interpreting the parameter SYMMETRY_EPS. The user can specify a
+    second float value to be used for symmetry comparisons in the z direction.
+    If no second value is given, the first value is used by default."""
+
+    value: float
+    z: Optional[float] = None
+
+    def __new__(cls, value, z=None):
+        """Initialize instance."""
+        return float.__new__(cls, value)
+
+    def __init__(self, value, z=None):
+        float.__init__(self)
+        self.__z = z
+
+    @property
+    def Z(self):
+        """Return z value of instance."""
+        if self.__z is None:
+            return float(self)
+        return self.__z

tleedmlib/classes/slab.py
@@ -1150,7 +1150,7 @@ class Slab:
         ----------
         rp : RunParams
             The current parameters. The only attributes
-            used are SYMMETRY_EPS and SYMMETRY_EPS_Z.
+            used are SYMMETRY_EPS and SYMMETRY_EPS.Z.
         warn_convention : bool, optional
             If True, warnings are added to the current
             logger in case making the reduced unit cell
@@ -1174,7 +1174,7 @@ class Slab:
         """
         # TODO: write a testcase for the reduction of POSCAR Sb on Si(111)
         eps = rp.SYMMETRY_EPS
-        epsz = rp.SYMMETRY_EPS_Z
+        epsz = rp.SYMMETRY_EPS.Z
         abst = self.ucell[:2, :2].T
 
         # Create a test slab: C projected to Z
@@ -1265,11 +1265,11 @@ class Slab:
         vector in cartesian coordinates, or None if no match is found."""
         eps = rp.SYMMETRY_EPS
         if len(self.sublayers) == 0:
-            self.createSublayers(rp.SYMMETRY_EPS_Z)
+            self.createSublayers(rp.SYMMETRY_EPS.Z)
         if self.bulkslab is None:
             self.makeBulkSlab(rp)
         if len(self.bulkslab.sublayers) == 0:
-            self.bulkslab.createSublayers(rp.SYMMETRY_EPS_Z)
+            self.bulkslab.createSublayers(rp.SYMMETRY_EPS.Z)
         nsub = len(self.bulkslab.sublayers)
         if len(self.sublayers) < 2*nsub:
             return None
@@ -1537,7 +1537,7 @@ class Slab:
         # create a pseudo-bulkslab
         tsl.bulkslab = tsl.makeBulkSlab(rp_dummy)
         bsl = tsl.bulkslab
-        bsl.createSublayers(rp.SYMMETRY_EPS_Z)
+        bsl.createSublayers(rp.SYMMETRY_EPS.Z)
         # reduce unit cell in xy
         changecell, mincell = bsl.getMinUnitCell(rp)
         if changecell:
@@ -1556,13 +1556,13 @@ class Slab:
         rp_dummy.SUPERLATTICE = np.eye(2)
         tsl.bulkslab = tsl.makeBulkSlab(rp_dummy)
         bsl = tsl.bulkslab
-        bsl.createSublayers(rp.SYMMETRY_EPS_Z)
+        bsl.createSublayers(rp.SYMMETRY_EPS.Z)
 
         # calculate cut plane
         frac_bulk_thickness = abs(newC[2]) / abs(self.ucell[2, 2])
         frac_lowest_pos = min([at.pos[2] for at in self.atlist])
         frac_bulk_onset = (frac_lowest_pos + frac_bulk_thickness
-                           - (rp.SYMMETRY_EPS_Z / self.ucell[2, 2]))
+                           - (rp.SYMMETRY_EPS.Z / self.ucell[2, 2]))
         slab_cuts = [(max([at.pos[2] for at in self.atlist
                           if at.pos[2] < frac_bulk_onset])
                      + min([at.pos[2] for at in self.atlist
@@ -1653,7 +1653,7 @@ class Slab:
         bsl.getCartesianCoordinates(updateOrigin=True)
         bsl.updateElementCount()   # update the number of atoms per element
         # remove duplicates
-        bsl.createSublayers(rp.SYMMETRY_EPS_Z)
+        bsl.createSublayers(rp.SYMMETRY_EPS.Z)
         newatlist = []
         for subl in bsl.sublayers:
             i = 0
@@ -1698,7 +1698,7 @@ class Slab:
         ssl.ucell_mod = []
         # if self.ucell_mod is not empty, don't drag that into the new slab.
         # remove duplicates
-        ssl.createSublayers(rp.SYMMETRY_EPS_Z)
+        ssl.createSublayers(rp.SYMMETRY_EPS.Z)
         newatlist = []
         for subl in ssl.sublayers:
             i = 0

commit ca49bc2f53e08673b5d2206465862d8dc92ce926

fix: warn if SYMMETRY_EPS is large
Used to be correct. Broken in 33cccd07d55dde4f8bd77ad95779597916374fe2

tleedmlib/files/parameters/_interpret.py
@@ -1578,7 +1578,7 @@ class ParameterInterpreter:  # pylint: disable=too-many-public-methods
         eps_value = self.interpret_numerical_parameter(assignment,
                                                        bounds=bounds,
                                                        return_only=True)
-        if self.rpars.SYMMETRY_EPS > 1.0:
+        if eps_value > 1.0:
             # pylint: disable-next=logging-format-interpolation
             _LOGGER.warning(warning_str.format(''))
         # interpret possible second value as SYMMETRY_EPS.z