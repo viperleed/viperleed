08a2080e7c3a96c8cba610340d22385a08d53ed5
Michele Riva <riva@iap.tuwien.ac.at>
2024-02-02 13:53:22 +0100

    minor, tests: renaming

--- tests/poscar_slabs.py
+++ tests/poscar_slabs.py
@@ -295,8 +295,8 @@ class CasePOSCARSlabs:
         return self.case_poscar(info)
 
     @case(tags=Tag.NON_MINIMAL_CELL)
-    def case_poscar_lsmo(self):
-        """Return a sqrt(2) x sqrt(2) LSMO (001) A-site bulk-truncated slab"""
+    def case_poscar_lsmo_001_rt2(self):
+        """Return a sqrt(2) x sqrt(2) LSMO (001) A-site bulk-truncated slab."""
         info = _get_poscar_info('POSCAR_LSMO_001_A_site', 56)
         info.poscar.n_cells = 4
         return self.case_poscar(info)
