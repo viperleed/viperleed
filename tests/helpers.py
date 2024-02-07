33beca89a03dd54e2869a01e717dfb512bc435d3
Michele Riva <riva@iap.tuwien.ac.at>
2024-02-07 17:39:35 +0100

    chore, tests: alphabetical order

--- tests/helpers.py
+++ tests/helpers.py
@@ -244,45 +244,6 @@ class InfoBase:
 
 
 @dataclass(repr=False)
-class POSCARInfo(InfoBase):
-    """Container for information about POSCAR files."""
-    name: str = ''
-    n_atoms: int = None
-    n_atoms_by_elem: dict = field(init=False, default_factory=dict)
-    n_cells: int = 1  # How many 2D cells are in the POSCAR?
-
-    def __post_init__(self):
-        """Post-process initialization values."""
-        if isinstance(self.n_atoms, Mapping):
-            self.n_atoms_by_elem = self.n_atoms
-            self.n_atoms = sum(self.n_atoms_by_elem.values())
-
-
-# Avoid using atoms that are on planes: Easier to test.
-@dataclass(repr=False)
-class DisplacementInfo(InfoBase):
-    """Information about an atom to be displaced."""
-    atom_nr: int
-    atom_on_axis: bool
-
-
-LinkGroup = Dict[int, Set[int]]
-
-
-@dataclass(repr=False)
-class SymmetryInfo(InfoBase):
-    """Symmetry information pertaining to atoms in a slab."""
-
-    on_planes: Tuple[int] = ()        # .num of atoms on plane
-    on_axes: Tuple[int] = ()          # .num of atoms on rotation axis
-    link_groups: LinkGroup = field(   # {num: {linked_nums}}
-        default_factory=dict
-        )
-    hermann: str = ''          # expected plane group (Hermann-Maugin)
-    invalid_direction: str = ''       # For subgroup reduction
-
-
-@dataclass(repr=False)
 class BulkInfo(InfoBase):
     """Information exclusively valid for bulk slabs."""
 
@@ -290,30 +251,6 @@ class BulkInfo(InfoBase):
     n_glide_planes: int = None       # Number of 3D glide planes
     repeat: (float,)*3 = None        # Repeat vector
     periods: list = None             # Candidate periods
-
-
-@dataclass(repr=False)
-class ParametersInfo(InfoBase):
-    """A container of information for PARAMETERS tests."""
-    param_path : str = '' # Relative to data_path
-    expected: dict = field(default_factory=dict)
-
-
-@dataclass(repr=False)
-class TestInfo(InfoBase):
-    """Container of various pieces of information useful while testing."""
-    poscar: ... = field(default_factory=POSCARInfo)
-    symmetry: ... = field(default_factory=SymmetryInfo)
-    bulk: ... = field(default_factory=BulkInfo)
-    displacements: List[DisplacementInfo] = field(default_factory=list)
-    parameters: ... = field(default_factory=ParametersInfo)
-    param_presets: dict = field(default_factory=dict)  # For Rparams
-    debug: dict = field(default_factory=dict)
-
-    def update_params(self, param):
-        """Update an Rparam with the presets stored."""
-        for attr_name, attr_value in self.param_presets.items():
-            setattr(param, attr_name, attr_value)
 
 
 @dataclass(repr=False)
@@ -326,6 +263,14 @@ class BulkSlabAndRepeatInfo(InfoBase):
     bulk_cuts: List[float]
     bulk_dist: float
     bulk_ucell: np.ndarray
+
+
+# Avoid using atoms that are on planes: Easier to test.
+@dataclass(repr=False)
+class DisplacementInfo(InfoBase):
+    """Information about an atom to be displaced."""
+    atom_nr: int
+    atom_on_axis: bool
 
 
 @dataclass(repr=False)
@@ -350,7 +295,62 @@ class NearestNeighborInfo(InfoBase):
 
 
 @dataclass(repr=False)
+class ParametersInfo(InfoBase):
+    """A container of information for PARAMETERS tests."""
+    param_path : str = '' # Relative to data_path
+    expected: dict = field(default_factory=dict)
+
+
+@dataclass(repr=False)
+class POSCARInfo(InfoBase):
+    """Container for information about POSCAR files."""
+    name: str = ''
+    n_atoms: int = None
+    n_atoms_by_elem: dict = field(init=False, default_factory=dict)
+    n_cells: int = 1  # How many 2D cells are in the POSCAR?
+
+    def __post_init__(self):
+        """Post-process initialization values."""
+        if isinstance(self.n_atoms, Mapping):
+            self.n_atoms_by_elem = self.n_atoms
+            self.n_atoms = sum(self.n_atoms_by_elem.values())
+
+
+@dataclass(repr=False)
 class SurfaceAtomInfo(InfoBase):
     """Container for information about which atoms are at the surface."""
     # Keys are atom number
     surface_atom_nums: Tuple[int]
+
+
+LinkGroup = Dict[int, Set[int]]
+
+
+@dataclass(repr=False)
+class SymmetryInfo(InfoBase):
+    """Symmetry information pertaining to atoms in a slab."""
+
+    on_planes: Tuple[int] = ()        # .num of atoms on plane
+    on_axes: Tuple[int] = ()          # .num of atoms on rotation axis
+    link_groups: LinkGroup = field(   # {num: {linked_nums}}
+        default_factory=dict
+        )
+    hermann: str = ''          # expected plane group (Hermann-Maugin)
+    invalid_direction: str = ''       # For subgroup reduction
+
+
+@dataclass(repr=False)
+class TestInfo(InfoBase):
+    """Container of various pieces of information useful while testing."""
+    poscar: ... = field(default_factory=POSCARInfo)
+    symmetry: ... = field(default_factory=SymmetryInfo)
+    bulk: ... = field(default_factory=BulkInfo)
+    displacements: List[DisplacementInfo] = field(default_factory=list)
+    parameters: ... = field(default_factory=ParametersInfo)
+    param_presets: dict = field(default_factory=dict)  # For Rparams
+    debug: dict = field(default_factory=dict)
+
+    def update_params(self, param):
+        """Update an Rparam with the presets stored."""
+        for attr_name, attr_value in self.param_presets.items():
+            setattr(param, attr_name, attr_value)
