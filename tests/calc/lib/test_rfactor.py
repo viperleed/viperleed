1cbc293e6f2c229958f77ad00fb958ef951485ea
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-02-27 11:23:49 +0100

    test: test rfactor file

---
+++ tests/calc/lib/test_rfactor.py
@@ -0,0 +1,10 @@
+"""Test Module R-factor"""
+import pytest
+
+from jax import numpy as jnp
+from src import rfactor
+
+
+def test_pendry_R_from_intensity_and_derivative():
+    """Test the R-factor calculation from intensity and derivative"""
+    pass
