dc931476edca7bd2bfa55eb673ff122676220eb3
Michele Riva <riva@iap.tuwien.ac.at>
2024-03-08 18:26:25 +0100

    feat, tests: Lattice2D raises

--- tests/guilib/classes/test_lattice2d.py
+++ tests/guilib/classes/test_lattice2d.py
@@ -4,50 +4,43 @@ Created: 2020-01-11
 Author: Michele Riva
 """
 
-import sys
-import os
-
 import pytest
+from pytest_cases import parametrize
 import numpy as np
 
-from viperleed.guilib import base
 from viperleed.guilib.classes.lattice2d import Lattice2D
 
 
-def test_lattice_init():
-    b = [[1, 0], [0, 2]]
-    g = 'pgg'
-    latt = Lattice2D(b, group=g)
-    assert np.array_equal(latt.basis, b)
-    assert latt.group.group == g
-    assert latt.cell_shape == 'Rectangular'
-    assert latt.n_beams == 3
-
-def test_lattice_wrong_basis_input():
-    for b in ('', dict(), set(), 3, np.array([]), (1, 0), 2.5):
-        with pytest.raises((TypeError, ValueError)) as exc:
-            Lattice2D(b)
-            if base.check_type(b, 'arraylike'):
-                assert exc == ValueError
-            else:
-                assert exc == TypeError
+_HEX_OBTUSE = (1, 0), (-0.5, 3**0.5/2)
+_SQUARE = (1, 0), (0, 1)
 
 
-def test_lattice_wrong_space_input():
-    for space in ('', dict(), set(), 3, np.array([]), (1, 0), 2.5):
-        with pytest.raises((TypeError, ValueError)) as exc:
-            Lattice2D([[1, 0], [0, 1]], space=space)
-            if base.check_type(space, 'str'):
-                assert exc == ValueError
-            else:
-                assert exc == TypeError
+class TestRaises:
+    """Collection of tests for complaints by Lattice2D objects."""
 
+    _init = {
+        'space': (_SQUARE, {'space': 'invalid_space'}, ValueError),
+        'limit': (_SQUARE, {'limit': 'not_a_number'}, TypeError),
+        'basis': ('invalid_basis', {}, TypeError),
+        'basis shape': (np.eye(3), {}, ValueError),
+        'basis singular': (np.zeros((2, 2)), {}, ValueError),
+        'group': (_HEX_OBTUSE, {'group': 'p4'}, ValueError),
+        }
 
-def test_lattice_wrong_limit_input():
-    for space in ('', dict(), set(), np.array([]), (1, 0), 2.5):
-        with pytest.raises((TypeError, ValueError)) as exc:
-            Lattice2D([[1, 0], [0, 1]], space=space)
-            if base.check_type(space, 'str'):
-                assert exc == ValueError
-            else:
-                assert exc == TypeError
+    @parametrize('basis,kwargs,exc', _init.values(), ids=_init)
+    def test_init_invalid(self, basis, kwargs, exc):
+        """Check complaints for making a Lattice2D with invalid arguments."""
+        with pytest.raises(exc):
+            Lattice2D(basis, **kwargs)
+
+    _attr = {
+        'n_beams': Lattice2D(_SQUARE, space='real'),
+        'n_points': Lattice2D(_SQUARE, space='reciprocal')
+        }
+
+    @parametrize('attr,lattice', _attr.items(), ids=_attr)
+    def test_attribute_error(self, lattice, attr):
+        """Check complaints when accessing a property for the wrong space."""
+        with pytest.raises(AttributeError):
+            _ = getattr(lattice, attr)
+
