b4dda1557b847feae8904410dd8ee2a8668b3cac
Michele Riva <riva@iap.tuwien.ac.at>
2024-04-02 18:16:11 +0200

    feat: ViPErLEEDCLI for viperleed main
    As of this commit, it would not run anything since none of the children subclass ViPErLEEDCLI yet.

--- viperleed/__main__.py
+++ viperleed/__main__.py
@@ -11,12 +11,7 @@ __copyright__ = 'Copyright (c) 2019-2024 ViPErLEED developers'
 __created__ = '2023-08-01'
 __license__ = 'GPLv3+'
 
-import sys
-
-from viperleed.cli import main
+from viperleed.cli import ViPErLEEDMain
 
 
-EXIT_CODE = main()
-if EXIT_CODE is None:
-    EXIT_CODE = 0
-sys.exit(EXIT_CODE)
+ViPErLEEDMain.run_as_script()
