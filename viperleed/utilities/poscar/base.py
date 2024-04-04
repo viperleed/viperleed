a40f2227dedda7e992180d7ad19b3517c9532fd4
Michele Riva <riva@iap.tuwien.ac.at>
2024-04-04 12:23:39 +0200

    fix: wrong date
    Co-Authored-By: Alexander M. Imre <90624999+amimre@users.noreply.github.com>

--- viperleed/utilities/poscar/base.py
+++ viperleed/utilities/poscar/base.py
@@ -9,7 +9,7 @@ __authors__ = (
     'Michele Riva (@michele-riva)',
     )
 __copyright__ = 'Copyright (c) 2019-2024 ViPErLEED developers'
-__created__ = '2024-09-27'
+__created__ = '2024-03-27'
 __license__ = 'GPLv3+'
 
 from abc import ABC, abstractmethod
