153ff125cdc1e2ba210cb4b3c52d2c09039585b7
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-04-05 15:40:19 +0200

    feat,test: absolute vacuum gaps

--- tests/calc/utilities/poscar/test_modify_vacuum.py
+++ tests/calc/utilities/poscar/test_modify_vacuum.py
@@ -21,8 +21,8 @@ from viperleed.utilities.poscar.modify_vacuum import ModifyVacuumCLI
 class TestModifyVacuum:
     """Tests for the modify_vacuum utility."""
 
-    @pytest.mark.parametrize('vacuum_gap_size', [1.0, 3.14, 10.0])
-    @pytest_cases.parametrize_with_cases('test_slab', cases=poscar_slabs)
+    @pytest.mark.parametrize('vacuum_gap_size', [1.0, 3.14, 10.0, -1.0])
+    @pytest_cases.parametrize_with_cases('test_slab', cases=poscar_slabs, has_tag=poscar_slabs.Tag.NO_INFO)
     def test_modify_vacuum_relative(self, test_slab, vacuum_gap_size):
         """Test the ModifyVacuumCLI class."""
         parser = ModifyVacuumCLI().parser
@@ -34,21 +34,13 @@ class TestModifyVacuum:
         assert (modified_slab.vacuum_gap
                 == pytest.approx(vacuum_gap_size + original_gap))
 
-    @pytest.mark.parametrize('vacuum_gap_size', [-1.0, 1.0])
+    @pytest.mark.parametrize('vacuum_gap_size', [1.0, 3.14, 10.0,])
+    @pytest_cases.parametrize_with_cases('test_slab', cases=poscar_slabs)
     def test_modify_vacuum_gap_absolute(self, test_slab, vacuum_gap_size):
         """Test the ModifyVacuumCLI class with invalid vacuum gap size."""
         parser = ModifyVacuumCLI().parser
         slab, *_ = test_slab
-        args = parser.parse_args([str(vacuum_gap_size)])
+        args = parser.parse_args([str(vacuum_gap_size), '-a'])
         modified_slab = ModifyVacuumCLI().process_slab(slab, args)
         # check if vacuum gap size is modified correctly
         assert modified_slab.vacuum_gap == pytest.approx(vacuum_gap_size)
-
-    @pytest.mark.parametrize('vacuum_gap_size', [-10, -1000])
-    def test_invalid_vacuum_gap_size(self, test_slab, vacuum_gap_size):
-        """Test the ModifyVacuumCLI class with invalid vacuum gap size."""
-        parser = ModifyVacuumCLI().parser
-        slab, *_ = test_slab
-        args = parser.parse_args([str(vacuum_gap_size)])
-        with pytest.raises(SystemExit):
-            ModifyVacuumCLI().process_slab(slab, args)
