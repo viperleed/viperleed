7a1113f9cfc9369870d85f44abcc899d8177439d
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-04-05 14:49:19 +0200

    test: util poscar delete_between

--- tests/calc/utilities/poscar/test_delete.py
+++ tests/calc/utilities/poscar/test_delete.py
@@ -14,12 +14,12 @@ import pytest_cases
 
 from viperleed.utilities.poscar.delete_above import DeleteAboveCLI
 from viperleed.utilities.poscar.delete_below import DeleteBelowCLI
-#from viperleed.utilities.poscar.delete_between import DeleteBetweenCLI
+from viperleed.utilities.poscar.delete_between import DeleteBetweenCLI
 
 from ... import poscar_slabs
 
 VALID_CUT_FRACTIONS = [0.1234, 0.5, 0.75]
-INVALID_CUT_FRACTIONS = [-0.1, 1.1, 0.0]
+INVALID_CUT_FRACTIONS = [-0.1, 1.1, 0.0, 'abc']
 
 class TestDeleteAbove:
     """Tests for the delete_above utility."""
@@ -46,6 +46,7 @@ class TestDeleteAbove:
         with pytest.raises(SystemExit):
             args = parser.parse_args([str(c)])
 
+
 class TestDeleteBelow:
     """Tests for the delete_below utility."""
 
@@ -70,3 +71,30 @@ class TestDeleteBelow:
         slab, *_ = ag100
         with pytest.raises(SystemExit):
             args = parser.parse_args([str(c)])
+
+
+class TestDeleteBetween:
+    """Tests for the delete_between utility."""
+
+    @pytest.mark.parametrize('c1, c2', [(0.2, 0.8), (0.4, 0.6)])
+    @pytest_cases.parametrize_with_cases('test_slab', cases=poscar_slabs)
+    def test_delete_between_cli(self, test_slab, c1, c2):
+        """Test the DeleteBetweenCLI class."""
+        parser = DeleteBetweenCLI().parser
+        slab, *_ = test_slab
+        original_ucell = slab.ucell.copy()
+        args = parser.parse_args([str(c1), str(c2)])
+        n_atoms_between_c1_c2 = sum(1 for atom in slab if atom.pos[2] >= c1
+                                    and atom.pos[2] <= c2)
+        # right number of atoms
+        assert DeleteBetweenCLI().process_slab(slab, args).n_atoms == n_atoms_between_c1_c2
+        # unit cell is unchanged
+        assert np.allclose(slab.ucell, original_ucell)
+
+    @pytest.mark.parametrize('c1, c2', [(0.8, 0.2), (0.6, 0.4)])
+    def test_invalid_c(self, ag100, c1, c2):
+        """Test the DeleteBetweenCLI class."""
+        parser = DeleteBetweenCLI().parser
+        slab, *_ = ag100
+        with pytest.raises(SystemExit):
+            args = parser.parse_args([str(c1), str(c2)])
