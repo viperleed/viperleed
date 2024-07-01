c552bd460918ae9830cd29bcbfc6e749c2046537
Michele Riva <riva@iap.tuwien.ac.at>
2024-07-01 13:40:09 +0200

    feat: custom action for BookkeeperMode
    Gets rid of the if...elif, and is more expandable.
    
    Also some minor line-too-long edits.

--- tests/calc/bookkeeper/test_cli.py
+++ tests/calc/bookkeeper/test_cli.py
@@ -4,6 +4,7 @@ import pytest
 from pytest_cases import fixture, parametrize
 
 from viperleed.calc.bookkeeper.cli import BookkeeperCLI
+from viperleed.calc.bookkeeper.mode import BookkeeperMode
 
 
 @fixture(name='bookkeeper_parser')
@@ -23,23 +24,31 @@ class TestBookkeeperParser:
 
     @parametrize(flag=('-a', '--archive'))
     def test_parser_archive(self, bookkeeper_parser, flag):
-        """Check interpretation of --clear mode."""
-        assert bookkeeper_parser.parse_args([flag,]).archive
+        """Check interpretation of --archive mode."""
+        parsed = bookkeeper_parser.parse_args([flag,])
+        assert parsed.archive
+        assert parsed.mode is BookkeeperMode.ARCHIVE
 
     @parametrize(flag=('-c', '--clear'))
     def test_parser_clear(self, bookkeeper_parser, flag):
         """Check interpretation of --clear mode."""
-        assert bookkeeper_parser.parse_args([flag,]).clear
+        parsed = bookkeeper_parser.parse_args([flag,])
+        assert parsed.clear
+        assert parsed.mode is BookkeeperMode.CLEAR
 
     @parametrize(flag=('-d', '--discard'))
     def test_parser_discard(self, bookkeeper_parser, flag):
         """Check interpretation of --discard mode."""
-        assert bookkeeper_parser.parse_args([flag,]).discard
+        parsed = bookkeeper_parser.parse_args([flag,])
+        assert parsed.discard
+        assert parsed.mode is BookkeeperMode.DISCARD
 
     @parametrize(flag=('-df', '--discard-full'))
     def test_parser_discard_full(self, bookkeeper_parser, flag):
         """Check interpretation of --discard-full mode."""
-        assert bookkeeper_parser.parse_args([flag,]).discard_full
+        parsed = bookkeeper_parser.parse_args([flag,])
+        assert parsed.discard_full
+        assert parsed.mode is BookkeeperMode.DISCARD_FULL
 
     _exclusive = {
         'archive & clear': ('-a', '-c'),
--- viperleed/calc/bookkeeper/cli.py
+++ viperleed/calc/bookkeeper/cli.py
@@ -13,6 +13,7 @@ __copyright__ = 'Copyright (c) 2019-2024 ViPErLEED developers'
 __created__ = '2020-01-30'
 __license__ = 'GPLv3+'
 
+from argparse import Action
 from pathlib import Path
 
 from viperleed.calc import DEFAULT_HISTORY
@@ -22,6 +23,47 @@ from viperleed.cli_base import ViPErLEEDCLI
 from .bookkeeper import Bookkeeper
 from .constants import HISTORY_INFO_NAME
 from .mode import BookkeeperMode
+
+
+# All the pylint disables below are needed as Action subclasses
+# must have this interface. The problem rather lies in argparse.
+# pylint: disable-next=too-few-public-methods
+class StoreBookkeeperMode(Action):
+    """An action to select the correct BookkeeperMode.
+
+    It behaves as 'store_true', making the arguments to which
+    it is associated not require any other information.
+    """
+
+    # pylint: disable-next=too-many-arguments
+    def __init__(self,
+                 option_strings,
+                 dest,
+                 default=False,
+                 required=False,
+                 help=None):  # pylint: disable=redefined-builtin
+        """Initialize instance to behave the same as 'store_true'."""
+        super().__init__(
+            option_strings=option_strings,
+            dest=dest,
+            nargs=0,
+            const=True,
+            default=default,
+            type=None,
+            choices=None,
+            required=required,
+            help=help,
+            metavar=None,
+            )
+
+    def __call__(self, parser, args, values, option_string=None):
+        """Set args.mode to the right BookkeeperMode."""
+        try:
+            mode = BookkeeperMode(self.dest)
+        except ValueError:
+            parser.error(f'Unknown bookkeeper mode {self.dest!r}')
+        setattr(args, 'mode', mode)
+        setattr(args, self.dest, self.const)
 
 
 class BookkeeperCLI(ViPErLEEDCLI, cli_name='bookkeeper'):
@@ -35,30 +77,32 @@ class BookkeeperCLI(ViPErLEEDCLI, cli_name='bookkeeper'):
             '-a', '--archive',
             help=('Store last run in history. Overwrite PARAMETERS, POSCAR &'
                   'VIBROCC from OUT. Runs after viperleed.calc by default.'),
-            action='store_true',
+            action=StoreBookkeeperMode,
             )
         what_next.add_argument(
             '-c', '--clear',
-            help=('Clear the input directory and add last run to history if not'
-                  ' already there. Runs before viperleed.calc by default.'),
-            action='store_true',
+            help=('Clear the input directory and add last run '
+                  'to history if not already there. Runs before '
+                  'viperleed.calc by default.'),
+            action=StoreBookkeeperMode,
             )
         what_next.add_argument(
             '-d', '--discard',
             help=('Discard all results from the last run, and restore the '
                   'previous inputs. The discarded run is kept in history.'),
-            action='store_true',
+            action=StoreBookkeeperMode,
             )
         what_next.add_argument(
             '-df', '--discard-full',
             help=('Discard all results from the last run as if it never '
                   'happened. The discarded run is removed from history.'),
-            action='store_true',
+            action=StoreBookkeeperMode,
             )
         parser.add_argument(
             '-j', '--job-name',
-            help=('define a string to be appended to the name of the history '
-                  f'folder that is created, and is logged in {HISTORY_INFO_NAME}'),
+            help=('define a string to be appended to the name '
+                  'of the history folder that is created, and '
+                  f'is logged in {HISTORY_INFO_NAME}'),
             type=str
             )
         parser.add_argument(
@@ -76,22 +120,14 @@ class BookkeeperCLI(ViPErLEEDCLI, cli_name='bookkeeper'):
             default=DEFAULT_WORK_HISTORY
             )
 
-
     def __call__(self, args=None):
         """Call the bookkeeper with command-line args."""
         parsed_args = self.parse_cli_args(args)
-
-        bookkeeper = Bookkeeper(job_name=parsed_args.job_name,
-                                history_name=parsed_args.history_name,
-                                work_history_name=parsed_args.work_history_name,
-                                cwd=Path.cwd().resolve())
-        # Select mode
-        if parsed_args.clear:
-            mode = BookkeeperMode.CLEAR
-        elif parsed_args.discard:
-            mode = BookkeeperMode.DISCARD
-        elif parsed_args.discard_full:
-            mode = BookkeeperMode.DISCARD_FULL
-        else:
-            mode = BookkeeperMode.ARCHIVE
+        bookkeeper = Bookkeeper(
+            job_name=parsed_args.job_name,
+            history_name=parsed_args.history_name,
+            work_history_name=parsed_args.work_history_name,
+            cwd=Path.cwd().resolve()
+            )
+        mode = getattr(parsed_args, 'mode', BookkeeperMode.ARCHIVE)
         return bookkeeper.run(mode)
