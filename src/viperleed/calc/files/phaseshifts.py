e5f8d2badab7d53c03cfb105e8667dc40cad7c7f
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-07-10 15:25:56 +0900

    Update src/viperleed/calc/files/phaseshifts.py
    Co-authored-by: Michele Riva <riva@iap.tuwien.ac.at>

--- src/viperleed/calc/files/phaseshifts.py
+++ src/viperleed/calc/files/phaseshifts.py
@@ -445,7 +445,7 @@ def __check_consitency_element_order(rp, sl, phaseshifts,
             "Could not check consistency of PHASESHIFTS file: "
             f"PHASESHIFTS for some sites are smaller than {eps} at the largest "
             "energy for LMAX >= {l_max_cutoff}. This may happen if you are "
-            "using very light scatterers (e.g. Hydrogen)."
+            "using very light scatterers (e.g. hydrogen)."
             )
         rp.setHaltingLevel(1)
         return
