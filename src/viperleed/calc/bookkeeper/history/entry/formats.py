8cf998b26dffbb38ca10e797438a9161f0cb027f
Michele Riva <riva@iap.tuwien.ac.at>
2024-07-28 08:10:48 +0200

    feat: use DateTimeformat for time stamp

--- src/viperleed/calc/bookkeeper/history/entry/formats.py
+++ src/viperleed/calc/bookkeeper/history/entry/formats.py
@@ -14,18 +14,25 @@ __license__ = 'GPLv3+'
 
 from enum import Enum
 
+from viperleed.calc.lib.time_utils import DateTimeFormat
+from viperleed.calc.lib.time_utils import now_
+
 
 class TimestampFormat(Enum):
     """A collection of known formats for timestamps."""
 
     # Underscore names are used only for conversion purposes
     # while parsing strings, and never for formatting.
-    GERMAN = '%d.%m.%y %H:%M:%S'  # Format used for < v1.0.0
-    ISO = '%Y-%m-%d %H:%M:%S'     # Format used for >= v1.0.0
-    _CALC = '%y%m%d-%H%M%S'       # Format used by calc in log files
-    DEFAULT = ISO
+    GERMAN = '%d.%m.%y %H:%M:%S'              # < v1.0.0
+    ISO = DateTimeFormat.ISO.value            # >= v1.0.0
+    _CALC = DateTimeFormat.FILE_SUFFIX.value  # Format of log names
+    DEFAULT = ISO  # Just an alias of one of the above!
 
     @property
     def writable(self):
-        """Return whether this format can be used for writing."""
+        """Return whether self is suitable for writing to history.info."""
         return not self.name.startswith('_')
+
+    def now(self):
+        """Return the local time formatted according to this format."""
+        return now_(self.value, use_gmt=False)
