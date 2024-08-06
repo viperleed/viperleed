7e9a8bc8b15c42ecd034c26a8fc754f0cfa0a3ef
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-08-06 14:59:50 +0200

    feat: state recorder class

---
+++ src/viperleed/calc/classes/state_recorder.py
@@ -0,0 +1,46 @@
+"""
+"""
+
+__authors__ = (
+    'Alexander M. Imre (@amimre)',
+    )
+__copyright__ = 'Copyright (c) 2019-2024 ViPErLEED developers'
+__created__ = '2024-08-06'
+__license__ = 'GPLv3+'
+
+from collections import namedtuple
+from copy import deepcopy
+
+from viperleed.calc.sections.calc_section import CalcSection
+
+# A state is given by a tuple (slab, rparams, section).
+State = namedtuple('State', 'slab, rparams, section')
+
+class StateRecorder:
+    """
+    A state is given by a tuple (slab, rparams).
+    """
+
+    def __init__(self):
+        """
+        """
+        self.recorded_states = []
+
+    def record(self, slab, rparams, section):
+        """Freezes and records the current state."""
+        state = State(slab=deepcopy(slab),
+                      rparams=deepcopy(rparams),
+                      section=section)
+        self.recorded_states.append(state)
+
+    def get_last_state(self):
+        """Returns the last recorded state."""
+        return self.recorded_states[-1]
+
+    def get_last_section_state(self, section):
+        """Returns the last state recorded for a given section."""
+        _section = CalcSection(section)
+        for state in reversed(self.recorded_states):
+            if state.rparams.section == _section:
+                return state
+        raise ValueError(f"No state recorded for section {section.long_name}")
