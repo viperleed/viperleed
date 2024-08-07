18785ae1982629ee33c24de8efc3632ccd46206e
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-08-07 09:42:38 +0200

    style: implement suggestions
    Co-Authored-By: Michele Riva <riva@iap.tuwien.ac.at>

--- src/viperleed/calc/classes/state_recorder.py
+++ src/viperleed/calc/classes/state_recorder.py
@@ -15,35 +15,35 @@ from copy import deepcopy
 
 from viperleed.calc.sections.calc_section import CalcSection
 
-# A state is given by a tuple (slab, rparams).
-State = namedtuple('State', 'slab, rparams, section')
+# A state is given by a tuple (slab, rpars).
+State = namedtuple('State', 'slab, rpars, section')
 
 class StateRecorder:
     """Class that records and retrieves calculation states."""
 
     def __init__(self):
         """Initialize the state recorder with an empty list of states."""
-        self.recorded_states = []
+        self._recorded_states = []
 
-    def record(self, slab, rparams, section):
+    def record(self, slab, rpars, section):
         """Freeze and record the current state."""
         state = State(slab=deepcopy(slab),
-                      rparams=deepcopy(rparams),
+                      rpars=deepcopy(rpars),
                       section=CalcSection(section))
-        self.recorded_states.append(state)
+        self._recorded_states.append(state)
 
     def get_last_state(self):
         """Returns the last recorded state."""
-        return self.recorded_states[-1]
+        return self._recorded_states[-1]
 
     def get_last_section_state(self, section):
         """Return the last state recorded for a given section."""
         _section = CalcSection(section)
-        for state in reversed(self.recorded_states):
-            if state.rparams.section == _section:
+        for state in reversed(self._recorded_states):
+            if state.rpars.section == _section:
                 return state
         raise ValueError(f'No state recorded for section {section.long_name}.')
 
     def pop_last_state(self, index=None):
         """Removes and returns the last recorded state, similar to List.pop()."""
-        return self.recorded_states.pop(index if index is not None else -1)
+        return self._recorded_states.pop(index if index is not None else -1)
