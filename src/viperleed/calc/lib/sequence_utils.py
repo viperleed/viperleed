f2a9a8d0e70a24a1c2787ae42e51443ef58ef04a
Michele Riva <riva@iap.tuwien.ac.at>
2024-08-25 16:46:16 +0200

    feat: conditional sorting of sequence

--- src/viperleed/calc/lib/sequence_utils.py
+++ src/viperleed/calc/lib/sequence_utils.py
@@ -10,6 +10,44 @@ __copyright__ = 'Copyright (c) 2019-2024 ViPErLEED developers'
 __created__ = '2024-08-25'
 __license__ = 'GPLv3+'
 
+from collections.abc import Sequence
+
+
+# Adapted from https://stackoverflow.com/questions/45382174
+def conditional_sort(sequence, skip, key=None):
+    """Return a sorted list of items in sequence, skipping some.
+
+    Parameters
+    ----------
+    sequence : Sequence
+        The sequence whose items should be sorted.
+    skip : callable
+        A callable that will be called on each item. Should return True
+        for those items that the user wants to stay in their place and
+        not be sorted. Only those items for which skip returns False
+        are sorted.
+    key : callable, optional
+        Same meaning as for the sorted built-in function.
+
+    Returns
+    -------
+    sorted_sequence : type(Sequence)
+        Sequence with items selected by `skip` in unchanged position,
+        all the others sorted according to `key`.
+    """
+    if not isinstance(sequence, Sequence):
+        raise TypeError('Not a sequence')
+    if not callable(skip):
+        raise TypeError('skip must be callable')
+    cls = type(sequence)
+
+    sorted_items = iter(sorted(
+        (item for item in sequence if not skip(item)),
+        key=key,
+        ))
+    return cls(item if skip(item) else next(sorted_items)
+               for item in sequence)
+
 
 def recombineListElements(llist, com):
     """Merge items that start/end with `com` with the next/previous ones."""
--- tests/calc/lib/test_sequence_utils.py
+++ tests/calc/lib/test_sequence_utils.py
@@ -10,7 +10,46 @@ __license__ = 'GPLv3+'
 import pytest
 from pytest_cases import parametrize
 
+from viperleed.calc.lib.sequence_utils import conditional_sort
 from viperleed.calc.lib.sequence_utils import recombineListElements
+
+
+class TestConditionalSort:
+    """Tests for the conditional_sort function."""
+
+    _valid = {
+        'empty': ([], lambda x: False, None, []),
+        'mixed types': (['cherry', 'apple', 8, 'banana', 2],
+                        lambda x: isinstance(x, str),
+                        None,
+                        ['cherry', 'apple', 2, 'banana', 8]),
+        'no skipping': ([5, 3, 4, 2, 1], lambda x: False, None,
+                        [1, 2, 3, 4, 5]),
+        # pylint: disable-next=magic-value-comparison
+        'skip one': ((5, 3, 4, 2, 1), lambda x: x == 3, None,
+                     (1, 3, 2, 4, 5)),
+        'with key': (
+            # pylint: disable-next=magic-value-comparison
+            ['apple_long', 'banana', 'cherry'], lambda x: x == 'banana', len,
+            ['cherry', 'banana', 'apple_long']),
+        }
+
+    @parametrize('seq,skip,key,expect', _valid.values(), ids=_valid)
+    def test_valid(self, seq, skip, key, expect):
+        """Check correctness of result with valid arguments."""
+        seq_sorted = conditional_sort(seq, skip, key=key)
+        assert seq_sorted == expect
+
+    _raises = {
+        'not a sequence': (None, lambda x: False),
+        'skip not callable': ([1, 2, 3], 'not_callable'),
+        }
+
+    @parametrize(args=_raises.values(), ids=_raises)
+    def test_raises(self, args):
+        """Check complaints for invalid arguments."""
+        with pytest.raises(TypeError):
+            conditional_sort(*args)
 
 
 class TestRecombineListElements:
