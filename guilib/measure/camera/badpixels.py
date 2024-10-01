d0e1802492eadbc9ed3b5b4182cd95a3dc51b0b6
FlorianDoerr <79530483+FlorianDoerr@users.noreply.github.com>
2024-10-01 15:38:53 +0200

    minor: docstring
    Co-authored-by: Michele Riva <riva@iap.tuwien.ac.at>

--- guilib/measure/camera/badpixels.py
+++ guilib/measure/camera/badpixels.py
@@ -1354,6 +1354,10 @@ class BadPixels:
             raise RuntimeError(f"{self.__class__.__name__}: No "
                                "bad pixel coordinates to write.")
 
+        # Since numpy 2.0, numpy data types are printed out together
+        # with scalars. This means that str(np.int(x)) == 'np.int(x)'
+        # rather than 'x'. Reverting the behavior to the v1.25 one
+        # ensures that we can then read the information back using ast.
         with np.printoptions(legacy='1.25'):
             # Prepare the column contents
             columns = [
