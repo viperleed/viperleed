9fdc966162dcc81707b58a2fde7e3a3401616c2e
FlorianDoerr <79530483+FlorianDoerr@users.noreply.github.com>
2024-10-09 16:20:43 +0200

    minor: docstring
    Co-authored-by: Michele Riva <riva@iap.tuwien.ac.at>

--- guilib/measure/hardwarebase.py
+++ guilib/measure/hardwarebase.py
@@ -282,7 +282,7 @@ def get_object_settings(obj_cls, obj_info, **kwargs):
 
     Parameters
     ----------
-    obj_cls : object
+    obj_cls : type
         The class of the object to get settings for.
     obj_info : SettingsInfo
         This SettingsInfo is necessary to determine the correct
