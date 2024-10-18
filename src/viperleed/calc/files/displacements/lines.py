0582cdcc06be9ba19e8d4e86a5513e12d69716f8
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-10-18 10:53:47 +0200

    feat: update lines

--- src/viperleed/calc/files/displacements/lines.py
+++ src/viperleed/calc/files/displacements/lines.py
@@ -21,14 +21,14 @@ def _get_target(label, which):
 class GeoDeltaLine:
     def __init__(self, line, label, which, direction, start, stop, step):
         self.line = line
-        self.target = _get_target(label, which)
+        self.targets = _get_target(label, which)
         self.direction = Direction(direction)
         self.range = DisplacementsRange(start, stop, step)
 
     def __eq__(self, other):
         if isinstance(other, GeoDeltaLine):
             return (
-                self.target == other.target
+                self.targets == other.targets
                 and self.direction == other.direction
                 and self.range == other.range
             )
@@ -40,13 +40,13 @@ class GeoDeltaLine:
 class VibDeltaLine:
     def __init__(self, line, label, which, start, stop, step):
         self.line = line
-        self.target = _get_target(label, which)
+        self.targets = _get_target(label, which)
         self.range = DisplacementsRange(start, stop, step)
 
     def __eq__(self, other):
         if isinstance(other, VibDeltaLine):
             return (
-                self.target == other.target
+                self.targets == other.targets
                 and self.direction == other.direction
                 and self.range == other.range
             )
@@ -59,13 +59,13 @@ class VibDeltaLine:
 class OccDeltaLine:
     def __init__(self, line, label, which, chem_blocks):
         self.line = line
-        self.target = _get_target(label, which)
+        self.targets = _get_target(label, which)
         self.chem_blocks = chem_blocks
 
     def __eq__(self, other):
         if isinstance(other, OccDeltaLine):
             return (
-                self.target == other.target
+                self.targets == other.targets
                 and self.chem_blocks == other.chem_blocks
             )
         return False
@@ -78,7 +78,17 @@ class ConstraintLine:
     def __init__(self, line, constraint_type, targets, direction, value):
         self.line = line
         self.constraint_type = constraint_type
-        self.parameters = parameters
+        if self.constraint_type == "geo":
+            if direction is not None:
+                self.direction = Direction(direction)
+            else:
+                self.direction = None
+        elif self.constraint_type != "geo" and direction is not None:
+            raise ValueError(
+                f"A direction is not allowed for {self.constraint_type} "
+                f"constraints."
+            )
+        self.targets = BSTarget(targets)
         self.value = value
 
     def __eq__(self, other):
