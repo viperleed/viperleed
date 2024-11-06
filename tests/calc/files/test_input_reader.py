4129c0b3a2bb32be3b0c050d3aeba33e8aa28e6a
Michele Riva <riva@iap.tuwien.ac.at>
2024-11-06 18:48:35 +0100

    chore,tests: clean-up tests

--- tests/calc/files/test_input_reader.py
+++ tests/calc/files/test_input_reader.py
@@ -1,97 +1,87 @@
 """Tests for module viperleed.calc.files.iorfactor."""
 
-__authors__ = ("Alexander M. Imre (@amimre)",)
-__copyright__ = "Copyright (c) 2019-2024 ViPErLEED developers"
-__created__ = "2024-11-06"
-__license__ = "GPLv3+"
+__authors__ = (
+    'Alexander M. Imre (@amimre)',
+    'Michele Riva (@michele-riva)',
+    )
+__copyright__ = 'Copyright (c) 2019-2024 ViPErLEED developers'
+__created__ = '2024-11-06'
+__license__ = 'GPLv3+'
+
+from io import StringIO
 
 import pytest
-from io import StringIO
-from viperleed.calc.files.input_reader import (
-    InputStreamReader,
-    InputFileReader,
-    ShouldSkipLineError,
-)
+from pytest_cases import parametrize
+
+from viperleed.calc.files.input_reader import InputStreamReader
+from viperleed.calc.files.input_reader import InputFileReader
+from viperleed.calc.files.input_reader import ShouldSkipLineError
+
+from ...helpers import not_raises
 
 
-class MockInputStreamReader(InputStreamReader):
-    """
-    Mock implementation of InputStreamReader for testing purposes.
-
-    This class provides a concrete implementation of the abstract
-    `_read_one_line` method from InputReader to facilitate testing of the
-    InputStreamReader functionality.
-    """
-
-    def __init__(self, source, noisy=True):
-        super().__init__(source, noisy)
-
-    def _read_one_line(self, line):
-        if "skip" in line:
-            raise ShouldSkipLineError("Line contains 'skip'")
-        return line.strip()
+def raise_on_skip(self, line):  # pylint: disable=unused-argument
+    """Raise if line contains 'skip'."""
+    # pylint: disable-next=magic-value-comparison
+    if 'skip' in line:
+        raise ShouldSkipLineError('Line contains \'skip\'')
+    return line.strip()
 
 
+# pylint: disable-next=too-few-public-methods  # Only one abstract
 class MockInputFileReader(InputFileReader):
-    """
-    Mock implementation of InputFileReader for testing purposes.
+    """A concrete InputFileReader for testing."""
 
-    This class provides a concrete implementation of the abstract
-    `_read_one_line` method from InputReader to facilitate testing of the
-    InputFileReader functionality.
-    """
+    _read_one_line = raise_on_skip
 
-    def __init__(self, filename, noisy=True):
-        super().__init__(filename, noisy)
 
-    def _read_one_line(self, line):
-        if "skip" in line:
-            raise ShouldSkipLineError("Line contains 'skip'")
-        return line.strip()
+# pylint: disable-next=too-few-public-methods  # Only one abstract
+class MockInputStreamReader(InputStreamReader):
+    """A concrete InputStreamReader for testing."""
+
+    _read_one_line = raise_on_skip
 
 
 class TestInputStreamReader:
     """Tests for the InputStreamReader class."""
 
-    def test_valid_input(self):
-        source = StringIO("line1\nline2\nline3\n")
-        reader = MockInputStreamReader(source)
-        lines = [line for line in reader]
-        assert lines == ["line1", "line2", "line3"]
+    _valid = {
+        'no skip': ('line1\nline2\nline3\n', ('line1', 'line2', 'line3')),
+        'skip one': ('line1\nskip this line\nline3\n', ('line1', 'line3')),
+        }
+
+    @parametrize('lines,expect', _valid.values(), ids=_valid)
+    def test_iteration(self, lines, expect):
+        """Check that iteration over a stream yields the expected lines."""
+        reader = MockInputStreamReader(StringIO(lines))
+        assert tuple(reader) == expect
 
     def test_invalid_input(self):
+        """Check complaints when a non-stream object is given."""
         with pytest.raises(TypeError):
-            MockInputStreamReader("not a stream")
-
-    def test_skip_line(self):
-        source = StringIO("line1\nskip this line\nline3\n")
-        reader = MockInputStreamReader(source)
-        lines = [line for line in reader]
-        assert lines == ["line1", "line3"]
+            MockInputStreamReader('not a stream')
 
 
 class TestInputFileReader:
     """Tests for the InputFileReader class."""
 
-    def test_file_not_found(self, tmp_path):
-        non_existent_file = tmp_path / "non_existent.txt"
+    def test_file_not_found(self):
+        """Check complaints when accessing a non-existing file."""
+        non_existent_file = '_this_file__DOES_non_exist.funny_ext'
         with pytest.raises(FileNotFoundError):
             MockInputFileReader(non_existent_file)
 
-    def test_context_manager(self, tmp_path):
-        # Create a temporary test file
-        test_file = tmp_path / "test.txt"
-        test_file.write_text("line1\nline2\nline3\n")
+    _files = {
+        'no skip': ('line1\nline2\nline3\n', ('line1', 'line2', 'line3')),
+        'skip one': ('line1\nskip this line\nline3\n', ('line1', 'line3')),
+        }
+
+    @parametrize('contents,expect', _files.values(), ids=_files)
+    def test_context_manager(self, contents, expect, tmp_path):
+        """Check correct reading from a file."""
+        test_file = tmp_path / 'test.txt'
+        test_file.write_text(contents)
 
         with MockInputFileReader(test_file) as reader:
-            lines = [line for line in reader]
-        assert lines == ["line1", "line2", "line3"]
-
-    def test_skip_line(self, tmp_path):
-        # Create a temporary test file
-        test_file = tmp_path / "test_skip.txt"
-        test_file.write_text("line1\nskip this line\nline3\n")
-
-        with MockInputFileReader(test_file) as reader:
-            lines = [line for line in reader]
-        assert lines == ["line1", "line3"]
+            lines = tuple(reader)
+        assert lines == expect
