viperleed/viperleed

commit 4409dbc44326705ade7cc77c3333877876a14f61

feat,refactor: calc.constants module
More maintainable than having constants in __init__ and cleanup, and also less prone to cyclic imports in the future.

In the meanwhile, also improve readability of a few pieces of code (using pathlib.Path, f-srings, unrolling indented if...else, ...)

src/viperleed/calc/sections/deltas.py
@@ -17,6 +17,9 @@ import subprocess
 
 import numpy as np
 
+from viperleed.calc.constants import DEFAULT_DELTAS
+from viperleed.calc.constants import DEFAULT_SUPP
+from viperleed.calc.constants import DEFAULT_TENSORS
 from viperleed.calc.files import iodeltas
 from viperleed.calc.files import iotensors
 from viperleed.calc.files.beams import writeAUXBEAMS
@@ -88,101 +91,124 @@ class DeltaRunTask():
 
 
 def runDelta(runtask):
-    """Function meant to be executed by parallelized workers. Executes a
-    DeltaRunTask."""
-    home = os.getcwd()
-    base = runtask.comptask.basedir
-    workname = "calculating_"+runtask.deltaname
-    workfolder = os.path.join(base, workname)
-    # make folder and go there:
-    if os.path.isdir(workfolder):
-        logger.warning("Folder "+workname+" already exists. "
-                       "Contents may get overwritten.")
-    else:
-        os.mkdir(workfolder)
+    """Execute a single delta-amplitude DeltaRunTask.
+
+    This function is meant to be executed by parallelized workers.
+
+    Parameters
+    ----------
+    runtask : DeltaRunTask
+        The delta-amplitudes calculation to be executed.
+
+    Returns
+    -------
+    error_info : str
+        An message with information about errors occurred during
+        execution of `runtask`.
+    """
+    home = Path.cwd()
+    base = Path(runtask.comptask.basedir).resolve()
+    workfolder = base / f'calculating_{runtask.deltaname}'
+    # Make folder and go there:
+    try:
+        workfolder.mkdir()
+    except FileExistsError:
+        logger.warning(f'Folder {workfolder.name} already exists. '
+                       'Contents may get overwritten.')
     os.chdir(workfolder)
-    # get tensor file
-    if os.path.isfile(os.path.join(base, "Tensors", runtask.tensorname)):
-        try:
-            shutil.copy2(os.path.join(base, "Tensors", runtask.tensorname),
-                         "AMP")
-        except Exception:
-            logger.error("Error copying Tensor file: ", exc_info=True)
-            return ("Error encountered by DeltaRunTask " + runtask.deltaname
-                    + ": Error copying Tensor file.")
-    else:
-        logger.error("Tensor file not found: " + runtask.tensorname)
-        return ("Error encountered by DeltaRunTask " + runtask.deltaname
-                + ": Tensor not found.")
-    # get executable
+
+    # Collect tensor file
+    tensor = base / DEFAULT_TENSORS / runtask.tensorname
+    if not tensor.is_file():
+        logger.error(f'{DEFAULT_TENSORS} file not found: {runtask.tensorname}')
+        return (f'Error encountered by {type(runtask).__name__} '
+                f'{runtask.deltaname}: {DEFAULT_TENSORS} not found.')
+    try:
+        shutil.copy2(tensor, 'AMP')
+    except OSError:
+        logger.error(f'Error copying {DEFAULT_TENSORS} file: ', exc_info=True)
+        return (f'Error encountered by {type(runtask).__name__} '
+                f'{runtask.deltaname}: Error copying {DEFAULT_TENSORS} file.')
+
+    # Fetch compiled executable
     exename = runtask.comptask.exename
     try:
-        shutil.copy2(os.path.join(base, runtask.comptask.foldername, exename),
-                     os.path.join(workfolder, exename))
-    except Exception:
-        logger.error("Error getting delta executable: ", exc_info=True)
-        return ("Error encountered by DeltaRunTask " + runtask.deltaname
-                + ": Failed to get delta executable.")
-    # run execution
+        shutil.copy2(base / runtask.comptask.foldername / exename,
+                     workfolder / exename)
+    except OSError:
+        logger.error('Error getting delta executable: ', exc_info=True)
+        return (f'Error encountered by {type(runtask).__name__} '
+                f'{runtask.deltaname}: Failed to get delta executable.')
+
+    # Run delta-amplitudes calculation
+    log_file = Path('delta.log')
     try:
-        with open("delta.log", "w") as log:
-            subprocess.run(os.path.join(workfolder, exename),
-                           input=runtask.din, encoding="ascii",
-                           stdout=log, stderr=log)
+        with log_file.open('w', encoding='utf-8') as log:
+            subprocess.run(str(workfolder / exename),
+                           input=runtask.din,
+                           encoding="ascii",
+                           stdout=log,
+                           stderr=log)
     except Exception:
-        logger.error("Error while executing delta-amplitudes "
-                     "calculation for " + runtask.deltaname + ". Also check "
-                     "delta log file.")
-        return ("Error encountered by DeltaRunTask " + runtask.deltaname
-                + ": Error during delta execution.")
-    # copy delta file out
+        logger.error('Error while executing delta-amplitudes calculation '
+                     f'for {runtask.deltaname}. Also check delta log file.')
+        return (f'Error encountered by {type(runtask).__name__} '
+                f'{runtask.deltaname}: Error during delta execution.')
+
+    # Copy delta file out
     try:
-        shutil.copy2(os.path.join(workfolder, "DELWV"),
-                     os.path.join(base, runtask.deltaname))
-    except Exception:
-        logger.error("Failed to copy delta output file DELWV"
-                     " to main folder as " + runtask.deltaname,
-                     exc_info=True)
-        return ("Error encountered by DeltaRunTask " + runtask.deltaname
-                + ": Failed to copy result file out.")
-    # append log
-    log = ""
+        shutil.copy2(workfolder / "DELWV", base / runtask.deltaname)
+    except OSError:
+        logger.error('Failed to copy delta output file DELWV to main '
+                     f'folder as {runtask.deltaname}', exc_info=True)
+        return (f'Error encountered by {type(runtask).__name__} '
+                f'{runtask.deltaname}: Failed to copy result file out.')
+
+    # Collate log results
+    log = ''
     try:
-        with open("delta.log", "r") as rf:
-            log = rf.read()
-    except Exception:
-        logger.warning("Could not read local delta log for "
-                       + runtask.deltaname)
-    if log != "":
-        deltalog = os.path.join(base, runtask.deltalogname)
+        log = log_file.read_text(encoding='utf-8')
+    except OSError:
+        logger.warning('Could not read local delta '
+                       f'log for {runtask.deltaname}')
+    if log:
+        deltalog = base / runtask.deltalogname
         try:
-            with open(deltalog, "a") as wf:
-                wf.write("\n\n### STARTING LOG FOR " + runtask.deltaname
-                         + " ###\n\n" + log)
-        except Exception:
-            logger.warning("Error writing delta log part "
-                           + runtask.deltaname + ": ", exc_info=True)
-    # clean up
+            with deltalog.open('a', encoding='utf-8') as collated_log:
+                collated_log.write(
+                    '\n\n'
+                    f'### STARTING LOG FOR {runtask.deltaname} ###\n\n'
+                    + log
+                    )
+        except OSError:
+            logger.warning(
+                f'Error writing delta log part {runtask.deltaname}: ',
+                exc_info=True
+                )
+
+    # Clean up
     os.chdir(home)
     try:
         shutil.rmtree(workfolder)
     except Exception:
-        logger.warning("Error deleting folder " + workname)
-    return ""
+        logger.warning(f'Error deleting folder {workfolder.name}')
+    return ''
 
 
 def compileDelta(comptask):
     """Function meant to be executed by parallelized workers. Executes a
     DeltaCompileTask."""
-    home = os.getcwd()
-    workfolder = os.path.join(comptask.basedir, comptask.foldername)
-    # make folder and go there:
-    if os.path.isdir(workfolder):
-        logger.warning("Folder "+comptask.foldername+" already exists. "
-                       "Contents may get overwritten.")
-    else:
-        os.mkdir(workfolder)
+    home = Path.cwd()
+    workfolder = (Path(comptask.basedir) / comptask.foldername).resolve()
+
+    # Make folder and go there:
+    try:
+        workfolder.mkdir()
+    except FileExistsError:
+        logger.warning(f'Folder {workfolder.name} already exists. '
+                       'Contents may get overwritten.')
     os.chdir(workfolder)
+
     # write PARAM:
     try:
         with open("PARAM", "w") as wf:
@@ -236,58 +262,56 @@ def deltas(sl, rp, subdomain=False):
     if not rp.disp_block_read:
         readDISPLACEMENTS_block(rp, sl, rp.disp_blocks[rp.search_index])
         rp.disp_block_read = True
+
     # get Tensors
-    if not os.path.isdir(os.path.join(".", "Tensors")):
-        logger.error("No Tensors directory found.")
-        raise RuntimeError("Tensors not found")
+    if not Path(DEFAULT_TENSORS).is_dir():
+        logger.error(f'No {DEFAULT_TENSORS} directory found.')
+        raise RuntimeError(f'{DEFAULT_TENSORS} not found')                      # TODO: FileNotFoundError?
     iotensors.getTensors(rp.TENSOR_INDEX)
     if 1 not in rp.runHistory:
-        dn = "Tensors_"+str(rp.TENSOR_INDEX).zfill(3)
+        load_from = Path(DEFAULT_TENSORS)
+        load_from /= f'{DEFAULT_TENSORS}_{rp.TENSOR_INDEX:03d}'
         logger.debug(
-            "Running without reference calculation, checking "
-            "input files in "+dn+" to determine original configuration.")
-        iotensors.getTensorOriStates(sl, os.path.join(".", "Tensors", dn))
+            'Running without reference calculation, checking input files '
+            f'in {load_from.name} to determine original configuration.'
+            )
+        iotensors.getTensorOriStates(sl, load_from)
         sl.restoreOriState(keepDisp=True)
+
     # if there are old deltas, fetch them
     leedbase.getDeltas(rp.TENSOR_INDEX, required=False)
     dbasic = iodeltas.generateDeltaBasic(sl, rp)
+
     # get AUXBEAMS; if AUXBEAMS is not in work folder, check SUPP folder
-    if not os.path.isfile(os.path.join(".", "AUXBEAMS")):
-        if os.path.isfile(os.path.join(".", "SUPP", "AUXBEAMS")):
-            try:
-                shutil.copy2(os.path.join(".", "SUPP", "AUXBEAMS"),
-                             "AUXBEAMS")
-            except Exception:
-                logger.warning("Failed to copy AUXBEAMS from SUPP folder. "
-                               "Generating new file...")
-                try:
-                    writeAUXBEAMS(ivbeams=rp.ivbeams, beamlist=rp.beamlist)
-                except Exception:
-                    logger.error("Exception during writeAUXBEAMS: ")
-                    raise
-        else:
-            try:
-                writeAUXBEAMS(ivbeams=rp.ivbeams, beamlist=rp.beamlist)
-            except Exception:
-                logger.error("Exception during writeAUXBEAMS: ")
-                raise
+    auxbeams_file = Path('AUXBEAMS')
+    if not auxbeams_file.is_file() and (DEFAULT_SUPP/auxbeams_file).is_file():
+        try:
+            shutil.copy2(auxbeams_file, auxbeams_file.name)
+        except OSError:
+            logger.warning(f'Failed to copy {auxbeams_file.name} from '
+                           f'{DEFAULT_SUPP} folder. Generating new file...')
+    if not auxbeams_file.is_file():
+        try:
+            writeAUXBEAMS(ivbeams=rp.ivbeams, beamlist=rp.beamlist)
+        except Exception:                                                       # TODO: better exception
+            logger.error('Exception during writeAUXBEAMS: ')
+            raise
     try:
-        with open("AUXBEAMS", "r") as rf:
-            auxbeams = rf.read()
-        if auxbeams[-1] != "\n":
-            auxbeams += "\n"
-    except Exception:
-        logger.error("Could not read AUXBEAMS for delta-input")
+        auxbeams = auxbeams_file.read_text(encoding='utf-8')
+    except OSError:
+        logger.error('Could not read {auxbeams_file.name} for delta input')
         raise
+    if not auxbeams.endswith('\n'):
+        auxbeams += '\n'
+
     # get PHASESHIFTS
     try:
-        with open("PHASESHIFTS", "r") as rf:
-            phaseshifts = rf.read()
-        if phaseshifts[-1] != "\n":
-            phaseshifts += "\n"
-    except Exception:
-        logger.error("Could not read PHASESHIFTS for delta-input")
+        phaseshifts = Path('PHASESHIFTS').read_text(encoding='utf-8')
+    except OSError:
+        logger.error('Could not read PHASESHIFTS for delta-input')
         raise
+    if not phaseshifts.endswith('\n'):
+        phaseshifts += '\n'
 
     # go through atoms, remove those that have no variation whatsoever:
     attodo = [at for at in sl if not at.is_bulk]
@@ -375,7 +399,7 @@ def deltas(sl, rp, subdomain=False):
                     "already present in the Deltas.zip file. Skipping new "
                     "calculations.")
         return
-    elif countExisting > 0:
+    if countExisting > 0:
         logger.info("{} of {} required Delta-files are already present. "
                     "Generating remaining {} files..."
                     .format(countExisting, len(atElTodo) + countExisting,
@@ -411,7 +435,7 @@ def deltas(sl, rp, subdomain=False):
     # assemble tasks
     deltaCompTasks = []  # keep track of what versions to compile
     deltaRunTasks = []   # which deltas to run
-    tensordir = "Tensors_"+str(rp.TENSOR_INDEX).zfill(3)
+    tensordir = f"{DEFAULT_TENSORS}_{rp.TENSOR_INDEX:03d}"
     tl_source = rp.get_tenserleed_directory()
     tl_path = tl_source.path
     for (at, el) in atElTodo:
@@ -502,7 +526,7 @@ def deltas(sl, rp, subdomain=False):
 
     if subdomain:   # actual calculations done in deltas_domains
         if len(deltaRunTasks) > 0:
-            rp.manifest.append("Deltas")
+            rp.manifest.append(DEFAULT_DELTAS)
         return (deltaCompTasks, deltaRunTasks)
 
     rp.updateCores()
@@ -544,7 +568,7 @@ def deltas(sl, rp, subdomain=False):
         except Exception:
             logger.warning("Error deleting delta compile folder "
                            + ct.foldername)
-    rp.manifest.append("Deltas")
+    rp.manifest.append(DEFAULT_DELTAS)
     return
 
 

src/viperleed/calc/__init__.py
@@ -49,9 +49,4 @@ __license__ = 'GPLv3+'
 
 import logging
 
-DEFAULT_HISTORY = 'history'
-DEFAULT_WORK = 'work'
-DEFAULT_WORK_HISTORY = 'workhistory'
 LOGGER = logging.getLogger(__name__)
-LOG_PREFIX = 'viperleed-calc'
-ORIGINAL_INPUTS_DIR_NAME = 'original_inputs'

src/viperleed/calc/bookkeeper/bookkeeper.py
@@ -14,13 +14,15 @@ from enum import IntEnum
 from pathlib import Path
 import shutil
 
-from viperleed.calc import ORIGINAL_INPUTS_DIR_NAME
+from viperleed.calc.constants import DEFAULT_DELTAS
+from viperleed.calc.constants import DEFAULT_OUT
+from viperleed.calc.constants import DEFAULT_SUPP
+from viperleed.calc.constants import DEFAULT_TENSORS
+from viperleed.calc.constants import ORIGINAL_INPUTS_DIR_NAME
 from viperleed.calc.lib.leedbase import getMaxTensorIndex
 from viperleed.calc.lib.log_utils import logging_silent
 from viperleed.calc.lib.time_utils import DateTimeFormat
 from viperleed.calc.sections.calc_section import ALL_INPUT_FILES
-from viperleed.calc.sections.cleanup import DEFAULT_OUT
-from viperleed.calc.sections.cleanup import DEFAULT_SUPP
 
 from . import log
 from .constants import STATE_FILES
@@ -439,8 +441,12 @@ class Bookkeeper:
 
     def _remove_tensors_and_deltas(self):
         """Delete the most recent tensor and delta files."""
-        tensor_file = f'Tensors/Tensors_{self.tensor_number:03d}.zip'
-        delta_file = f'Deltas/Deltas_{self.tensor_number:03d}.zip'
+        tensor_file = (
+            f'{DEFAULT_TENSORS}/{DEFAULT_TENSORS}_{self.tensor_number:03d}.zip'
+            )
+        delta_file = (
+            f'{DEFAULT_DELTAS}/{DEFAULT_DELTAS}_{self.tensor_number:03d}.zip'
+            )
         discard_files(self.cwd / tensor_file, self.cwd / delta_file)
 
     def _run_archive_mode(self):

src/viperleed/calc/bookkeeper/cli.py
@@ -16,6 +16,7 @@ __license__ = 'GPLv3+'
 from argparse import Action
 from pathlib import Path
 
+from viperleed.calc.constants import DEFAULT_OUT
 from viperleed.cli_base import ViPErLEEDCLI
 
 from .bookkeeper import Bookkeeper
@@ -73,7 +74,8 @@ class BookkeeperCLI(ViPErLEEDCLI, cli_name='bookkeeper'):
         what_next.add_argument(
             '-a', '--archive',
             help=('Store last run in history. Overwrite PARAMETERS, POSCAR &'
-                  'VIBROCC from OUT. Runs after viperleed.calc by default.'),
+                  f'VIBROCC from {DEFAULT_OUT}. Runs after viperleed.calc by '
+                  'default.'),
             action=StoreBookkeeperMode,
             )
         what_next.add_argument(

src/viperleed/calc/bookkeeper/constants.py
@@ -12,7 +12,7 @@ __license__ = 'GPLv3+'
 
 import re
 
-from viperleed.calc import LOG_PREFIX
+from viperleed.calc.constants import LOG_PREFIX
 
 CALC_LOG_PREFIXES = (
     LOG_PREFIX,

src/viperleed/calc/bookkeeper/history/explorer.py
@@ -13,7 +13,7 @@ __license__ = 'GPLv3+'
 from collections import defaultdict
 from operator import attrgetter
 
-from viperleed.calc import DEFAULT_HISTORY
+from viperleed.calc.constants import DEFAULT_HISTORY
 
 from ..utils import make_property
 from ..utils import needs_update_for_attr
@@ -75,7 +75,7 @@ class HistoryExplorer:
 
     def check_last_folder_consistent(self):
         """Raise if the last folder in history has some inconsistency.
-        
+
         Raises
         ------
         FileNotFoundError

src/viperleed/calc/bookkeeper/root_explorer.py
@@ -18,9 +18,9 @@ from pathlib import Path
 import shutil
 import re
 
-from viperleed.calc import DEFAULT_WORK_HISTORY
-from viperleed.calc.sections.cleanup import DEFAULT_OUT
-from viperleed.calc.sections.cleanup import DEFAULT_SUPP
+from viperleed.calc.constants import DEFAULT_OUT
+from viperleed.calc.constants import DEFAULT_SUPP
+from viperleed.calc.constants import DEFAULT_WORK_HISTORY
 
 from .constants import CALC_LOG_PREFIXES
 from .constants import STATE_FILES

src/viperleed/calc/classes/rparams/rparams.py
@@ -27,6 +27,8 @@ import shutil
 import numpy as np
 
 from viperleed.calc.classes.searchpar import SearchPar
+from viperleed.calc.constants import DEFAULT_OUT
+from viperleed.calc.constants import DEFAULT_SUPP
 from viperleed.calc.files import beams as iobeams
 from viperleed.calc.files.iodeltas import checkDelta
 from viperleed.calc.lib import leedbase
@@ -42,8 +44,6 @@ from viperleed.calc.lib.time_utils import ExecutionTimer
 from viperleed.calc.lib.version import Version
 from viperleed.calc.files.tenserleed import get_tenserleed_sources
 from viperleed.calc.sections.calc_section import EXPBEAMS_NAMES
-from viperleed.calc.sections.cleanup import DEFAULT_OUT
-from viperleed.calc.sections.cleanup import DEFAULT_SUPP
 
 from .defaults import DEFAULTS, NO_VALUE, TENSERLEED_FOLDER_NAME
 from .limits import PARAM_LIMITS

commit 595b754b74fd7adea7435d145fdc8b700a97b2a9

fix: unintended bug
Was introduced in 4409dbc44326705ade7cc77c3333877876a14f61

src/viperleed/calc/sections/deltas.py
@@ -286,7 +286,7 @@ def deltas(sl, rp, subdomain=False):
     auxbeams_file = Path('AUXBEAMS')
     if not auxbeams_file.is_file() and (DEFAULT_SUPP/auxbeams_file).is_file():
         try:
-            shutil.copy2(auxbeams_file, auxbeams_file.name)
+            shutil.copy2(DEFAULT_SUPP/auxbeams_file, auxbeams_file.name)
         except OSError:
             logger.warning(f'Failed to copy {auxbeams_file.name} from '
                            f'{DEFAULT_SUPP} folder. Generating new file...')