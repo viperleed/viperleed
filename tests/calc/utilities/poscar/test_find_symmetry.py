68f2bdc1e01e1392f8896309df0abff15c843e91
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-11-20 15:20:45 +0100

    test: test for find symmetry utility parser

---
+++ tests/calc/utilities/poscar/test_find_symmetry.py
@@ -0,0 +1,33 @@
+"""Tests for the viperleed poscar find symmetry utility."""
+
+__authors__ = (
+    'Alexander M. Imre (@amimre)',
+    )
+__copyright__ = 'Copyright (c) 2019-2024 ViPErLEED developers'
+__created__ = '2024-11-20'
+__license__ = 'GPLv3+'
+
+import pytest_cases
+
+from viperleed.utilities.poscar.find_symmetry import FindSymmetryCLI
+
+from ... import poscar_slabs
+from ...tags import CaseTag as Tag
+
+with_info = pytest_cases.parametrize_with_cases(
+    'test_slab',
+    cases=poscar_slabs.CasePOSCARSlabs.case_poscar,
+)
+
+
+@with_info
+def test_find_symmetry_parser(test_slab):
+    slab, _, info = test_slab
+    expected_plane_group = info.symmetry.hermann
+    parser = FindSymmetryCLI().parser
+    args = parser.parse_args([])
+
+    # Check if the correct symmetry group is found
+    found_plane_group = FindSymmetryCLI().process_slab(slab, args).foundplanegroup
+    assert found_plane_group == expected_plane_group
+
