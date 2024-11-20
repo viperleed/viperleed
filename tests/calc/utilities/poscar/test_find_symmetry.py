cfd776c9972c0532de8d18a9a5a1f6d5bbaf9db2
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-11-20 16:23:08 +0100

    test: test writing to outfile

--- tests/calc/utilities/poscar/test_find_symmetry.py
+++ tests/calc/utilities/poscar/test_find_symmetry.py
@@ -7,6 +7,7 @@ __copyright__ = 'Copyright (c) 2019-2024 ViPErLEED developers'
 __created__ = '2024-11-20'
 __license__ = 'GPLv3+'
 
+import pytest
 import pytest_cases
 
 from viperleed.utilities.poscar.find_symmetry import FindSymmetryCLI
@@ -23,12 +24,30 @@ with_info = pytest_cases.parametrize_with_cases(
 def test_find_symmetry_parser(test_slab):
     slab, _, info = test_slab
     expected_plane_group = info.symmetry.hermann
-    cli = FindSymmetryCLI()
-    parser = cli.parser
+    find_symmetry_cli = FindSymmetryCLI()
+    parser = find_symmetry_cli.parser
     args = parser.parse_args([])
 
     # Check if the correct symmetry group is found
     found_plane_group = (
-        cli.process_slab(slab, args).foundplanegroup)
+        find_symmetry_cli.process_slab(slab, args).foundplanegroup)
     assert found_plane_group == expected_plane_group
 
+@with_info
+def test_find_symmetry_to_outfile(test_slab, tmp_path):
+    slab, _, info = test_slab
+    out_path = tmp_path / f'out_{info.poscar.name}.txt'
+    expected_plane_group = info.symmetry.hermann
+    cli = FindSymmetryCLI()
+    parser = cli.parser
+    args = parser.parse_args(['-o', str(out_path)])
+
+    # process the slab and write the output to the outfile
+    processed_slab = cli.process_slab(slab, args)
+    cli.write_output(processed_slab, args)
+    args.outfile.close()
+
+    # Check if the correct symmetry group is written to the outfile
+    with open(out_path, 'r') as f:
+        read_plane_group = f.read().strip()
+    assert read_plane_group == expected_plane_group
