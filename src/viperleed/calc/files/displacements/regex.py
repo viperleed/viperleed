a89a5bd62c90c7c765b059af635efa8368e5c576
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-11-21 17:09:59 +0100

    fix: allow target '*' in DISPLACEMENTS

--- src/viperleed/calc/files/displacements/regex.py
+++ src/viperleed/calc/files/displacements/regex.py
@@ -13,7 +13,7 @@ OFFSETS_LINE_PATTERN = re.compile(
 )
 
 GEO_LINE_PATTERN = re.compile(
-    r'^(?P<label>\*?\w+)'
+    r'^(?P<label>\*|\*?\w+)'
     r'(?:\s+(?P<which>L\(\d+(-\d+)?\)|\d+(-\d+)?(\s+\d+(-\d+)?)*)?)?'
     r'\s+(?P<dir>[a-zA-Z]+(?:\[[^\]]+\]|\([^\)]+\))?)'
     r'\s*=\s*(?P<start>-?\d+(\.\d+)?)'
@@ -21,13 +21,13 @@ GEO_LINE_PATTERN = re.compile(
     r'(?:\s+(?P<step>-?\d+(\.\d+)?))?$'
 )
 VIB_LINE_PATTERN = re.compile(
-    r'^(?P<label>\*?\w+)(?:\s+(?P<which>L\(\d+(-\d+)?\)|\d+(-\d+)?(\s+\d+(-\d+)?)*)?)?'
+    r'^(?P<label>\*|\*?\w+)(?:\s+(?P<which>L\(\d+(-\d+)?\)|\d+(-\d+)?(\s+\d+(-\d+)?)*)?)?'
     r'\s*=\s*(?P<start>-?\d+(\.\d+)?)'
     r'\s+(?P<stop>-?\d+(\.\d+)?)'
     r'(?:\s+(?P<step>-?\d+(\.\d+)?))?$'
 )
 OCC_LINE_PATTERN = re.compile(
-    r'^(?P<label>\*?\w+)'
+    r'^(?P<label>\*|\*?\w+)'
     r'(?:\s+(?P<which>L\(\d+(-\d+)?\)|\d+(-\d+)?(\s+\d+(-\d+)?)*)?)?'
     r'\s*=\s*(?P<chem_blocks>(?P<chem>\w+)\s+(?P<start>-?\d+(\.\d+)?)'
     r'\s+(?P<stop>-?\d+(\.\d+)?)(?:\s+(?P<step>-?\d+(\.\d+)?))?'
