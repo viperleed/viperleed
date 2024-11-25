d90d172236e71811c553dba2b1bc77a1065ce69b
Michele Riva <riva@iap.tuwien.ac.at>
2024-11-25 11:38:41 +0100

    chore, tests: better __doc__

--- tests/calc/lib/test_dataclass_utils.py
+++ tests/calc/lib/test_dataclass_utils.py
@@ -138,13 +138,13 @@ class TestCheckTypes:
             check_types(instance)
 
     def test_dont_check_items(self):
-        """Check complaints for an invalid type of list items."""
+        """Check there's no complaints for invalid types of list items."""
         instance = SampleClass(attr=1, mutable_optional=[None, 'string'])
         with not_raises(TypeError):  # We don't check items
             check_types(instance)
 
     def test_dont_check_initvar(self):
-        """Check complaints for an invalid type of list items."""
+        """Check there's no complaints for InitVar arguments."""
         instance = ComplexClass(attr=1, init_only_var='something wrong')
         with not_raises(TypeError):
             check_types(instance)
@@ -152,7 +152,7 @@ class TestCheckTypes:
     def test_init_only(self):
         """Check no complaints if only types of init fields are verified."""
         instance = SampleClass(1)
-        instance.non_init_with_default = []  # should be int
+        instance.non_init_with_default = []  # Should be int
         with pytest.raises(TypeError):
             check_types(instance)
         with not_raises(TypeError):
