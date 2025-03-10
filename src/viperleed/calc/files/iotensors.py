39d8499852a2fc12a1f56e0385a2ad58dd88fbf3
Michele Riva <riva@iap.tuwien.ac.at>
2025-03-10 07:36:07 +0100

    chore: __doc__

--- src/viperleed/calc/files/iotensors.py
+++ src/viperleed/calc/files/iotensors.py
@@ -138,7 +138,7 @@ def getTensorOriStates(sl, path):
 
 
 def unpack_tensor_file(tensor_file, unpack_path):                               # TODO: maybe it would be nicer to read directly from the zip file. See also https://github.com/viperleed/viperleed/pull/305#discussion_r1971601291
-    """Extract the contents of tensor_file into unpack_path."""
+    """Extract the contents of `tensor_file` into `unpack_path`."""
     unpack_path.mkdir(parents=True, exist_ok=True)
     with ZipFile(tensor_file, 'r') as archive:
         archive.extractall(unpack_path)
