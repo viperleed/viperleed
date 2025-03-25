viperleed/viperleed

commit 0f1dd479890fb0f9f9068ecccd84dfa7bef207f4

refactor: 'moved-' to module constant

src/viperleed/calc/bookkeeper/history/folder.py
@@ -29,6 +29,7 @@ from viperleed.calc.constants import DEFAULT_HISTORY
 from viperleed.calc.lib.dataclass_utils import frozen
 from viperleed.calc.lib.dataclass_utils import non_init_field
 from viperleed.calc.lib.dataclass_utils import set_frozen_attr
+from viperleed.calc.sections.cleanup import MOVED_LABEL
 
 
 LOGGER = logging.getLogger(__name__)
@@ -106,8 +107,10 @@ class IncompleteHistoryFolder:
         # match "(moved-)\d{6}-\d{6}". The "moved-" bit is also
         # very unlikely, as it is only added by bookkeeper if it
         # does not find a log file.
-        match_ = re.fullmatch(r'.*_(?P<timestamp>(moved-)?\d{6}-\d{6})',
-                              match_['rest'])
+        match_ = re.fullmatch(
+            rf'.*_(?P<timestamp>({MOVED_LABEL})?\d{6}-\d{6})',
+            match_['rest']
+            )
         if not match_:
             raise ValueError(
                 f'Invalid {DEFAULT_HISTORY} folder {path.name} at '

src/viperleed/calc/bookkeeper/bookkeeper.py
@@ -32,6 +32,7 @@ from viperleed.calc.constants import DEFAULT_SUPP
 from viperleed.calc.lib.log_utils import logging_silent
 from viperleed.calc.lib.time_utils import DateTimeFormat
 from viperleed.calc.sections.calc_section import ALL_INPUT_FILES
+from viperleed.calc.sections.cleanup import MOVED_LABEL
 
 
 LOGGER = logging.getLogger(__name__)
@@ -192,7 +193,7 @@ class Bookkeeper:
             self._root.collect_info()
             self._state_info['timestamp'] = (
                 self._root.calc_timestamp  # If a log file was found
-                or f'moved-{DateTimeFormat.FILE_SUFFIX.now()}'
+                or f'{MOVED_LABEL}{DateTimeFormat.FILE_SUFFIX.now()}'
                 )
 
             # Infer which history subfolder we should be handling

src/viperleed/calc/bookkeeper/history/entry/string_field.py
@@ -21,6 +21,7 @@ from viperleed.calc.bookkeeper.history.errors import FixableSyntaxError
 from viperleed.calc.lib.dataclass_utils import frozen
 from viperleed.calc.lib.dataclass_utils import non_init_field
 from viperleed.calc.lib.dataclass_utils import set_frozen_attr
+from viperleed.calc.sections.cleanup import MOVED_LABEL
 
 
 @frozen
@@ -49,10 +50,10 @@ _FOLDER_NAME_RE = re.compile(
     r't\d+'          # Tensor number
     r'\.r\d+'        # Run number, given by bookkeeper
     r'(?:\.\d+)?'    # Optional search number, from workhistory number
-    '_(?:moved-)?'   # Optional, if bookkeeper runs twice
+    f'_(?:{MOVED_LABEL})?'   # Optional, if bookkeeper runs twice
     r'\d{6}-\d{6})'  # Date and time, with log-name format
     rf'(?:_(?P<job_name>{_JOB_NAME_PATTERN}))?'
-    r'(?P<tail>_moved-\d{6}-\d{6})?'  # Ran twice within one second
+    rf'(?P<tail>_{MOVED_LABEL}\d{{6}}-\d{{6}})?'  # Ran twice within 1s
     )
 
 
@@ -102,7 +103,7 @@ class FolderField(StringField, tag=FieldTag.FOLDER, mandatory=True):
         if not match_:
             raise EntrySyntaxError(
                 'Does not have format '
-                'tTTT.rRRR_[moved-]yymmdd-HHMMSS[_job_name]'
+                f'tTTT.rRRR_[{MOVED_LABEL}]yymmdd-HHMMSS[_job_name]'
                 )
         set_frozen_attr(self, 'value', match_.group())  # Stripped
         set_frozen_attr(self, 'job_name', match_['job_name'])

src/viperleed/calc/bookkeeper/history/entry/time_field.py
@@ -27,6 +27,7 @@ from viperleed.calc.lib.dataclass_utils import frozen
 from viperleed.calc.lib.dataclass_utils import replace_values
 from viperleed.calc.lib.dataclass_utils import set_frozen_attr
 from viperleed.calc.lib.time_utils import DateTimeFormat
+from viperleed.calc.sections.cleanup import MOVED_LABEL
 
 
 _OUTDATED = 'outdated format'
@@ -152,7 +153,7 @@ class TimestampField(FieldBase, tag=FieldTag.TIMESTAMP, mandatory=True):
     def _sanitize_string_value(self):
         """Clean up a string value."""
         try:
-            value_str = self.value.replace('moved-', '').strip()
+            value_str = self.value.replace(MOVED_LABEL, '').strip()
         except AttributeError:
             # Not a string. Error pops up in super()._check_str_value()
             return

src/viperleed/calc/sections/cleanup.py
@@ -115,6 +115,10 @@ _OUT_FILES = (
 # from previous viperleed.calc executions from the work directory
 PREVIOUS_LABEL = 'previous'
 
+# Label given to workhistory folders from previous runs when no
+# log file was found
+MOVED_LABEL = 'moved-'
+
 # Output files that may be inputs in future runs - keep during prerun
 _IOFILES = (
     'control.chem',
@@ -514,7 +518,7 @@ def _find_next_workistory_dir_name(rpars, prerun):
                 if f.is_file() and f.name not in rpars.manifest
                 )
         except ValueError:  # No relevant log files
-            old_timestamp = f'moved-{rpars.timestamp}'
+            old_timestamp = f'{MOVED_LABEL}{rpars.timestamp}'
         else:
             old_timestamp = most_recent_log[-17:-4]
         return f'{dirname_prefix}_{PREVIOUS_LABEL}_{old_timestamp}'

commit f02150cce05f88c9b7730c8a4559996bf4d61fa6

fix: wrong regex pattern
Introduced in 0f1dd479890fb0f9f9068ecccd84dfa7bef207f4. Forgotten to escape curly braces.

src/viperleed/calc/bookkeeper/history/folder.py
@@ -108,8 +108,8 @@ class IncompleteHistoryFolder:
         # very unlikely, as it is only added by bookkeeper if it
         # does not find a log file.
         match_ = re.fullmatch(
-            rf'.*_(?P<timestamp>({MOVED_LABEL})?\d{6}-\d{6})',
-            match_['rest']
+            rf'.*_(?P<timestamp>({MOVED_LABEL})?\d{{6}}-\d{{6}})',
+            match_['rest'],
             )
         if not match_:
             raise ValueError(