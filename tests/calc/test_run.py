viperleed/viperleed

commit af8695daea148f3ef8320b75b0b09d572089de1d

refactor: pylint

src/viperleed/calc/run.py
@@ -9,7 +9,7 @@ __authors__ = (
     'Alexander M. Imre (@amimre)',
     'Michele Riva (@michele-riva)',
     )
-__copyright__ = 'Copyright (c) 2019-2024 ViPErLEED developers'
+__copyright__ = 'Copyright (c) 2019-2025 ViPErLEED developers'
 __created__ = '2019-11-12'  # Was originally tleedm.py
 __license__ = 'GPLv3+'
 
@@ -19,7 +19,6 @@ from pathlib import Path
 import shutil
 
 from viperleed import __version__
-from viperleed.calc import LOGGER
 from viperleed.calc.classes.rparams.rparams import Rparams
 from viperleed.calc.constants import DEFAULT_OUT
 from viperleed.calc.constants import DEFAULT_SUPP
@@ -38,6 +37,9 @@ from viperleed.calc.sections.initialization import (
     warn_if_slab_has_atoms_in_multiple_c_cells
     )
 from viperleed.calc.sections.run_sections import section_loop
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 def run_calc(

src/viperleed/calc/classes/rparams/defaults.py
@@ -1,4 +1,4 @@
-"""Moule defaults of viperleed.calc.classes.rparams.
+"""Module defaults of viperleed.calc.classes.rparams.
 
 Defines the default values of 'simple' user PARAMETERS. Not-so-simple
 parameters, which are defined as their own classes in package special,

src/viperleed/calc/classes/rparams/domain_params.py
@@ -1,4 +1,4 @@
-"""Moule domain_params of viperleed.calc.classes.rparams.
+"""Module domain_params of viperleed.calc.classes.rparams.
 
 Defines the DomainParameters class. Contains of information useful
 when a calculation with multiple structural domains is carried out.

src/viperleed/calc/classes/rparams/limits.py
@@ -1,4 +1,4 @@
-"""Moule limits of viperleed.calc.classes.rparams.
+"""Module limits of viperleed.calc.classes.rparams.
 
 Defines the limiting values of 'simple' user PARAMETERS. Not-so-simple
 parameters, which are defined as their own classes in package special,

src/viperleed/calc/classes/rparams/rparams.py
@@ -38,8 +38,6 @@ from viperleed.calc.files.tenserleed import get_tenserleed_sources
 from viperleed.calc.lib import fortran_utils
 from viperleed.calc.lib import leedbase
 from viperleed.calc.lib.base import available_cpu_count
-from viperleed.calc.lib.checksums import KNOWN_TL_VERSIONS
-from viperleed.calc.lib.checksums import UnknownTensErLEEDVersionError
 from viperleed.calc.lib.context import execute_in_dir
 from viperleed.calc.lib.matplotlib_utils import CAN_PLOT
 from viperleed.calc.lib.matplotlib_utils import close_figures
@@ -50,7 +48,8 @@ from viperleed.calc.lib.time_utils import ExecutionTimer
 from viperleed.calc.lib.version import Version
 from viperleed.calc.sections.calc_section import EXPBEAMS_NAMES
 
-from .defaults import DEFAULTS, NO_VALUE, TENSERLEED_FOLDER_NAME
+from .defaults import DEFAULTS
+from .defaults import NO_VALUE
 from .limits import PARAM_LIMITS
 from .special.base import NotASpecialParameterError
 from .special.base import SpecialParameter

src/viperleed/calc/classes/rparams/special/layer_cuts.py
@@ -16,7 +16,6 @@ __license__ = 'GPLv3+'
 
 from collections.abc import Sequence
 from enum import IntEnum, auto
-import itertools
 from numbers import Real
 import re
 from typing import Any

src/viperleed/calc/from_ase.py
@@ -14,7 +14,6 @@ from dataclasses import FrozenInstanceError, dataclass
 from io import StringIO
 import logging
 from numbers import Real
-import os
 from pathlib import Path
 import shutil
 from typing import Any, Sequence

src/viperleed/calc/lib/checksums.py
@@ -95,10 +95,8 @@ class TLSourceFile:
         base_path = self.path.parents[1]
         self._name = self.path.relative_to(base_path)
 
-        if version in OLD_TL_VERSION_NAMES.keys():
-            version = OLD_TL_VERSION_NAMES[version]
-        version = Version(version)
-        if (version not in KNOWN_TL_VERSIONS):
+        version = Version(OLD_TL_VERSION_NAMES.get(version, version))
+        if version not in KNOWN_TL_VERSIONS:
             raise UnknownTensErLEEDVersionError(version)
         self._version = version
 
@@ -235,10 +233,9 @@ def validate_checksum(tl_version, filename):
     # Ensure TL version is valid
     if not isinstance(tl_version, (Version, str)):
         raise TypeError('Invalid type for tl_version')
-    version = Version(tl_version)
+    version_str = str(Version(tl_version))
     # convert old version names if necessary
-    if str(version) in OLD_TL_VERSION_NAMES.keys():
-        version = Version(OLD_TL_VERSION_NAMES[str(version)])
+    version = Version(OLD_TL_VERSION_NAMES.get(version_str, version_str))
 
     if str(version) not in KNOWN_TL_VERSIONS:
         raise UnknownTensErLEEDVersionError(version)
@@ -444,16 +441,16 @@ def _add_checksums_for_dir(source,
     -------
     None.
     """
-    if str(source.version) not in checksum_dict_.keys():
-        checksum_dict_[str(source.version)] = {}
-    version = source.version
+    version = str(source.version)
+    if version not in checksum_dict_:
+        checksum_dict_[version] = {}
     for pattern in patterns:
         for file in source.path.glob(pattern):
             checksum = get_file_checksum(file)
             key = str(file.relative_to(source.path).as_posix())
             if key not in checksum_dict_:
-                checksum_dict_[str(source.version)][key] = set()
-            checksum_dict_[str(source.version)][key].add(checksum)
+                checksum_dict_[version][key] = set()
+            checksum_dict_[version][key].add(checksum)
 
 
 def _parse_args(parser):
@@ -583,9 +580,9 @@ if __name__ != '__main__':
 
     # Generate set of all files
     TL_INPUT_FILES = set()
-    for version, checksums in VALID_CHECKSUMS.items():
-        for file_, f_checksums in checksums.items():
-            TL_INPUT_FILES.add(TLSourceFile(file_, version, f_checksums))
+    for known_version, known_checksums in VALID_CHECKSUMS.items():
+        for file_, f_checksums in known_checksums.items():
+            TL_INPUT_FILES.add(TLSourceFile(file_, known_version, f_checksums))
 
 
 else:  # Write new checksum file when executed as a module

commit 31799879f00d26b9def7aae55c4fff6490c97e6e

fix: wrong logger in run
Would suppress all logging messages emitted during section_loop, as the "viperleed.calc.run" logger was the one to which handlers would be assigned rather than the "viperleed.calc" one.

Bug was introduced in af8695daea148f3ef8320b75b0b09d572089de1d

src/viperleed/calc/run.py
@@ -29,6 +29,7 @@ from viperleed.calc.files.manifest import ManifestFile
 from viperleed.calc.files.tenserleed import get_tensorleed_path
 from viperleed.calc.lib.log_utils import close_all_handlers
 from viperleed.calc.lib.log_utils import prepare_calc_logger
+from viperleed.calc.lib.string_utils import parent_name
 from viperleed.calc.lib.time_utils import DateTimeFormat
 from viperleed.calc.sections.cleanup import cleanup
 from viperleed.calc.sections.cleanup import prerun_clean
@@ -38,8 +39,11 @@ from viperleed.calc.sections.initialization import (
     )
 from viperleed.calc.sections.run_sections import section_loop
 
-
-LOGGER = logging.getLogger(__name__)
+# It is VERY IMPORTANT to ensure that the LOGGER of this module is the
+# one of calc, not the one for the "run.py" module. Otherwise, all log
+# messages produced during running (which rely on the parent-child
+# relationship between loggers) would NOT be emitted.
+LOGGER = logging.getLogger(parent_name(__name__))
 
 
 def run_calc(

tests/calc/test_run.py
@@ -14,8 +14,10 @@ import pytest
 from pytest_cases import fixture
 from pytest_cases import parametrize
 
+from viperleed.calc import LOGGER as calc_logger
 from viperleed.calc.files import parameters
 from viperleed.calc.classes.rparams.rparams import Rparams
+from viperleed.calc.run import LOGGER as run_logger
 from viperleed.calc.run import _finalize_on_early_exit
 from viperleed.calc.run import _make_rpars_and_slab
 from viperleed.calc.run import _interpret_parameters
@@ -74,6 +76,10 @@ class TestRunCalc:
             'shutdown': mocker.patch(f'{_MODULE}.logging.shutdown'),
             }
         return mocks
+
+    def test_logger_is_from_main_calc(self):
+        """Ensure that the LOGGER in the run module is the one of calc."""
+        assert run_logger is calc_logger
 
     _user_args = (
         {'system_name': 'some name'},