fe464dc0540c88e69f7ee4dd74eb9bba52b076e1
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-04-11 18:07:05 +0200

    feat: add new error classes

--- src/viperleed/calc/files/displacements/errors.py
+++ src/viperleed/calc/files/displacements/errors.py
@@ -4,6 +4,27 @@ __authors__ = ('Alexander M. Imre (@amimre)',)
 __created__ = '2024-10-04'
 
 
+class DisplacementsError(ValueError):
+    """Base class for all displacement-related errors."""
+
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+class InvalidDisplacementsFileError(DisplacementsError):
+    """Base class for all errors related to the DISPLACEMENTS file."""
+
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+class IncompatibleBackendError(DisplacementsError):
+    """Error raised when the chosen backend is incompatible with the file."""
+
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
 class InvalidDisplacementsSyntaxError(ValueError):
     pass
 
