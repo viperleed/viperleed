0c2f2f5b8e2acb914587f90a4ba9761ca5d810b1
Michele Riva <riva@iap.tuwien.ac.at>
2025-04-14 07:22:49 +0200

    feat: find real domain folders
    Implementation is as discussed in #325. Currently, --fix mode cannot handle the corner cases of missing/renamed main folder and domain subfolders. These will be tackled in separate PRs.

--- src/viperleed/calc/bookkeeper/domain_finder.py
+++ src/viperleed/calc/bookkeeper/domain_finder.py
@@ -8,19 +8,57 @@ __created__ = '2025-04-10'
 __license__ = 'GPLv3+'
 
 import logging
+from pathlib import Path
 
 from viperleed.calc.bookkeeper.constants import CALC_LOG_PREFIXES
 from viperleed.calc.bookkeeper.history.constants import HISTORY_INFO_NAME
+from viperleed.calc.bookkeeper.history.explorer import HistoryExplorer
+from viperleed.calc.bookkeeper.history.errors import MetadataMismatchError
+from viperleed.calc.bookkeeper.utils import ask_user_confirmation
 from viperleed.calc.bookkeeper.utils import make_property
+from viperleed.calc.bookkeeper.utils import needs_update_for_attr
 from viperleed.calc.constants import DEFAULT_DOMAIN_FOLDER_PREFIX
 from viperleed.calc.constants import DEFAULT_HISTORY
 from viperleed.calc.constants import DEFAULT_OUT
 from viperleed.calc.constants import DEFAULT_SUPP
 from viperleed.calc.constants import DEFAULT_WORK_HISTORY
+from viperleed.calc.lib.log_utils import logging_silent
 from viperleed.calc.lib.string_utils import harvard_commas
 
 
 LOGGER = logging.getLogger(__name__)
+
+# The following are keys in the 'domains' section of the
+# metadata files that signal whether a folder belongs to
+# the history of the main folder in a domains calculation
+# (_MAIN_DOMAIN_KEY) or to that of a subdomain thereof
+# (_SUBDOMAIN_KEY). If neither key exists, the folder
+# belongs to a "normal" single-domain calc run.
+# See history.meta.BookkeeperMetaFile.domains for the
+# definition of the keys.
+_MAIN_DOMAIN_KEY = 'domains'
+_SUBDOMAIN_KEY = 'main'
+
+
+class DomainFinderError(Exception):
+    """Base class for errors in DomainFinder."""
+
+
+class FolderNotFoundError(DomainFinderError):
+    """Some expected folder was not found."""
+
+    def __init__(self, path):
+        """Initialize instance from a path."""
+        self.path = path
+        super().__init__(f'Could not find folder at {path}.')
+
+
+class DomainPathNotFoundError(FolderNotFoundError):
+    """The path of a domain subfolder was not found from the main."""
+
+
+class MainPathNotFoundError(FolderNotFoundError):
+    """The main path of the calculation was not found from a subdomain."""
 
 
 class DomainFinder:
@@ -29,8 +67,29 @@ class DomainFinder:
     def __init__(self, bookkeeper):
         """Initialize an instance from a `bookkeeper`."""
         self._bookkeeper = bookkeeper
+        self._domain_info = None
 
     path = make_property('_bookkeeper.cwd')
+
+    @property
+    def is_subdomain(self):
+        """Return whether self.path is a subdomain of a main one."""
+        return _SUBDOMAIN_KEY in self._domain_info
+
+    @property
+    def domain_info(self):
+        """Return domain information found in self.path."""
+        key = _SUBDOMAIN_KEY if self.is_subdomain else _MAIN_DOMAIN_KEY
+        return self._domain_info.get(key)
+
+    def collect_info(self):
+        """Collect domain information from self.path."""
+        self._bookkeeper.update_from_cwd(silent=True)
+        last_folder = self._bookkeeper.history.last_folder
+        self._domain_info = (
+            {} if last_folder is None
+            else last_folder.domain_info
+            )
 
     def find_potential_domains(self):
         """Return paths to subfolders of self.path that may be domains."""
@@ -45,6 +104,128 @@ class DomainFinder:
                       and d.name not in not_a_domain)
         return tuple(d for d in subfolders
                      if self._is_potential_domain_subfolder(d))
+
+    @needs_update_for_attr('_domain_info', updater='collect_info')
+    def find_domains(self, mode):
+        """Return domains found in self.path.
+
+        Domain information is collected from the metadata files
+        found in the 'history' directory of self.path.
+
+        Parameters
+        ----------
+        mode : BookkeeperMode
+            The mode in which self.bookkeeper is currently executed.
+            Only used for error reporting.
+
+        Returns
+        -------
+        domain_paths : tuple
+            Paths to all subdomains of self.path.
+
+        Raises
+        ------
+        MetadataError
+            If any metadata file contains corrupted domain information.
+        MainPathNotFoundError
+            If we're running in the main directory of a domains
+            calculation, but this path is inconsistent with the
+            one stored in any of the domain subfolders. This
+            exception is not raised if users explicitly confirm
+            their will to proceed anyway.
+        """
+        domain_info = self.domain_info
+        if not domain_info:
+            return tuple()
+        if self.is_subdomain:
+            main_path, _ = domain_info
+            self._warn_running_in_subdomain(mode, main_path)
+            return tuple()
+        return self._find_domains_from_main()
+
+    @staticmethod
+    def _ask_user_confirmation_on_main_path_mismatch(already_confirmed):
+        """Main path is mismatched. Ask confirmation to proceed."""
+        return already_confirmed or ask_user_confirmation()
+
+    def _check_subdomain_info_consistent(self, domain_path, domain_hash):
+        """Raise if domain information is not up to date."""
+        domain_path = Path(domain_path)
+        try:
+            folder = self._get_history_folder(domain_path, domain_hash)
+        except FileNotFoundError:
+            LOGGER.warning(f'Could not find expected domain '
+                           f'folder {domain_path}.')
+            raise DomainPathNotFoundError(domain_path) from None
+        if not folder:  # No history folder with that hash
+            err_msg = ('Could not find history folder with hash '
+                       f'{domain_hash} in domain {domain_path}.')
+            LOGGER.warning(err_msg)
+            raise MetadataMismatchError(err_msg)
+        main_path, _ = folder.metadata.domains[_SUBDOMAIN_KEY]
+        if Path(main_path) != self.path:
+            LOGGER.error(f'Domain {domain_path}: Inconsistent path to the '
+                         f'main folder of the domain calculation. Expected '
+                         f'{main_path}, found {self.path} instead.')
+            raise MainPathNotFoundError(main_path)
+
+    def _find_domains_from_main(self):
+        """Return paths to domain subfolders of the current directory."""
+        domain_paths = []
+        user_confirmed = False
+        for domain in self.domain_info:
+            try:
+                self._check_subdomain_info_consistent(*domain)
+            except DomainPathNotFoundError:
+                # TODO: once --fix can take care of renaming of the
+                # domain subfolders, we can suggest running it here.
+                continue
+            except MainPathNotFoundError:
+                # We have found the domain subfolder (albeit in the
+                # wrong path).
+                # TODO: once --fix can take care of renaming of the
+                # main folder, we can suggest running it here and
+                # raise again. For now make sure users are sure.
+                user_confirmed = (
+                    self._ask_user_confirmation_on_main_path_mismatch(
+                        user_confirmed,
+                        )
+                    )
+                if not user_confirmed:
+                    raise
+            except MetadataMismatchError:
+                # Users must have edited some files in the history
+                # folder. We do not allow updating the hashes in the
+                # metadata files, not even via --fix. We have found
+                # the domain subfolder, though, so we can still
+                # return it.
+                pass
+            domain_paths.append(Path(domain.path))
+        return tuple(domain_paths)
+
+    def _find_domains_from_subdomain(self):
+        """Return relative domain paths when self.path is a subdomain."""
+        main_info = self.domain_info
+        try:
+            main_folder = self._get_history_folder(*main_info)
+        except FileNotFoundError:  # Probably main path renamed
+            raise MainPathNotFoundError(main_info.path) from None
+        if not main_folder:
+            return tuple()
+        domain_info = main_folder.metadata.domains.get(_MAIN_DOMAIN_KEY, ())
+        return tuple(Path(d.path) for d in domain_info)
+
+    @staticmethod
+    def _get_history_folder(root_path, folder_hash):
+        """Return the `root_path`/'history' folder with `folder_hash`."""
+        root_path = Path(root_path)
+        if not root_path.is_dir():
+            raise FileNotFoundError
+        history = HistoryExplorer(root_path)
+        with logging_silent():
+            history.collect_subfolders()
+        return history.subfolder_from_hash(folder_hash)
+
     def _is_potential_domain_subfolder(self, path):
         """Return whether `path` points to a (likely) domain subfolder."""
         if path.name.startswith(DEFAULT_DOMAIN_FOLDER_PREFIX):
@@ -62,3 +243,25 @@ class DomainFinder:
             HISTORY_INFO_NAME,
             )
         return any(f for pattern in contents for f in path.glob(pattern))
+
+    def _warn_running_in_subdomain(self, mode, main_path):
+        """Emit warnings when calling find_domains in a subdomain."""
+        log_msg = (
+            f'Running bookkeeper in mode {mode.name} in a domain subfolder. '
+            )
+        try:
+            domains = self._find_domains_from_subdomain()
+        except MainPathNotFoundError:
+            # TODO: add here a suggestion to run in --fix mode in
+            # the parent path once we can fix renaming of main
+            log_msg += ('The path to the corresponding main directory could '
+                        f'not be identified. It should have been {main_path}, '
+                        f'but the parent directory is now {self.path.parent}.')
+            LOGGER.error(log_msg)
+            return
+
+        siblings = (d for d in domains if d.name != self.path.name)
+        LOGGER.warning(f'{log_msg} Make sure to also execute \'bookkeeper '
+                       f'{mode.long_flag}\' in the main path ({main_path}) '
+                       'or in other domain subfolders (%s).',
+                       harvard_commas(*siblings))
--- tests/calc/bookkeeper/test_domain_finder.py
+++ tests/calc/bookkeeper/test_domain_finder.py
@@ -7,7 +7,16 @@ __copyright__ = 'Copyright (c) 2019-2025 ViPErLEED developers'
 __created__ = '2025-04-10'
 __license__ = 'GPLv3+'
 
+from pathlib import Path
+import re
+
+import pytest
+from pytest_cases import fixture
+from pytest_cases import parametrize
+
 from viperleed.calc.bookkeeper.domain_finder import DomainFinder
+from viperleed.calc.bookkeeper.domain_finder import MainPathNotFoundError
+from viperleed.calc.bookkeeper.history.meta import DomainInfo
 from viperleed.calc.constants import DEFAULT_DELTAS
 from viperleed.calc.constants import DEFAULT_OUT
 from viperleed.calc.constants import DEFAULT_SUPP
@@ -17,10 +26,87 @@ from viperleed.calc.constants import LOG_PREFIX
 from ...helpers import filesystem_from_dict
 
 
+_MODULE = 'viperleed.calc.bookkeeper.domain_finder'
+
+
+@fixture(name='bookkeeper')
+def fixture_bookkeeper(mocker):
+    """Return a fake bookkeeper."""
+    return mocker.MagicMock()
+
+
+@fixture(name='finder')
+def fixture_finder(bookkeeper):
+    """Return a DomainFinder with a fake bookkeeper."""
+    finder = DomainFinder(bookkeeper)
+    finder.bookkeeper = bookkeeper
+    return finder
+
+
+@fixture(name='mock_finder_info')
+def fixture_mock_finder_info(finder):
+    """Set _domain_info to the given dict."""
+    def _mock(info):
+        # pylint: disable-next=protected-access           # OK in tests
+        finder._domain_info = info
+        return finder
+    return _mock
+
+
 class TestDomainFinder:
     """Tests for the DomainFinder class."""
 
-    def test_find_potential_domains(self, tmp_path, caplog, mocker):
+    def test_collect_info_last_folder(self, finder, mocker):
+        """Check the collection of domain information with a history folder."""
+        mock_info = {'main': mocker.MagicMock()}
+        bookkeeper = finder.bookkeeper
+        bookkeeper.history.last_folder = mocker.MagicMock(
+            domain_info=mock_info,
+            )
+        finder.collect_info()
+        bookkeeper.update_from_cwd.assert_called_once_with(silent=True)
+        assert finder.domain_info is mock_info['main']
+
+    def test_collect_info_no_history(self, finder):
+        """Check the collection of domain information without history."""
+        bookkeeper = finder.bookkeeper
+        bookkeeper.history.last_folder = None
+        finder.collect_info()
+        bookkeeper.update_from_cwd.assert_called_once_with(silent=True)
+        assert finder.domain_info is None
+
+    _find_domains = {  # _domain_info, called, result
+        'not a domain calc': ({}, (), ()),
+        'subdomain': ({'main': ('main_path', 'main_hash')},
+                      ('_warn_running_in_subdomain',),
+                      ()),
+        }
+
+    @parametrize('info,called,expect',
+                 _find_domains.values(),
+                 ids=_find_domains)
+    # pylint: disable-next=too-many-arguments  # 2/6 are fixtures
+    def test_find_domains(self, info, called, expect,
+                          mock_finder_info, mocker):
+        """Check the subcalls in find_domains."""
+        finder = mock_finder_info(info)
+        mocks = [mocker.patch.object(finder, attr) for attr in called]
+        result = finder.find_domains(mocker.MagicMock())
+        assert result == expect
+        for mock in mocks:
+            mock.assert_called_once()
+
+    def test_find_domains_calls_from_main(self, mock_finder_info, mocker):
+        """Check the subcalls in find_domains when called in a domain main."""
+        finder = mock_finder_info(
+            {'domains': (('domain_path', 'domain_hash'),)}
+            )
+        mock_from_main = mocker.patch.object(finder, '_find_domains_from_main')
+        result = finder.find_domains(mocker.MagicMock())
+        assert result is mock_from_main.return_value
+        mock_from_main.assert_called_once()
+
+    def test_find_potential_domains(self, finder, tmp_path, caplog):
         """Test the find_potential_domains method."""
         caplog.set_level(0)  # All messages
         not_domains = {
@@ -59,9 +145,257 @@ class TestDomainFinder:
                 },
             }
         filesystem_from_dict({**not_domains, **domains}, tmp_path)
-        mock_bookkeeper = mocker.MagicMock(cwd=tmp_path)
-        finder = DomainFinder(mock_bookkeeper)
+        finder.bookkeeper.cwd = tmp_path
         domains_found = finder.find_potential_domains()
         assert not caplog.text
         assert len(domains_found) == len(domains)
         assert {d.name for d in domains_found} == set(domains)
+
+    _is_subdomain = {
+        'not a domain calc': ({}, False),
+        'main domain folder': (
+            {'domains': (('sub_path_1', 'sub_hash_1'),
+                         ('sub_path_2', 'sub_hash_2'))},
+            False,
+            ),
+        'subdomain folder': ({'main': ('path', 'hash')}, True),
+        }
+
+    @parametrize('info,expect', _is_subdomain.values(), ids=_is_subdomain)
+    def test_is_subdomain(self, info, expect, mock_finder_info):
+        """Check the expected value of the is_subdomain property."""
+        finder = mock_finder_info(info)
+        assert finder.is_subdomain == expect
+
+
+class TestDomainFinderFromMain:
+    """Tests for the _find_domains_from_main method."""
+
+    @fixture(name='find')
+    def fixture_find(self, mock_finder_info, existing_domains):
+        """Return the domains found by `finder`."""
+        def _find(info=None):
+            if info is None:
+                info = {'domains': existing_domains}
+            finder = mock_finder_info(info)
+            # pylint: disable-next=protected-access       # OK in tests
+            return finder._find_domains_from_main()
+        return _find
+
+    @fixture(name='existing_domains')
+    def fixture_existing_domains(self):
+        """Return information of some existing domains."""
+        return (
+            DomainInfo('existing_domain_path_one', 'existing_hash_one'),
+            DomainInfo('existing_domain_path_two', 'existing_hash_two'),
+            )
+
+    def test_domain_folder_not_found(self, find, caplog):
+        """Check the expected result when a domain subfolder is not found."""
+        domains = find({'domains': (
+            ('non_exiting_path', 'hash_of_non_existing'),
+            )})
+        assert not domains
+        assert caplog.text
+
+    def test_main_path_mismatched(self, finder, find, caplog, mocker):
+        """Check result the main path stored in history is mismatched."""
+        def _mock_get_history_folder(*_):
+            """Return a fake history folder."""
+            folder = mocker.MagicMock()
+            folder.metadata.domains = {'main': ('not-the-same-main-path',
+                                                'main_hash')}
+            return folder
+
+        mocker.patch.object(finder,
+                            '_get_history_folder',
+                            _mock_get_history_folder)
+        mock_ask_user = mocker.patch(f'{_MODULE}.ask_user_confirmation',
+                                     return_value=True)
+        domains = find()
+        # Path to domain folder exists, even if the history folder
+        # is mismatched. This problem will bubble up at a later
+        # point, if it is critical to have a specific folder.
+        assert domains
+        assert caplog.text
+        # Ask user only once, even if there's 2 mismatched folders!
+        mock_ask_user.assert_called_once()
+
+    def test_main_path_mismatched_user_says_no(self, finder, find, mocker):
+        """Check behavior when users don't want to proceed."""
+        check_info = mocker.patch.object(
+            finder,
+            '_check_subdomain_info_consistent',
+            side_effect=MainPathNotFoundError(None),
+            )
+        mocker.patch(f'{_MODULE}.ask_user_confirmation', return_value=False)
+        with pytest.raises(MainPathNotFoundError):
+            find()
+        assert check_info.call_count == 1  # Even if there's 2 folders
+
+    def test_no_domain_history_folder(self, finder, find, caplog, mocker):
+        """Check the expected result when the history folder is not found."""
+        mock_get_history = mocker.patch.object(finder,
+                                               '_get_history_folder',
+                                               return_value=None)
+        domains = find({'domains': (
+            DomainInfo('existing_domain_path',
+                       'hash_of_non_existing_history_folder'),
+            )})
+        mock_get_history.assert_called_once_with(
+            Path('existing_domain_path'),
+            'hash_of_non_existing_history_folder',
+            )
+        # Path to domain folder exists, even if the history folder
+        # is mismatched. This problem will bubble up at a later
+        # point, if it is critical to have a specific folder.
+        assert domains
+        assert caplog.text
+
+    # pylint: disable-next=too-many-arguments    # All fixtures
+    def test_success(self, existing_domains, finder, find,
+                     tmp_path, caplog, mocker):
+        """Test the successful detection of subdomains."""
+        def _mock_get_history_folder(*_):
+            """Return a fake history folder."""
+            folder = mocker.MagicMock()
+            folder.metadata.domains = {'main': (str(tmp_path), 'main_hash')}
+            return folder
+
+        mocker.patch.object(finder,
+                            '_get_history_folder',
+                            _mock_get_history_folder)
+        finder.bookkeeper.cwd = tmp_path
+        domains = find()
+        assert not caplog.text
+        assert domains
+        assert all(isinstance(d, Path) for d in domains)
+        assert all(d.name == i.path for d, i in zip(domains, existing_domains))
+
+
+class TestDomainFinderFromSubdomain:
+    """Tests for the _find_domains_from_subdomain method."""
+
+    @fixture(name='find')
+    def fixture_find(self, mock_finder_info):
+        """Call _find_domains_from_subdomain."""
+        def _find(info):
+            finder = mock_finder_info(info)
+            # pylint: disable-next=protected-access       # OK in tests
+            return finder._find_domains_from_subdomain()
+        return _find
+
+    def test_main_path_not_found(self, finder, find, mocker):
+        """Check complaints when the main path does not exist."""
+        main_path = 'some_path'
+        mocker.patch.object(finder,
+                            '_get_history_folder',
+                            side_effect=FileNotFoundError)
+        with pytest.raises(MainPathNotFoundError, match=main_path):
+            find({'main': DomainInfo(main_path, 'main_hash')})
+
+    def test_no_history_in_main_path(self, finder, find, mocker):
+        """Check complaints when the main path does not exist."""
+        main_path = 'some_path'
+        mocker.patch.object(finder,
+                            '_get_history_folder',
+                            return_value=None)
+        assert not find({'main': DomainInfo(main_path, 'main_hash')})
+
+    def test_success(self, finder, find, mocker):
+        """Check complaints when the main path does not exist."""
+        main_path = 'some_path'
+        mock_folder = mocker.MagicMock()
+        all_domains = (
+            DomainInfo('domain_1', 'hash_1'),
+            DomainInfo('domain_2', 'hash_2'),
+            DomainInfo('this_domain', 'hash_this'),
+            DomainInfo('domain_4', 'hash_4'),
+            )
+        mock_folder.metadata.domains = {'domains': all_domains}
+        mocker.patch.object(finder,
+                            '_get_history_folder',
+                            return_value=mock_folder)
+        domains = find({'main': DomainInfo(main_path, 'main_hash')})
+        assert domains
+        assert len(domains) == len(all_domains)
+        assert all(isinstance(d, Path) for d in domains)
+        assert (d.name == i.path for d, i in zip(domains, all_domains))
+
+
+class TestDomainFinderGetHistoryFolder:
+    """Tests for the _get_history_folder method."""
+
+    @fixture(name='mock_explorer')
+    def fixture_patch_explorer(self, mocker):
+        """Replace HistoryExplorer with a mock."""
+        explorer_instance = mocker.MagicMock()
+        explorer_cls = mocker.patch(f'{_MODULE}.HistoryExplorer',
+                                    return_value=explorer_instance)
+        return explorer_cls, explorer_instance
+
+    @fixture(name='get')
+    def fixture_get(self, finder):
+        """Call _get_history_folder."""
+        def _call(*args):
+            # pylint: disable-next=protected-access       # OK in tests
+            return finder._get_history_folder(*args)
+        return _call
+
+    def test_root_not_found(self, get, mocker):
+        """Check complaints when the root path does not exist."""
+        with pytest.raises(FileNotFoundError):
+            get('not_a_root_folder', mocker.MagicMock())
+
+    def test_success(self, get, mock_explorer, mocker):
+        """Check the successful fetching of a history folder."""
+        explorer_cls, explorer = mock_explorer
+        mock_root = mocker.MagicMock(spec=Path)
+        mock_hash = mocker.MagicMock()
+        result = get(mock_root, mock_hash)
+        assert result is explorer.subfolder_from_hash.return_value
+        explorer_cls.assert_called_once_with(Path(mock_root))
+        explorer.collect_subfolders.assert_called_once_with()
+        explorer.subfolder_from_hash.assert_called_once_with(mock_hash)
+
+
+class TestDomainFinderWarnSubdomain:
+    """Tests for the _warn_running_in_subdomain helper method."""
+
+    @fixture(name='warn')
+    def fixture_warn(self, finder, mocker):
+        """Call _warn_running_in_subdomain."""
+        def _call(main_path='some_path'):
+            # pylint: disable-next=protected-access       # OK in tests
+            finder._warn_running_in_subdomain(mocker.MagicMock(), main_path)
+        return _call
+
+    # pylint: disable-next=too-many-arguments  # All fixtures
+    def test_main_path_exists(self, finder, warn, tmp_path, caplog, mocker):
+        """Check complaints when the main path is mismatched."""
+        all_domains = Path('this_domain'), Path('domain_1'), Path('domain_2')
+        mocker.patch.object(finder,
+                            '_find_domains_from_subdomain',
+                            return_value=all_domains)
+        finder.bookkeeper.cwd = tmp_path/all_domains[0]
+        warn()
+        expect_log = (
+            re.compile('Running bookkeeper in mode.* in a domain subfolder'),
+            re.compile(r'also execute \'bookkeeper.*\' in the main path'),
+            re.compile(r'other domain subfolders \(domain_1 and domain_2\)'),
+            )
+        log = caplog.text
+        assert all(expect.search(log) for expect in expect_log)
+
+    def test_main_path_not_found(self, finder, warn, caplog, mocker):
+        """Check complaints when the main path is mismatched."""
+        mocker.patch.object(finder,
+                            '_find_domains_from_subdomain',
+                            side_effect=MainPathNotFoundError(None))
+        warn()
+        expect_log = (
+            re.compile('Running bookkeeper in mode.* in a domain subfolder'),
+            re.compile('path to the .*main directory could not be identified'),
+            )
+        log = caplog.text
+        assert all(expect.search(log) for expect in expect_log)
