dcd8df71182f37eb0aec7cbd748b3490b3cfde6b
Michele Riva <riva@iap.tuwien.ac.at>
2025-05-02 10:03:10 +0200

    feat,tests: DomainBookkeeper full coverage

--- tests/calc/bookkeeper/bookkeeper/test_domain_bookkeeper.py
+++ tests/calc/bookkeeper/bookkeeper/test_domain_bookkeeper.py
@@ -9,31 +9,48 @@ __license__ = 'GPLv3+'
 
 from pathlib import Path
 
+from pytest_cases import fixture
+
 from viperleed.calc.bookkeeper.bookkeeper import DomainBookkeeper
 
 from .conftest import _MODULE
 
 
-class TestDomainBookkeeper:
-    """Tests for the DomainBookkeeper class."""
-
-    def test_clean_state(self, mocker):
-        """Test the _clean_state private method."""
-        mock_path = mocker.MagicMock(spec=Path)
-        mock_domain_root_instance = mocker.MagicMock(path=mock_path)
+@fixture(name='make_domain_bookie')
+def fixture_domain_bookie(mocker):
+    """Return a DomainBookkeeper with a fake path and main root."""
+    def _make():
+        mock_cwd = mocker.MagicMock(spec=Path)
+        mock_domain_root_instance = mocker.MagicMock(path=mock_cwd)
         mock_domain_root_cls = mocker.patch(
             f'{_MODULE}.DomainRootExplorer',
             return_value=mock_domain_root_instance,
             )
         mock_main_root = mocker.MagicMock()
-        domain_bookie = DomainBookkeeper(mock_main_root,
-                                         cwd=mock_path)
+        bookie = DomainBookkeeper(mock_main_root, cwd=mock_cwd)
+        return bookie, mock_main_root, mock_domain_root_cls
+    return _make
+
+
+class TestDomainBookkeeper:
+    """Tests for the DomainBookkeeper class."""
+
+    def test_check_may_discard_full(self, make_domain_bookie, mocker):
+        """Check private calls in check_may_discard_full."""
+        domain_bookie, *_ = make_domain_bookie()
+        private = mocker.patch.object(domain_bookie, '_check_may_discard_full')
+        domain_bookie.check_may_discard_full()
+        private.assert_called_once_with()
+
+    def test_clean_state(self, make_domain_bookie, mocker):
+        """Test the _clean_state private method."""
+        domain_bookie, main_root, mock_domain_root_cls = make_domain_bookie()
         # pylint: disable=protected-access                # OK in tests
         domain_bookie._state_info['logger_prepared'] = log = mocker.MagicMock()
         domain_bookie._clean_state()
 
         # Check unchanged attributes
-        assert domain_bookie._main_root is mock_main_root
+        assert domain_bookie._main_root is main_root
         assert domain_bookie._state_info['logger_prepared'] is log
         # DomainRootExplorer should have been called twice, once
         # at creation of the domain_bookie instance above, once
@@ -41,26 +58,18 @@ class TestDomainBookkeeper:
         n_domain_root_calls = 2
         assert mock_domain_root_cls.call_count == n_domain_root_calls
 
-    def test_init(self, mocker):
+    def test_init(self, make_domain_bookie, mocker):
         """Test initialization."""
-        mock_domain_root_instance = mocker.MagicMock()
-        mock_domain_root_cls = mocker.patch(
-            f'{_MODULE}.DomainRootExplorer',
-            return_value=mock_domain_root_instance,
-            )
-        mock_main_root = mocker.MagicMock()
         mock_super_root_instance = mocker.MagicMock()
         mock_super_root_cls = mocker.patch(
             f'{_MODULE}.RootExplorer',
             return_value=mock_super_root_instance,
             )
-        mock_path = mocker.MagicMock(spec=Path)
-        domain_bookie = DomainBookkeeper(mock_main_root,
-                                         cwd=mock_path)
+        domain_bookie, main_root, mock_domain_root_cls = make_domain_bookie()
 
         # Check delegation to super().__init__
         mock_super_root_cls.assert_called_once_with(
-            path=mock_path,
+            path=domain_bookie.cwd,
             bookkeeper=domain_bookie,
             )
         # As well as replacement of the _root attribute with
@@ -68,11 +77,12 @@ class TestDomainBookkeeper:
         mock_domain_root_cls.assert_called_once_with(
             mock_super_root_instance.path,
             domain_bookie,
-            mock_main_root,
+            main_root,
             )
+        mock_domain_root_instance = mock_domain_root_cls()
         # pylint: disable=protected-access                # OK in tests
         assert domain_bookie._root is mock_domain_root_instance
-        assert domain_bookie._main_root is mock_main_root
+        assert domain_bookie._main_root is main_root
 
     def test_run_in_subdomain(self, tmp_path, mocker):
         """Check delegation to private methods of run_in_subdomain."""
