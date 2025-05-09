dfb3a05ace386e4981c03c95848177ed50663e50
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-05-09 11:10:27 +0200

    test, feat: more tests for range cases

--- tests/calc/files/displacements/test_range.py
+++ tests/calc/files/displacements/test_range.py
@@ -9,19 +9,40 @@ import pytest
 from viperleed_jax.files.displacements.range import DisplacementsRange
 
 
-def test_init_from_string_minimum():
-    dr = DisplacementsRange('1 2')
-    assert dr.start == 1.0
-    assert dr.stop == 2.0
+@pytest.mark.parametrize(
+    'input_str, exp_start, exp_stop',
+    [
+        ('-0.1 0.1', -0.1, 0.1),
+        ('1.0 2.0', 1.0, 2.0),
+        ('-3 4', -3.0, 4.0),
+        ('4 -3', 4.0, -3.0),
+        ('1e0 2E+0', 1.0, 2.0),
+        ('  5\t6  ', 5.0, 6.0),
+    ],
+)
+def test_init_from_string_without_step(input_str, exp_start, exp_stop):
+    dr = DisplacementsRange(input_str)
+    assert dr.start == exp_start
+    assert dr.stop == exp_stop
     assert dr.step is None
     assert dr.has_step is False
 
 
-def test_init_from_string_with_step():
-    dr = DisplacementsRange('-1.5 3.0 0.5')
-    assert dr.start == -1.5
-    assert dr.stop == 3.0
-    assert dr.step == 0.5
+@pytest.mark.parametrize(
+    'input_str, exp_start, exp_stop, exp_step',
+    [
+        ('-1.5 3.0 0.5', -1.5, 3.0, 0.5),
+        ('2 4 1', 2.0, 4.0, 1.0),
+        ('0.1 1e2 1e-1', 0.1, 100.0, 0.1),
+        ('-2E1 3e1 0.5', -20.0, 30.0, 0.5),
+        ('  7  8.0   0.25  ', 7.0, 8.0, 0.25),
+    ],
+)
+def test_init_from_string_with_step(input_str, exp_start, exp_stop, exp_step):
+    dr = DisplacementsRange(input_str)
+    assert dr.start == pytest.approx(exp_start)
+    assert dr.stop == pytest.approx(exp_stop)
+    assert dr.step == pytest.approx(exp_step)
     assert dr.has_step is True
 
 
