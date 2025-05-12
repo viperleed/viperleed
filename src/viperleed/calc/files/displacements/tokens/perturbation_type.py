dc34c2d68a92e45139681872f53a7fabb01bebad
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-05-12 14:19:11 +0200

    feat: use token base class for type

--- src/viperleed/calc/files/displacements/tokens/perturbation_type.py
+++ src/viperleed/calc/files/displacements/tokens/perturbation_type.py
@@ -6,6 +6,7 @@ __created__ = '2025-04-10'
 
 from enum import Enum
 
+from .base import DisplacementsFileToken, TokenParserError
 
 class PerturbationTypeError(ValueError):
     """Error class for unknown perturbation type errors."""
@@ -31,3 +32,26 @@ class PerturbationType(str, Enum):
             msg = (f'Unknown perturbation type tag: {s}. Tag must be one of '
                    '"geo", "vib", "occ", or "dom"."')
             raise PerturbationTypeError(msg) from None
+
+
+class TypeTokenParserError(TokenParserError):
+    """Class for errors during the TypeToken parsing."""
+
+class TypeToken(DisplacementsFileToken):
+    """Class for the TypeToken."""
+
+    def __init__(self, type_str):
+        self.type_str = type_str
+        try:
+            self.type = PerturbationType(type_str)
+        except PerturbationTypeError as err:
+            msg = f'Unable to parse perturbation type {self.type_str}.'
+            raise TypeTokenParserError(msg) from err
+
+    def __eq__(self, other):
+        """Compare self to other TypeToken."""
+        return self.type is other.type
+
+    def __repr__(self):
+        """Return representation of TypeToken."""
+        return f'TypeToken({self.type})'
