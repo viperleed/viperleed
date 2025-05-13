327fb613e74d000a8986d7699629311266fb2049
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-05-13 09:40:48 +0200

    feat: type token

---
+++ src/viperleed/calc/files/displacements/tokens/type.py
@@ -0,0 +1,36 @@
+"""Type token for DISPLACEMENTS file."""
+
+__authors__ = ('Alexander M. Imre (@amimre)',)
+__created__ = '2025-04-10'
+
+
+
+from viperleed_jax.files.displacements.perturbation_type import (
+    PerturbationType,
+    PerturbationTypeError,
+)
+
+from .base import DisplacementsFileToken, TokenParserError
+
+
+class TypeTokenParserError(TokenParserError):
+    """Class for errors during the TypeToken parsing."""
+
+class TypeToken(DisplacementsFileToken):
+    """Class for the TypeToken."""
+
+    def __init__(self, type_str):
+        self.type_str = type_str
+        try:
+            self.type = PerturbationType(type_str)
+        except PerturbationTypeError as err:
+            msg = f'Unable to parse perturbation type {self.type_str}.'
+            raise TypeTokenParserError(msg) from err
+
+    def __eq__(self, other):
+        """Compare self to other TypeToken."""
+        return self.type is other.type
+
+    def __repr__(self):
+        """Return representation of TypeToken."""
+        return f'TypeToken({self.type})'
