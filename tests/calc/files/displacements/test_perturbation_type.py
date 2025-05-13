2954a7c8baacd7bb854d5ca80c200704774578c6
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-05-13 09:55:43 +0200

    test,fix: testing of types

---
+++ tests/calc/files/displacements/test_perturbation_type.py
@@ -0,0 +1,51 @@
+"""Tests for module perturbation_type."""
+
+__authors__ = ('Alexander M. Imre (@amimre)',)
+__created__ = '2025-04-10'
+
+import pytest
+from viperleed_jax.files.displacements.perturbation_type import (
+    PerturbationType, PerturbationTypeError)
+
+
+@pytest.mark.parametrize(
+    'input_str, expected_enum',
+    [
+        ('geo', PerturbationType.GEO),
+        ('vib', PerturbationType.VIB),
+        ('occ', PerturbationType.OCC),
+    ],
+)
+def test_valid_enum_from_string(input_str, expected_enum):
+    """Test valid string-to-enum conversion."""
+    assert PerturbationType.from_string(input_str) is expected_enum
+
+
+@pytest.mark.parametrize(
+    'enum_member, expected_str',
+    [
+        (PerturbationType.GEO, 'geo'),
+        (PerturbationType.VIB, 'vib'),
+        (PerturbationType.OCC, 'occ'),
+    ],
+)
+def test_enum_str_values(enum_member, expected_str):
+    """Test that enum values behave like strings."""
+    assert str(enum_member) == expected_str
+    assert enum_member == expected_str  # thanks to str inheritance
+
+
+@pytest.mark.parametrize(
+    'invalid_str',
+    [
+        'geoo',
+        'VIB',
+        'oc',
+        '',
+        '123',
+    ],
+)
+def test_invalid_enum_from_string(invalid_str):
+    """Test that invalid strings raise ValueError."""
+    with pytest.raises(PerturbationTypeError, match='Unknown perturbation'):
+        PerturbationType.from_string(invalid_str)
--- tests/calc/files/displacements/tokens/test_type.py
+++ tests/calc/files/displacements/tokens/test_type.py
@@ -4,47 +4,35 @@ __authors__ = ('Alexander M. Imre (@amimre)',)
 __created__ = '2025-04-10'
 
 import pytest
+
+from viperleed_jax.files.displacements.tokens.type import TypeToken, TypeTokenParserError
 from viperleed_jax.files.displacements.perturbation_type import PerturbationType
 
 
 @pytest.mark.parametrize(
-    'input_str, expected_enum',
+    'type_str, expected_enum',
     [
         ('geo', PerturbationType.GEO),
         ('vib', PerturbationType.VIB),
         ('occ', PerturbationType.OCC),
+        ('dom', PerturbationType.DOM),
     ],
 )
-def test_valid_enum_from_string(input_str, expected_enum):
-    """Test valid string-to-enum conversion."""
-    assert PerturbationType.from_string(input_str) is expected_enum
+def test_valid_type_strings(type_str, expected_enum):
+    tok = TypeToken(type_str)
+    assert tok.type is expected_enum
+    # repr should mention the enum
+    rep = repr(tok)
+    assert 'TypeToken' in rep
+    assert expected_enum.name in rep or expected_enum.value in rep
 
 
-@pytest.mark.parametrize(
-    'enum_member, expected_str',
-    [
-        (PerturbationType.GEO, 'geo'),
-        (PerturbationType.VIB, 'vib'),
-        (PerturbationType.OCC, 'occ'),
-    ],
-)
-def test_enum_str_values(enum_member, expected_str):
-    """Test that enum values behave like strings."""
-    assert str(enum_member) == expected_str
-    assert enum_member == expected_str  # thanks to str inheritance
-
-
-@pytest.mark.parametrize(
-    'invalid_str',
-    [
-        'geoo',
-        'VIB',
-        'oc',
-        '',
-        '123',
-    ],
-)
-def test_invalid_enum_from_string(invalid_str):
-    """Test that invalid strings raise ValueError."""
-    with pytest.raises(ValueError, match='Unknown offset type'):
-        PerturbationType.from_string(invalid_str)
+def test_equality_and_type_mismatch():
+    a = TypeToken('geo')
+    b = TypeToken('geo')
+    c = TypeToken('vib')
+    assert a == b
+    assert a != c
+    # comparing to non-TypeToken always returns False
+    assert a != 'geo'
+    assert a != None
