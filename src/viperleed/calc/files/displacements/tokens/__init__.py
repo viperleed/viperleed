05994361e7c2668cb0aedd4b130c6febb20a9f1f
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-05-14 17:06:48 +0200

    fix: imports in token init

--- src/viperleed/calc/files/displacements/tokens/__init__.py
+++ src/viperleed/calc/files/displacements/tokens/__init__.py
@@ -5,8 +5,9 @@ __created__ = '2025-05-12'
 
 from .base import TokenParserError
 from .direction import DirectionToken
+from .element import ElementToken
+from .linear_operation import LinearOperationToken
 from .offset import OffsetToken
 from .range import RangeToken
 from .target import TargetToken
 from .type import TypeToken
-from .element import ElementToken
