12949921356ba52566511d9db7199bc905514a34
Michele Riva <riva@iap.tuwien.ac.at>
2025-05-21 08:31:00 +0200

    fix: execute GUI in non-windows
    Fixes loading the measurement package on non-Windows environments. Changes:
    
    - EnvironmentError has been an alias for OSError since Python 3.3. Use ImportError instead.
    - Add a note about using a better exception in the future.

--- guilib/measure/camera/drivers/imagingsource/__init__.py
+++ guilib/measure/camera/drivers/imagingsource/__init__.py
@@ -13,13 +13,19 @@ This module contains drivers for cameras produced by The Imaging Source.
 
 import sys
 
-if sys.platform.startswith('win'):
-    from viperleed.guilib.measure.camera.drivers.imagingsource.tisgrabber import (
-        WindowsCamera as ISCamera, FrameReadyCallbackType, SinkFormat
-        )
-    from viperleed.guilib.measure.camera.drivers.imagingsource.winerrors import (
-        ImagingSourceError
-        )
-else:
-    raise EnvironmentError("Imaging Source cameras are currently "
-                           "unsupported on non-Windows platforms.")
+if not sys.platform.startswith('win'):
+    # TODO: it would be better to have a custom exception for this,
+    # but we should first remove the various __init__ imports in
+    # guilib. When we do have a custom exception, we should also fix
+    # the except block in camera.__init__.
+    raise ImportError('Imaging Source cameras are currently '
+                      'unsupported on non-Windows platforms.')
+
+from viperleed.guilib.measure.camera.drivers.imagingsource.tisgrabber import (
+    FrameReadyCallbackType,
+    SinkFormat,
+    WindowsCamera as ISCamera,
+    )
+from viperleed.guilib.measure.camera.drivers.imagingsource.winerrors import (
+    ImagingSourceError,
+    )
