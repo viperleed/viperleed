a3dd624d4d212778b8933448fb65daa397d0079b
Michele Riva <riva@iap.tuwien.ac.at>
2025-05-26 04:51:54 +0200

    refacor: remove GLOBALS['version']
    In favor of __version__

--- src/viperleed/__init__.py
+++ src/viperleed/__init__.py
@@ -16,7 +16,6 @@ __version__ = '0.14.0'
 
 GLOBALS = {
     'USE_GUI': None,
-    'version': __version__,
     'version_message': ('ViPErLEED (Vienna Package for Erlangen LEED) '
                         f'v{__version__}')
     }
