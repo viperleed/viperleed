04e08a0d55e3d4264f60bf3cb95b4c377d28df5f
Michele Riva <riva@iap.tuwien.ac.at>
2025-05-29 15:13:10 +0200

    refactor,tests: remove unused code
    As well as alphabetic sorting of tests.

--- tests/calc/files/test_experiment_symmetry.py
+++ tests/calc/files/test_experiment_symmetry.py
@@ -34,22 +34,6 @@ _FLOAT_RE = r'-?\d+(.\d*)?'
 _MATRIX_RE = r'\[\[{0},\s*{0}\],\s*\[{0},\s*{0}\]\]'
 
 
-def mock_energies(rpars):
-    """Give an example THEO_ENERGIES to rpars."""
-    rpars.THEO_ENERGIES = TheoEnergies(10, 100, 2)
-
-
-def prepare_slab_and_rpars(slab, rpars, make_bulk=True):
-    """Prepare slab the same way as initialization would."""
-    mock_energies(rpars)
-    if slab.foundplanegroup == NO_GROUP:
-        symmetry.findSymmetry(slab, rpars, forceFindOri=True)
-    if make_bulk:
-        bulk = slab.make_bulk_slab(rpars)
-        symmetry.findBulkSymmetry(bulk, rpars)
-        symmetry.findSymmetry(bulk, rpars, bulk=True)
-
-
 @fixture(name='with_info')
 @parametrize_with_cases('args',
                         cases=CasePOSCARSlabs,
@@ -57,8 +41,33 @@ def prepare_slab_and_rpars(slab, rpars, make_bulk=True):
 def fixture_with_info(args):
     """Return a prepared slab and rpars."""
     slab, rpars, *_ = args
-    prepare_slab_and_rpars(slab, rpars)
+    # Add some energies to check that eMax is set correctly
+    rpars.THEO_ENERGIES = TheoEnergies(10, 100, 2)
+
+    # Find plane groups of slab and bulk to ensure symmetry
+    # info is written correctly.
+    if slab.foundplanegroup == NO_GROUP:
+        symmetry.findSymmetry(slab, rpars, forceFindOri=True)
+    bulk = slab.make_bulk_slab(rpars)
+    symmetry.findBulkSymmetry(bulk, rpars)
+    symmetry.findSymmetry(bulk, rpars, bulk=True)
     return args
+
+
+def test_get_dict_fails(mocker):
+    """Check complaints when fetching a dict of LEED parameters fails."""
+    mocker.patch(f'{_MODULE}.getLEEDdict', return_value=None)
+    args = (mocker.MagicMock() for _ in range(2))
+    with pytest.raises(ValueError):
+        experiment_symmetry.write(*args)
+
+
+def test_raises_without_bulk_info(mocker):
+    """Check complaints if no bulk-symmetry information is present."""
+    slab, rpars = (mocker.MagicMock() for _ in range(2))
+    slab.bulkslab = None
+    with pytest.raises(MissingBulkSlabError):
+        experiment_symmetry.write(slab, rpars)
 
 
 def test_success(with_info, tmp_path, subtests, first_case):
@@ -86,15 +95,7 @@ def test_success(with_info, tmp_path, subtests, first_case):
             assert any(re.match(pattern, line) for line in contents)
 
 
-def test_raises_without_bulk_info(mocker):
-    """Check complaints if no bulk-symmetry information is present."""
-    slab, rpars = (mocker.MagicMock() for _ in range(2))
-    slab.bulkslab = None
-    with pytest.raises(MissingBulkSlabError):
-        experiment_symmetry.write(slab, rpars)
-
-
-def test_write_failing(mocker):
+def test_write_fails(mocker):
     """Check complaints when writing to file fails."""
     mock_dict = {'SUPERLATTICE': mocker.MagicMock(),
                  'surfBasis': mocker.MagicMock()}
@@ -102,12 +103,4 @@ def test_write_failing(mocker):
     mocker.patch('builtins.open', side_effect=OSError)
     args = (mocker.MagicMock() for _ in range(2))
     with pytest.raises(OSError):
-        experiment_symmetry.write(*args)
-
-
-def test_get_dict_fails(mocker):
-    """Check complaints when fetching a dict of LEED parameters fails."""
-    mocker.patch(f'{_MODULE}.getLEEDdict', return_value=None)
-    args = (mocker.MagicMock() for _ in range(2))
-    with pytest.raises(ValueError):
         experiment_symmetry.write(*args)
