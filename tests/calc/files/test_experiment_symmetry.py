0a284e1fc49c5cbee41bb79c5c380e116802bb8c
Michele Riva <riva@iap.tuwien.ac.at>
2025-05-29 08:45:59 +0200

    refactor,tests: clean up

--- tests/calc/files/test_experiment_symmetry.py
+++ tests/calc/files/test_experiment_symmetry.py
@@ -7,12 +7,14 @@ __copyright__ = 'Copyright (c) 2019-2024 ViPErLEED developers'
 __created__ = '2024-07-01'
 __license__ = 'GPLv3+'
 
-import os
-
 import pytest
+from pytest_cases import fixture
 from pytest_cases import parametrize_with_cases
+
+from viperleed.calc.classes.rparams.special.energy_range import TheoEnergies
 from viperleed.calc.classes.slab import MissingBulkSlabError
 from viperleed.calc.files import experiment_symmetry
+from viperleed.calc.lib.context import execute_in_dir
 
 from pathlib import Path
 
@@ -24,54 +26,46 @@ CasePOSCARSlabs = poscar_slabs.CasePOSCARSlabs
 with_bulk_info = parametrize_with_cases('args', cases=CasePOSCARSlabs,
                                         has_tag=Tag.BULK_PROPERTIES)
 
+
+@fixture(name='mock_energies')
+def fixture_mock_energies():
+    """Give an example THEO_ENERGIES to rpars."""
+    def _mock(rpars):
+        rpars.THEO_ENERGIES = TheoEnergies(10, 100, 2)
+    return _mock
+
+
 @with_bulk_info
-def test_write_experiment_symmetry(args, tmp_path):
-    slab, rpars, info = args
-
-    os.chdir(tmp_path)
-    path = Path.cwd()
-
-    # create bulkslab
+def test_success(args, mock_energies, tmp_path):
+    """Check the expected result of writing experiment_symmetry.ini."""
+    slab, rpars, *_ = args
     slab.make_bulk_slab(rpars)
-
-    # put some fake THEO_ENERGIES
-    rpars.THEO_ENERGIES = rpars.THEO_ENERGIES.from_value([10, 100, 2])
-
-    experiment_symmetry.write(slab, rpars)
-    assert (path / 'experiment_symmetry.ini').exists()
-
-    content = (path / 'experiment_symmetry.ini').read_text()
+    mock_energies(rpars)
+    with execute_in_dir(tmp_path):
+        experiment_symmetry.write(slab, rpars)
+    written = tmp_path / 'experiment_symmetry.ini'
+    assert written.exists()
+    content = written.read_text()
     assert 'superlattice' in content
     assert 'eMax = 100.00' in content
 
 
 @parametrize_with_cases('args', cases=CasePOSCARSlabs,
                         has_tag=Tag.NO_INFO)
-def test_write_fails_without_bulk_info(args, tmp_path):
-    slab, rpars, info = args
-
-    os.chdir(tmp_path)
-
-    # put some fake THEO_ENERGIES
-    rpars.THEO_ENERGIES = rpars.THEO_ENERGIES.from_value([10, 100, 2])
-
+def test_raises_without_bulk_info(args, mock_energies):
+    """Check complaints if no bulk-symmetry information is present."""
+    slab, rpars, *_ = args
+    mock_energies(rpars)
     with pytest.raises(MissingBulkSlabError):
         experiment_symmetry.write(slab, rpars)
 
+
 @with_bulk_info
-def test_write_failing(args, monkeypatch):
-    slab, rpars, info = args
-
-    # create bulkslab
+def test_write_failing(args, mock_energies, mocker):
+    """Check complaints when writing to file fails."""
+    slab, rpars, *_ = args
     slab.make_bulk_slab(rpars)
-
-    # Monkey patch the `open` function to raise OSError
-    def mock_open(*args, **kwargs):
-        raise OSError("Mocked OSError")
-
-    # put some fake THEO_ENERGIES
-    rpars.THEO_ENERGIES = rpars.THEO_ENERGIES.from_value([10, 100, 2])
-
-    monkeypatch.setattr('builtins.open', mock_open)
+    mock_energies(rpars)
+    mocker.patch('builtins.open', side_effect=OSError)
     with pytest.raises(OSError):
         experiment_symmetry.write(slab, rpars)
