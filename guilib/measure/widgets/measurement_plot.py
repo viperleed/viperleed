d469b8436efbfd0fbd8c8c0c13848939305058f2
FlorianDoerr <doerr@iap.tuwien.ac.at>
2025-06-10 16:22:52 +0200

    minor: refactoring, docstring

--- guilib/measure/widgets/measurement_plot.py
+++ guilib/measure/widgets/measurement_plot.py
@@ -58,21 +58,23 @@ class MeasurementPlot(qtw.QWidget):
     def __init__(self, parent=None):
         """Initialise class."""
         super().__init__(parent=parent)
+        # Children widgets
         self._ctrls = {'quantities': PlotComboBox(),
                        'no_data': qtw.QLabel("NO DATA")}
         self._glob = {'plot_lines': defaultdict(dict),}
+        # Markers with their respective devices. Devices are the keys.
         self._ctrl_markers = {}
-        self.__data_points = None
-        self.__canvas = Canvas()
+        self._data_points = None
+        self._canvas = Canvas()
 
         self.setWindowTitle("Measurement data plot")
-        self.__compose()
+        self._compose()
         self._ctrls['quantities'].check_changed.connect(self.plot_all_data)
 
     @property
     def data_points(self):
         """Return the data points plotted."""
-        return self.__data_points
+        return self._data_points
 
     @data_points.setter
     def data_points(self, data_points):
@@ -98,7 +100,7 @@ class MeasurementPlot(qtw.QWidget):
                 f"{type(data_points).__name__!r} for data_points. "
                 "Expected a DataPoints object."
                 )
-        self.__data_points = data_points
+        self._data_points = data_points
         self.clear(uncheck_all=False)
         self.plot_all_data()
 
@@ -114,10 +116,10 @@ class MeasurementPlot(qtw.QWidget):
 
     def clear(self, uncheck_all=True):
         """Clear the plot."""
-        self.__canvas.ax.clear()
+        self._canvas.ax.clear()
         self._glob['plot_lines'] = defaultdict(dict)
-        self.__canvas.figure.tight_layout()
-        self.__canvas.draw_idle()
+        self._canvas.figure.tight_layout()
+        self._canvas.draw_idle()
         if uncheck_all:
             self._ctrls['quantities'].uncheck_all()
 
@@ -132,17 +134,17 @@ class MeasurementPlot(qtw.QWidget):
         if not self.data_points or not self.data_points.has_data:
             self._ctrls['no_data'].show()
             return
-        self.__canvas.ax.clear()
+        self._canvas.ax.clear()
         self._glob['plot_lines'] = defaultdict(dict)
 
         if self.plotted_quantities:
             if self.data_points.is_time_resolved:
-                self.__plot_all_time_resolved_data()
+                self._plot_all_time_resolved_data()
             else:
-                self.__plot_all_energy_resolved_data()
+                self._plot_all_energy_resolved_data()
 
-        self.__update_labels_and_legend()
-        self.__canvas.draw_idle()
+        self._update_labels_and_legend()
+        self._canvas.draw_idle()
 
     def plot_new_data(self):
         """Plot new data to already-plotted data.
@@ -157,14 +159,14 @@ class MeasurementPlot(qtw.QWidget):
             return
 
         if self.data_points.is_time_resolved:
-            self.__plot_new_time_resolved_data()
+            self._plot_new_time_resolved_data()
         else:
-            self.__plot_new_energy_resolved_data()
+            self._plot_new_energy_resolved_data()
 
-        self.__update_labels_and_legend()
-        self.__canvas.draw_idle()
+        self._update_labels_and_legend()
+        self._canvas.draw_idle()
 
-    def __compose(self):
+    def _compose(self):
         """Prepare widget."""
         layout = qtw.QGridLayout()
         self._ctrls['quantities'].addItems(QuantityInfo.get_axis_labels('y'))
@@ -175,33 +177,47 @@ class MeasurementPlot(qtw.QWidget):
         self._ctrls['no_data'].ensurePolished()
 
         layout.addWidget(self._ctrls['quantities'], 0, 0)
-        layout.addWidget(self.__canvas, 1, 0)
+        layout.addWidget(self._canvas, 1, 0)
         layout.addWidget(self._ctrls['no_data'], 1, 0,
                          alignment=qtc.Qt.AlignCenter)
         self.setLayout(layout)
-        self.__canvas.figure.tight_layout()
+        self._canvas.figure.tight_layout()
 
-    def __make_legend(self):
+    def _get_marker_style_for_device(self, ctrl, color):
+        """Get the marker style for the selected device.
+
+        Parameters
+        ----------
+        ctrl : ControllerABC
+            The controller for which the marker should be returned.
+        color : tuple of RGBA or str(color)
+            The color the marker should have.
+
+        Returns
+        -------
+        marker
+        style
+        """
+        marker = self._ctrl_markers[ctrl]
+        return _marker_style(*marker, color)
+
+    def _make_legend(self):
         """Create the legend for the displayed plot."""
         controllers = []
         legend_elements = []
-        for ctrl, marker in self._ctrl_markers.items():
+        for ctrl in self._ctrl_markers.keys():
             try:
                 ctrl_name = ctrl.name
             except AttributeError:
                 ctrl_name = ctrl
             controllers.append(ctrl_name)
-            style = _marker_style(*marker, 'black')
-            legend_elements.append(
-                Line2D([], [], label=ctrl_name, **style)
-                )
+            style = self._get_marker_style_for_device(ctrl, 'black')
+            legend_elements.append(Line2D([], [], label=ctrl_name, **style))
         if len(self.plotted_quantities) == 1:
             return legend_elements
 
         # Add a spacer
-        legend_elements.append(
-            Line2D([], [], linestyle='', label="")
-            )
+        legend_elements.append(Line2D([], [], linestyle='', label=""))
 
         for color, quantity in zip(_COLORS, self.plotted_quantities):
             legend_elements.append(
@@ -210,8 +226,8 @@ class MeasurementPlot(qtw.QWidget):
                 )
         return legend_elements
 
-    def __plot_all_energy_resolved_data(self):
-        axes = self.__canvas.ax
+    def _plot_all_energy_resolved_data(self):
+        axes = self._canvas.ax
         data, nominal_energies = (
             self.data_points.get_energy_resolved_data(*self.plotted_quantities)
             )
@@ -220,7 +236,7 @@ class MeasurementPlot(qtw.QWidget):
         if not has_data:
             return
 
-        self._ctrl_markers = dict(zip(data, _MARKERS))
+        self._store_markers_for_devices(data.keys())
 
         for color, quantity in zip(_COLORS, self.plotted_quantities):
             for ctrl, measurements in data.items():
@@ -233,13 +249,12 @@ class MeasurementPlot(qtw.QWidget):
                     energies = nominal_energies[:-1]
 
                 plot_color = color(COLOR_FRACTION)
-                marker = self._ctrl_markers[ctrl]
-                style = _marker_style(*marker, plot_color)
+                style = self._get_marker_style_for_device(ctrl, plot_color)
                 self.lines[quantity][ctrl], = axes.plot(energies, ctrl_data,
                                                         **style)
 
-    def __plot_all_time_resolved_data(self):
-        axes = self.__canvas.ax
+    def _plot_all_time_resolved_data(self):
+        axes = self._canvas.ax
         data, _ = self.data_points.get_time_resolved_data(
             *self.plotted_quantities,
             separate_steps=SEPARATE_STEPS
@@ -249,7 +264,7 @@ class MeasurementPlot(qtw.QWidget):
         if not has_data:
             return
 
-        self._ctrl_markers = dict(zip(data, _MARKERS))
+        self._store_markers_for_devices(data.keys())
 
         for color, quantity in zip(_COLORS, self.plotted_quantities):
             for ctrl, measurements in data.items():
@@ -257,11 +272,10 @@ class MeasurementPlot(qtw.QWidget):
                     continue
                 ctrl_data = measurements[quantity]
                 ctrl_times = measurements[QuantityInfo.TIMES]
-                marker = self._ctrl_markers[ctrl]
 
                 if not SEPARATE_STEPS:
                     plot_color = color(COLOR_FRACTION)
-                    style = _marker_style(*marker, plot_color)
+                    style = self._get_marker_style_for_device(ctrl, plot_color)
                     self.lines[quantity][ctrl], = axes.plot(ctrl_times,
                                                             ctrl_data, **style)
                     continue
@@ -274,11 +288,11 @@ class MeasurementPlot(qtw.QWidget):
                 # parallel because each step may (and usually will)
                 # have a variable number of data.
                 for clr, times, values in zip(colors, ctrl_times, ctrl_data):
-                    style = _marker_style(*marker, clr)
+                    style = self._get_marker_style_for_device(ctrl, clr)
                     axes.plot(times, values, **style)
 
-    def __plot_new_energy_resolved_data(self):
-        axes = self.__canvas.ax
+    def _plot_new_energy_resolved_data(self):
+        axes = self._canvas.ax
         data, nominal_energies = (
             self.data_points.get_energy_resolved_data(*self.plotted_quantities)
             )
@@ -287,7 +301,7 @@ class MeasurementPlot(qtw.QWidget):
         if not has_data:
             return
 
-        self._ctrl_markers = dict(zip(data, _MARKERS))
+        self._store_markers_for_devices(data.keys())
 
         for color, quantity in zip(_COLORS, self.plotted_quantities):
             for ctrl, measurements in data.items():
@@ -295,8 +309,7 @@ class MeasurementPlot(qtw.QWidget):
                     continue
                 ctrl_data = measurements[quantity]
                 plot_color = color(COLOR_FRACTION)
-                marker = self._ctrl_markers[ctrl]
-                style = _marker_style(*marker, plot_color)
+                style = self._get_marker_style_for_device(ctrl, plot_color)
                 lines = self.lines[quantity]
                 if ctrl not in lines:
                     lines[ctrl], = axes.plot(nominal_energies[-1],
@@ -306,8 +319,8 @@ class MeasurementPlot(qtw.QWidget):
         axes.relim()
         axes.autoscale_view()
 
-    def __plot_new_time_resolved_data(self):
-        axes = self.__canvas.ax
+    def _plot_new_time_resolved_data(self):
+        axes = self._canvas.ax
         data, _ = self.data_points.get_time_resolved_data(
             *self.plotted_quantities,
             separate_steps=SEPARATE_STEPS
@@ -317,7 +330,7 @@ class MeasurementPlot(qtw.QWidget):
         if not has_data:
             return
 
-        self._ctrl_markers = dict(zip(data, _MARKERS))
+        self._store_markers_for_devices(data.keys())
 
         for color, quantity in zip(_COLORS, self.plotted_quantities):
             for ctrl, measurements in data.items():
@@ -325,11 +338,10 @@ class MeasurementPlot(qtw.QWidget):
                     continue
                 ctrl_data = measurements[quantity]
                 ctrl_times = measurements[QuantityInfo.TIMES]
-                marker = self._ctrl_markers[ctrl]
 
                 if not SEPARATE_STEPS:
                     plot_color = color(COLOR_FRACTION)
-                    style = _marker_style(*marker, plot_color)
+                    style = self._get_marker_style_for_device(ctrl, plot_color)
                     lines = self.lines[quantity]
                     if ctrl not in lines:
                         lines[ctrl], = axes.plot(ctrl_times, ctrl_data,
@@ -343,7 +355,8 @@ class MeasurementPlot(qtw.QWidget):
                     np.linspace(0.2, 0.8, self.data_points.nr_steps_total)
                     )
                 color_idx = (len(ctrl_data) - 1) % len(colors)
-                style = _marker_style(*marker, colors[color_idx])
+                style = self._get_marker_style_for_device(ctrl,
+                                                          colors[color_idx])
                 # Enough to plot the last step for each
                 axes.plot(ctrl_times[-1], ctrl_data[-1], **style)
 
@@ -351,9 +364,24 @@ class MeasurementPlot(qtw.QWidget):
             axes.relim()
             axes.autoscale_view()
 
-    def __update_labels_and_legend(self):
+    def _store_markers_for_devices(self, ctrls):
+        """Set the markers for each controller.
+
+        Parameters
+        ----------
+        ctrls : iterable of ControllerABCs
+            An iterable that contains the controllers
+            for which markers have to be set.
+
+        Returns
+        -------
+        None.
+        """
+        self._ctrl_markers = dict(zip(ctrls, _MARKERS))
+
+    def _update_labels_and_legend(self):
         """Update axes labels and legend, if necessary."""
-        axes = self.__canvas.ax
+        axes = self._canvas.ax
         if axes.legend_ is not None:
             # Axes and labels already up to date
             return
@@ -368,7 +396,7 @@ class MeasurementPlot(qtw.QWidget):
         if not self.plotted_quantities:
             axes.legend_ = None
             axes.set_ylabel("")
-            self.__canvas.figure.tight_layout()
+            self._canvas.figure.tight_layout()
             return
 
         if len(self.plotted_quantities) == 1:
@@ -376,8 +404,8 @@ class MeasurementPlot(qtw.QWidget):
         else:
             yname = self.plotted_quantities[0].common_label
         axes.set_ylabel(f"{yname} ({self.plotted_quantities[0].units})")
-        axes.legend(handles=self.__make_legend(), loc='upper left')
-        self.__canvas.figure.tight_layout()
+        axes.legend(handles=self._make_legend(), loc='upper left')
+        self._canvas.figure.tight_layout()
 
 
 class PlotComboBox(CheckComboBox):
