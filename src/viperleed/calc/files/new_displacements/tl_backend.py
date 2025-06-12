c390f555c11cae0c3a00e6d07cc62dc13f9c4d34
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-06-12 15:50:42 +0200

    feat: remove search block argument
    Co-Authored-By: Michele Riva <riva@iap.tuwien.ac.at>

--- src/viperleed/calc/files/new_displacements/tl_backend.py
+++ src/viperleed/calc/files/new_displacements/tl_backend.py
@@ -24,12 +24,11 @@ class TensorLEEDBackend(ABC):
         self._handle_search_block_func = handle_search_block_func
 
     @abstractmethod
-    def replace_search_block(self, offsets_block, search_block):
+    def replace_search_block(self, search_block):
         """Check if the backend can handle the given search block.
 
         Parameters
         ----------
-        offsets_block (OffsetsBlock): The offsets block.
         search_block (SearchBlock): The search block.
 
         Returns
@@ -43,7 +42,7 @@ class TensorLEEDBackend(ABC):
         IncompatibleBackendError: If the backend can't handle the search block
             and can't provide a replacement.
         """
-        return self._handle_search_block_func(offsets_block, search_block)
+        return self._handle_search_block_func(search_block)
 
 
 def tenserleed_search_block_handler_func(offsets_block, search_block):
