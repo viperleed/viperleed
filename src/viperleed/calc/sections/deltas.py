b323da44378273442c2f695d1946b8198784cb3c
Michele Riva <riva@iap.tuwien.ac.at>
2025-07-10 04:50:33 +0200

    chore: remove unused imports

--- src/viperleed/calc/sections/deltas.py
+++ src/viperleed/calc/sections/deltas.py
@@ -20,11 +20,9 @@ import subprocess
 import numpy as np
 
 from viperleed.calc.constants import DEFAULT_DELTAS
-from viperleed.calc.constants import DEFAULT_SUPP
 from viperleed.calc.constants import DEFAULT_TENSORS
 from viperleed.calc.files import iodeltas
 from viperleed.calc.files import iotensors
-from viperleed.calc.files.beams import writeAUXBEAMS
 from viperleed.calc.files.displacements import readDISPLACEMENTS_block
 from viperleed.calc.lib import leedbase
 from viperleed.calc.lib import parallelization
