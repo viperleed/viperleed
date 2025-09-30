a3b8a7b31d6bb79610380cfdcb21dbc181931845
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-09-30 15:43:39 +0200

    fix,test: add more tests for loop blocks

--- tests/calc/files/displacements/test_segments.py
+++ tests/calc/files/displacements/test_segments.py
@@ -5,7 +5,11 @@ __copyright__ = 'Copyright (c) 2019-2025 ViPErLEED developers'
 __created__ = '2025-06-18'
 __license__ = 'GPLv3+'
 
+import pytest
 
+from viperleed.calc.files.new_displacements.errors import (
+    DisplacementsSyntaxError,
+)
 from viperleed.calc.files.new_displacements.lines import (
     ConstraintLine,
     GeoDeltaLine,
@@ -46,10 +50,13 @@ def test_layered_loop_block():
     lines = iter(
         [
             LoopMarkerLine('<loop>'),
-            SearchHeaderLine('== SEARCH test'),
+            SearchHeaderLine('== SEARCH z'),
             SectionHeaderLine('= GEO_DELTA'),
             GeoDeltaLine('Fe 1 z = 0.0 0.1'),
             LoopMarkerLine('</loop>'),
+            SearchHeaderLine('== SEARCH y'),
+            SectionHeaderLine('= GEO_DELTA'),
+            GeoDeltaLine('Fe 1 xy[0 1] = 0.0 0.1'),
             LoopMarkerLine('</loop>'),
         ]
     )
@@ -59,3 +66,41 @@ def test_layered_loop_block():
     returned_lines = list(returned_lines)
     # make sure all lines were processed
     assert len(returned_lines) == 0
+
+
+def test_empty_loop_fails():
+    header = LoopMarkerLine('<loop>')
+    lines = iter(
+        [
+            LoopMarkerLine('</loop>'),
+        ]
+    )
+    loop_block = LoopBlock(header)
+
+    with pytest.raises(
+        DisplacementsSyntaxError,
+        match='Loop block cannot be empty.',
+    ):
+        loop_block.read_lines(lines)
+
+
+def test_loop_with_only_loop_child_complains():
+    # test that a loop whose only child is another loop raises an error
+    header = LoopMarkerLine('<loop>')
+    lines = iter(
+        [
+            LoopMarkerLine('<loop>'),
+            SearchHeaderLine('== SEARCH test'),
+            SectionHeaderLine('= GEO_DELTA'),
+            GeoDeltaLine('Fe 1 z = 0.0 0.1'),
+            LoopMarkerLine('</loop>'),
+            LoopMarkerLine('</loop>'),
+        ]
+    )
+    loop_block = LoopBlock(header)
+
+    with pytest.raises(
+        DisplacementsSyntaxError,
+        match='Loop block cannot contain only another loop block',
+    ):
+        loop_block.read_lines(lines)
