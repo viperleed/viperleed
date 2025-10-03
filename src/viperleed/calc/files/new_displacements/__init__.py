8a5b0296daf105520318e2ad4b97974161e22b55
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-10-03 12:13:44 +0200

    feat,fix: import file class in init

--- src/viperleed/calc/files/new_displacements/__init__.py
+++ src/viperleed/calc/files/new_displacements/__init__.py
@@ -4,3 +4,5 @@ __authors__ = ('Alexander M. Imre (@amimre)',)
 __copyright__ = 'Copyright (c) 2019-2025 ViPErLEED developers'
 __created__ = '2024-10-04'
 __license__ = 'GPLv3+'
+
+from .file import DisplacementsFile
