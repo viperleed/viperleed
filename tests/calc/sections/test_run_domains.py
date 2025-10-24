c766ded57bd1d36d6f6ba8d49d81d4e9d83d37e1
Michele Riva <riva@iap.tuwien.ac.at>
2025-10-24 10:50:45 +0200

    fix,tests: TensErLEED-dependent domains
    Turns out that the search is broken in different manners in older TensErLEED version due to problems in the Fortran code (which were fixed since). Run tests only with the versions that we know are fine.

--- tests/calc/sections/test_run_domains.py
+++ tests/calc/sections/test_run_domains.py
@@ -9,13 +9,30 @@ __license__ = 'GPLv3+'
 
 
 from pytest_cases import fixture
+from pytest_cases import parametrize
+
+from viperleed.calc.files import tenserleed
+from viperleed.calc.lib.version import Version
 
 from .conftest import DomainsCalcFilesSetup as Setup
-from .conftest import with_tl_versions
-
 
 @fixture(name='domains_full', scope='session')
-@with_tl_versions
+@parametrize(tl_version=(
+    tenserleed.CURRENT_TL_VERSION,
+    Version('1.7.5'),    # The first one where search works
+    # Earlier versions fail during search for various reasons:
+    # - 1.7.1 -- 1.7.4
+    #    Race condition in writing SD.TL in fortran code prevents
+    #    from reading a full configuration. Fixed in v1.7.5.
+    # - 1.6.1
+    #    With the IVBEAMS currently used, at least one beam has
+    #    too few data for interpolation in the given energy range.
+    #    This beam, however, is used in lib.search/COMNEI (though
+    #    it should be skipped), giving a SEGFAULT (index error).
+    #    Using a smaller set of IVBEAMS (and thus re-running a
+    #    refcalc) prevents the segfault but has the same race
+    #    condition problem as v1.7.1--1.7.4.
+    ))
 def fixure_domains_full(tl_version, tensorleed_path, make_section_tempdir):
     """A setup for a full multi-domain calculation."""
     domains = 'telluride_stacking'
