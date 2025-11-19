9b5363973408c4697d836c7878a6f135278466e1
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-11-19 17:37:34 +0100

    doc: update header

--- src/viperleed/calc/lib/rfactor.py
+++ src/viperleed/calc/lib/rfactor.py
@@ -1,7 +1,10 @@
 """Module R-factor."""
 
 __authors__ = ('Alexander M. Imre (@amimre)',)
+__copyright__ = 'Copyright (c) 2019-2025 ViPErLEED developers'
 __created__ = '2024-02-21'
+__license__ = 'GPLv3+'
+
 
 import numpy as _np
 
