1d0429c720ec102c5ff0c8c243074ed7d7e6c313
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-11-19 17:17:07 +0100

    feat: use numpy and scipy

--- src/viperleed/calc/lib/spline_interpolation.py
+++ src/viperleed/calc/lib/spline_interpolation.py
@@ -7,9 +7,8 @@ library.
 __authors__ = ('Alexander M. Imre (@amimre)',)
 __created__ = '2024-02-19'
 
-import interpax
 import numpy as np
-from jax import numpy as jnp
+from scipy.interpolate import CubicSpline, PPoly
 
 # The two functions below are the only ones that are currently used
 # Everything else is done by the interpax module.
@@ -24,10 +23,10 @@ def make_1d_ragged_cubic_spline(
     """
     if x.ndim > 1 or y.ndim > 1:
         raise ValueError('x and y must be 1-dimensional arrays.')
-    y_mask = jnp.isnan(y)
+    y_mask = np.isnan(y)
     x_subarray, y_subarray = x[~y_mask], y[~y_mask]
-    start_index = jnp.where(~y_mask)[0][0]
-    subarray_spline = interpax.CubicSpline(
+    start_index = np.where(~y_mask)[0][0]
+    subarray_spline = CubicSpline(
         x_subarray, y_subarray, axis, bc_type, extrapolate, check=False
     )
 
@@ -37,14 +36,14 @@ def make_1d_ragged_cubic_spline(
 def interpolate_ragged_array(
     x, y, axis=0, bc_type='not-a-knot', extrapolate=False
 ):
-    all_coeffs = np.full((4, y.shape[0], y.shape[1]), fill_value=jnp.nan)
+    all_coeffs = np.full((4, y.shape[0], y.shape[1]), fill_value=np.nan)
     for dim in range(y.shape[1]):
         _y = y[:, dim]
-        all_nans = jnp.all(jnp.isnan(_y))
+        all_nans = np.all(np.isnan(_y))
         if all_nans:
             continue
         spline, start_id = make_1d_ragged_cubic_spline(
             x, y[:, dim], axis=0, bc_type=bc_type, extrapolate=None
         )
         all_coeffs[:, start_id : start_id + spline.c.shape[1], dim] = spline.c
-    return interpax.PPoly.construct_fast(all_coeffs, x)
+    return PPoly.construct_fast(all_coeffs, x)
