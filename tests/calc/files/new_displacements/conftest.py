8de9e44c9827a6d63098769516896f2370d7bff0
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2026-01-02 20:16:00 +0100

    fix,test: correct mode token name

--- tests/calc/files/new_displacements/conftest.py
+++ tests/calc/files/new_displacements/conftest.py
@@ -121,7 +121,7 @@ def mock_displacements_path_and_lines():
         (
             ConstraintLine,
             {
-                'type': ModeToken('vib'),
+                'mode_token': ModeToken('vib'),
                 'targets': (TargetToken('Ir_top'),),
                 'linear_operation': LinearOperationToken.from_array([[1.0]]),
                 'link_target': TargetToken('Ir_top'),
@@ -130,7 +130,7 @@ def mock_displacements_path_and_lines():
         (
             ConstraintLine,
             {
-                'type': ModeToken('geo'),
+                'mode_token': ModeToken('geo'),
                 'targets': (TargetToken('O L(1-2)'), TargetToken('Ir L(1)')),
                 'linear_operation': LinearOperationToken.from_array([[1.0]]),
                 'link_target': TargetToken('O L(1-2)'),
