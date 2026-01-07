0bd62b5f1b53c91df89dfdf80db61d588345a7bd
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2026-01-07 15:48:26 +0100

    feat,minor: add more rfactor synonyms

--- src/viperleed/calc/lib/rfactor/__init__.py
+++ src/viperleed/calc/lib/rfactor/__init__.py
@@ -13,10 +13,10 @@ from .smooth import R_s
 from .zannazi_jona import R_zj
 
 R_FACTOR_SYNONYMS = {
-    R_pendry: ('pendry', 'r_p', 'rp', 'pendry r-factor'),
+    R_pendry: ('pendry', 'r_p', 'rp', 'pendry r-factor', 'p'),
     R_1: ('r1', 'r_1', 'r1 factor'),
     R_2: ('r2', 'r_2', 'r2 factor'),
-    R_s: ('s', 'rs', 'r_s', 'smooth'),
+    R_s: ('s', 'rs', 'r_s', 'smooth', 'schmid'),
     R_zj: (
         'zj',
         'zj factor',
