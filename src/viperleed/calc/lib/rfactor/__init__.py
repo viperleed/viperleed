8dc48a77fe9a4060f600b7a8296b17f8c97b40f4
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2026-01-07 15:46:48 +0100

    feat: import R factors to init

--- src/viperleed/calc/lib/rfactor/__init__.py
+++ src/viperleed/calc/lib/rfactor/__init__.py
@@ -6,8 +6,14 @@ __created__ = '2024-02-21'
 __license__ = 'GPLv3+'
 
 
+from .pendry import R_pendry
+from .R_1 import R_1
+from .R_2 import R_2
+from .smooth import R_s
+from .zannazi_jona import R_zj
+
 R_FACTOR_SYNONYMS = {
-    pendry_R: ('pendry', 'r_p', 'rp', 'pendry r-factor'),
+    R_pendry: ('pendry', 'r_p', 'rp', 'pendry r-factor'),
     R_1: ('r1', 'r_1', 'r1 factor'),
     R_2: ('r2', 'r_2', 'r2 factor'),
     R_s: ('s', 'rs', 'r_s', 'smooth'),
