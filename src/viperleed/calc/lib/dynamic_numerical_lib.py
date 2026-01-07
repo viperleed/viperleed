fix: implement stop_gradient function
See also 068ea92af96ae83a37ace921308e0b6db115bfce

src/viperleed/calc/lib/dynamic_numerical_lib.py
@@ -9,8 +9,8 @@ from this module.
 For example, to use JAX instead of NumPy, you can do:
 
     from viperleed.calc.lib import dynamic_numerical_lib as dnl
-    import jax.numpy as jnp
-    dnl.xp = jnp
+    import jax
+    dnl.xp = jax.numpy
 """
 
 __authors__ = ('Alexander M. Imre (@amimre)',)
@@ -29,3 +29,21 @@ from scipy.interpolate import (
 xp = _np
 CubicSpline = _SciCubicSpline
 PPoly = _SciPPoly
+
+
+def stop_gradient(array):
+    """Stop gradient calculation of given array.
+
+    This is a no-op when using NumPy as the numerical library.
+    When using the R-factor calculation with a numerical library that
+    supports automatic differentiation (such as JAX in viperleed-jax),
+    this utility function should be patched with a method that stops
+    gradient propagation. In JAX this can be done as
+    ```
+    from viperleed.calc.lib import dynamic_numerical_lib as dnl
+
+    dnl.xp = jax.numpy
+    dnl.stop_gradient = jax.lax.stop_gradient
+    ```
+    """
+    return array