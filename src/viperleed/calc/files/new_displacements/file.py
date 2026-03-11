ac312e937f7195e2909c26a44773b9dbc23de4c1
Florian Kraushofer <kraushofer.florian@gmail.com>
2026-03-11 11:47:08 +0100

    fix: search loops and repeats
    - reset displacement iterator after a RUN=3 block
    - actually deal with all DISPLACEMENTS blocks with a RUN=3 block
    
    TODO: implement MAX_TL_DISPLACEMENT
    Co-Authored-By: Alexandra M. Imre <90624999+amimre@users.noreply.github.com>

--- src/viperleed/calc/files/new_displacements/file.py
+++ src/viperleed/calc/files/new_displacements/file.py
@@ -62,6 +62,15 @@ class DisplacementsFile(NodeMixin):
         if not self._has_been_read:
             raise ValueError('File has not been read yet. Call read() first.')
 
+    def reset_iterator(self):
+        """
+        Reset to the beginning of the displacement file, e.g. when search
+        repeats after a new reference calculation.
+        """
+        self._child_id = 0
+        for child in self.children:
+            child.reset_iterator()
+
     @property
     def offsets(self):
         """Return the OFFSETS block if present, else None."""
