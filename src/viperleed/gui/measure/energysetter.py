9e2b7e91382e2453322354a5a2854aa79e0ecdeb
FlorianDoerr <doerr@iap.tuwien.ac.at>
2026-08-27 15:56:55 +0200

    minor: refactoring

--- src/viperleed/gui/measure/energysetter.py
+++ src/viperleed/gui/measure/energysetter.py
@@ -283,9 +283,7 @@ class EnergySetter(qtw.QWidget):
         ctrl_cls_name = ctrl_settings.get('controller', 'controller_class')
         ctrl_settings.prepare_aliases(ctrl_cls_name)
         ctrl_cls = base.class_from_name('controller', ctrl_cls_name)
-        address = ctrl_settings.get('controller', 'address')
-        ctrl = ctrl_cls(settings=ctrl_settings, address=address,
-                        sets_energy=True)
+        ctrl = ctrl_cls(settings=ctrl_settings, sets_energy=True)
         return ctrl
 
     def cleanup_controller(self):
